088ed8f9f3344855b9a26ba2176c033a4c0f8414
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-03-21 09:53:19 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@128 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fBrowser/data/gamData.R
+++ pkg/fBrowser/data/gamData.R
@@ -1,20 +1,15 @@
 
 
-gamData = 
-function() 
-{   # A function implemented by Diethelm Wuertz
-
-    # GAM - Example Data:
-    # Example Data: x = 0.7*sin(x1) + 0.3*exp(x2) + eps
-    # fit = gam(formula = x ~ s(x1) + s(x2), data = x)
-    set.seed(4711)
-    x1 = rnorm(1000)
-    x2 = rnorm(1000)
-    y = sin(x1) + exp(x2) 
-    eps = 0.1 * rnorm(1000)
-    y = y + eps
-    data.frame(cbind(x = y, x1 = x1, x2 = x2))
-}
-
-gamData()
+# A function implemented by Diethelm Wuertz
+# GAM - Example Data:
+# Example Data: x = 0.7*sin(x1) + 0.3*exp(x2) + eps
+# fit = gam(formula = x ~ s(x1) + s(x2), data = x)
+set.seed(4711)
+x1 = rnorm(1000)
+x2 = rnorm(1000)
+y = sin(x1) + exp(x2) 
+eps = 0.1 * rnorm(1000)
+y = y + eps
+gamData = data.frame(cbind(x = y, x1 = x1, x2 = x2))
+rm("x1", "x2", "eps", "y")
 
