913461ee1ff3941efb0201f3cbcffe4948f8a4e8
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-04-28 09:15:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@261 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fCalendar/demo/xmpDWChapter021.R
+++ pkg/fCalendar/demo/xmpDWChapter021.R
@@ -10,11 +10,11 @@
 #
 # List of Examples:  
 #
-#	Exercise: Convert ISO-8601 Dates to Julian Counts  
-#	Exercise: Convert Julian Counts to ISO-8601 Dates  
-# 	Exercise: Test the functions .DateToJulian and .JulianToDate  
-# 	Exercise: Compute the Day-of-Week 
-#  	Exercise: Decide if a Year is a Leap Year or not        
+#   Exercise: Convert ISO-8601 Dates to Julian Counts  
+#   Exercise: Convert Julian Counts to ISO-8601 Dates  
+#   Exercise: Test the functions .DateToJulian and .JulianToDate  
+#   Exercise: Compute the Day-of-Week 
+#   Exercise: Decide if a Year is a Leap Year or not        
 #
 # Author:
 #   (C) 1997-2005, Diethelm Wuertz, GPL
@@ -29,10 +29,10 @@
 
 ### Load Packages:
 
-	require(fBasics)
-	require(fCalendar)
-	###
-	
+    require(fBasics)
+    require(fCalendar)
+    ###
+    
 
 # ------------------------------------------------------------------------------
 
@@ -60,16 +60,16 @@
     # Try:
     .DateToJulian(c(19600101, 19700101, 20050101))
     # [1] 2436936 2440588 2453372
- 	###
- 	
+    ###
+    
   
 # ------------------------------------------------------------------------------
 
 
 ### Exercise: Convert Julian Counts to ISO-8601 Dates
  
-	# Write your own function:
-	.JulianToDate = function(JD)
+    # Write your own function:
+    .JulianToDate = function(JD)
     {
         # To ISO Date:
         y <- (4 * (JD - 1721119) - 1) %/% 146097
@@ -101,65 +101,65 @@
 
 ### Exercise: Test the functions .DateToJulian and .JulianToDate  
     
-	# Generate 20 random Dates:
-	n = 20
-	YYYY = floor(c(1970, 1960, runif(n, 1620, 2100)))
-	MM = floor(c(1, 1, runif(n, 1, 12)))
-	DD = floor(c(1, 1, runif(n, 1, 28)))
-	cbind(YYYY, MM, DD)
-	###
-	
-		
-	# Test - Returns a Vector of zeros:
-	YYYY*10000+MM*100+DD - julian2date(date2julian(YYYY, MM, DD))
-	###
-	
-	
+    # Generate 20 random Dates:
+    n = 20
+    YYYY = floor(c(1970, 1960, runif(n, 1620, 2100)))
+    MM = floor(c(1, 1, runif(n, 1, 12)))
+    DD = floor(c(1, 1, runif(n, 1, 28)))
+    cbind(YYYY, MM, DD)
+    ###
+    
+        
+    # Test - Returns a Vector of zeros:
+    YYYY*10000+MM*100+DD - julian2date(date2julian(YYYY, MM, DD))
+    ###
+    
+    
 # ------------------------------------------------------------------------------
 
 
 ### Exercise: Compute the Day-of-Week
        
-	# Write your own function:
-	.DayOfWeek = function(YYYYMMDD)
+    # Write your own function:
+    .DayOfWeek = function(YYYYMMDD)
     {
         # Compute Day of Week:
         year = YYYYMMDD %/% 10000
         month = (YYYYMMDD - year*10000) %/% 100
         day = YYYYMMDD - year*10000 - month*100
         
-       	# Method based on Julian Counts:
-       	a = (14 - month) %/% 12
-	    y = year - a
-	    m = month + 12*a - 2
-	    DOW.JD = (day + y + y%/%4 - y%/%100 + y%/%400 + (31*m)%/%12) %% 7
-	   
+        # Method based on Julian Counts:
+        a = (14 - month) %/% 12
+        y = year - a
+        m = month + 12*a - 2
+        DOW.JD = (day + y + y%/%4 - y%/%100 + y%/%400 + (31*m)%/%12) %% 7
+       
         # Method used in Chron Package;
         ix = year + trunc((month - 14)/12)
-	    jx = (trunc((13 * (month + 10 - (month + 10)%/%13 * 12) - 1)/5) + 
-	    	day + 77 + (5 * (ix - (ix%/%100) * 100))%/%4 + ix%/%400 - 
-	    	(ix%/%100) * 2)
-	    DOW.CHRON = jx %% 7
+        jx = (trunc((13 * (month + 10 - (month + 10)%/%13 * 12) - 1)/5) + 
+            day + 77 + (5 * (ix - (ix%/%100) * 100))%/%4 + ix%/%400 - 
+            (ix%/%100) * 2)
+        DOW.CHRON = jx %% 7
 
-	    # Method of Uspensky and Heaslet:
-	    c = year %/% 100
-	    y = year - c*100
-	    m = ifelse(month > 2, month-2, month+10)
-	    y = y - ifelse(month < 3, 1, 0)
-	    DOW.UH = (day + floor(2.6*m-0.2) + y + y%/%4 + c%/%4 - 2*c ) %% 7
-	        
-	    # Return Value:
-	    cbind(DOW.JD, DOW.CHRON, DOW.UH)
+        # Method of Uspensky and Heaslet:
+        c = year %/% 100
+        y = year - c*100
+        m = ifelse(month > 2, month-2, month+10)
+        y = y - ifelse(month < 3, 1, 0)
+        DOW.UH = (day + floor(2.6*m-0.2) + y + y%/%4 + c%/%4 - 2*c ) %% 7
+            
+        # Return Value:
+        cbind(DOW.JD, DOW.CHRON, DOW.UH)
     }
     ###
     
     # Test it with Random Dates:
-	n = 20
-	YYYY = floor(c(1970, 1960, runif(n, 1620, 2100)))
-	MM = floor(c(1, 1, runif(n, 1, 12)))
-	DD = floor(c(1, 1, runif(n, 1, 28)))
-	YYYYMMDD = YYYY*10000 + MM*100 + DD
-	.DayOfWeek(YYYYMMDD)
+    n = 20
+    YYYY = floor(c(1970, 1960, runif(n, 1620, 2100)))
+    MM = floor(c(1, 1, runif(n, 1, 12)))
+    DD = floor(c(1, 1, runif(n, 1, 28)))
+    YYYYMMDD = YYYY*10000 + MM*100 + DD
+    .DayOfWeek(YYYYMMDD)
     ###
     
     
@@ -168,8 +168,8 @@
 
 ### Exercise: Decide if a Year is a Leap Year or Not
        
-	# Write your own function:
-	.LeapYear = function(YYYY)
+    # Write your own function:
+    .LeapYear = function(YYYY)
     {
         # Return TRUE or FALSE:
         leap.year =
@@ -182,9 +182,9 @@
     # Try:
     .LeapYear(c(2000, 2002, 2004, 2006))
     # [1]  TRUE FALSE  TRUE FALSE
-	###
-	
-	
+    ###
+    
+    
 ################################################################################  
-	    
-	   
+        
+       
