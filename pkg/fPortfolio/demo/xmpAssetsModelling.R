b36e3fb09150e31cb793962bae8fb45c3d05e872
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-04-30 16:20:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@273 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fPortfolio/demo/xmpAssetsModelling.R
+++ pkg/fPortfolio/demo/xmpAssetsModelling.R
@@ -31,7 +31,7 @@
     ###
     
 
-# ------------------------------------------------------------------------------
+################################################################################
 # 1. Select the 4 most dissimilar assets from Berndt's data set
 
 
@@ -45,22 +45,27 @@
     # and the last (the 18th) the risk free rate.
     ###
         
+    
     # Load the Data and Create an Object of Class 'timeSeries':
     data(berndtInvest)
-    berndtInvest = as.timeSeries(berndtInvest, format = "%d-%B-%y")
+    berndtInvest = as.timeSeries(berndtInvest)
     class(berndtInvest)
     head(berndtInvest)
+    ###
+    
+    
     # Exclude the Date, Market Returns and Interest Rate Columns 
     # from the data frame, then multiply by 100 for percentual returns ...
     allAssets = 100 * berndtInvest[, -c(1, 10, 17)]
-    # Assign Dates as Row Names:
     class(allAssets)
     head(allAssets)
     ###
         
+    
     # Graph Frame:
     par(mfrow = c(2, 1), cex = 0.7)
     ###
+    
     
     # Select the "n" Most Dissimilar Assets from 
     # Hierarchical Clustering:
@@ -77,10 +82,11 @@
     mu.vec = colMeans(myAssets@Data)
     mu.vec
     # Print the Covariance Matrix:
-    cov.mat = cov(myAssets)
+    cov.mat = cov(myAssets@Data)
     cov.mat
     ###
-            
+          
+      
     # Plot Cumulated Returns of the Assets:
     ts.plot(colCumsums(myAssets), col = 1:4)
     grid()
@@ -90,7 +96,7 @@
     ###
 
 
-# ------------------------------------------------------------------------------
+################################################################################
 # 2. Fit the distributional parameters for Berndt's data set
 
 
@@ -104,22 +110,40 @@
     # by maximum log-likelihood estimation.
     ###
     
+    
+    # Data:
+    data(berndtInvest)
+    stocks = c("CONTIL", "MOBIL", "TEXACO", "GERBER")
+    myAssets = 100 * as.timeSeries(berndtInvest)[, stocks]
+    ###
+    
+    
     # Fit myAsssets Data Set:   
     # Fit a multivariate normal distribution:
     fit.norm  = assetsFit(x = myAssets, method = "norm")
     fit.norm
+    ###
+    
+    
     # Fit a multivariate skewed normal distribution:
     fit.snorm = assetsFit(x = myAssets, method = "snorm") 
     fit.snorm
+    ###
+    
+    
     # Fit a multivariate skewed Student-t distribution:
     fit.st = assetsFit(x = myAssets, method = "st")
     fit.st
+    ###
+    
+    
     # Show Class and Slot Names:
     class(fit.st)
     slotNames(fit.st)
     print(fit.st@model)
     class(fit.st@model)
     ###
+    
         
     # Investigate the fitted Parameters:
     par(mfrow = c(1, 1), cex = 0.7)
@@ -159,8 +183,9 @@
     ###
     
 
-# ------------------------------------------------------------------------------
-# 3. Simulate an artificial data set of assets
+################################################################################
+# 3. Simulate an artificial data set of assets with the same distributional
+#    properties as Berndt's data set
 
 
     # Two ways for Simulation:
@@ -172,29 +197,254 @@
     # For details we refer to Rmetric's 'Multivariate Distribution'
     ###
 
+    
+    # Data:
+    data(berndtInvest)
+    units = c("CONTIL", "MOBIL", "TEXACO", "GERBER")
+    myAssets = 100 * as.timeSeries(berndtInvest)[, units]
+    ###
+    
+    
     # Method 1:
     simulatedAssets = assetsSim(
-        n = length(myAssets[,1]), 
+        n = dim(myAssets)[1], 
         model = fit.st@model)
     simulatedAssets
     ###
     
+    
     # Method 2:
-    # fit.st@model
-    # alpha = fit.st@model$alpha
-    # Omega = fit.st@model$Omega
-    # beta = fit.st@model$beta
-    # simulatedAssets = assetsSim(n = 120, 
-    #   model = list(beta = beta, Omega = Omega, alpha = alpha, df = Inf))
+    mu = fit.st@model$mu
+    Omega = fit.st@model$Omega
+    alpha = fit.st@model$alpha
+    assetsSim(
+        n = 120, 
+        model = list(mu = mu, Omega = Omega, alpha = alpha, df = Inf))
+    ###
+    
+    
     # Plot Cumulated Returns of the Assets:
+   
+    assetsSeriesPlot =
+    function(x, labels = TRUE)
+    {   # A function implemented by Diethelm Wuertz
+    
+        # Description:
+        #   Plots the series of prices or indices of a set of assets
+        #   given their returns.
+        
+        # Arguments:
+        #   x - a timeSeries object or any other rectangular object
+        #       which can be transformed by the function as. matrix
+        #       into a numeric matrix.
+        #   labels - a logical flag. Should labels and a legend added 
+        #       to the plot?
+        
+        # FUNCTION:
+        
+        # Plot:
+        x = as.matrix(x)
+        X = colCumsums(x)
+        nCols = dim(x)[2]
+        ts.plot(X, col = 1:nCols, xlab = "", ylab = "", main = "")
+        abline(h = 0, lty = 3) 
+        
+        # Add Labels:
+        if (labels) {
+            legend(0, max(X), legend = colnames(x), col = 1:nCols, lty = 1)
+            title(
+                main = "Cumulated Returns", 
+                xlab = "Time",
+                ylab = "Cumulated Returns")
+        }     
+        
+        # Return Value:
+        invisible()
+    }
+    
     par(mfrow = c(1, 1))
-    ts.plot(colCumsums(simulatedAssets), col = 1:4)
-    legend(0, 300, legend = colnames(simulatedAssets), col = 1:4, lty = 1)
-    title(main = "Cumulated Returns", ylab = "Cumulated Returns")
-    abline(h = 0, lty = 3)
+    assetsSeriesPlot(myAssets)
     ###
     
     
 ################################################################################
+# Robust Mean and Covarianre Estimates:
+    
+   
+    # Data:
+    data(berndtInvest)
+    units = c("CONTIL", "MOBIL", "TEXACO", "GERBER")
+    myAssets = 100 * as.timeSeries(berndtInvest)[, units]
+    class(myAssets)
+    summary(myAssets)
+    ###
+    
+    
+    # Load MASS Library:
+    require(MASS)
+    corpcorBuiltin()
+    covrobustBuiltin()  
+    ### 
 
     
+    # Classical Robust Estimation: 
+    assetsMeanCov(myAssets, "cov")
+    assetsMeanCov(myAssets, "mve")
+    assetsMeanCov(myAssets, "mcd")
+    assetsMeanCov(myAssets, "nnve")
+    ###
+    
+    
+    # Histogram Plot:
+    par(mfrow = c(2, 2), cex = 0.7)
+    assetsHistPlot(myAssets, "cov")
+    assetsHistPlot(myAssets, "mve")
+    assetsHistPlot(myAssets, "mcd")
+    ###
+    
+    
+    par(mfrow = c(1, 1))
+    assetsPairsPlot(myAssets)
+    assetsCorTestPlot
+    ###
+            
+    
+    par(mfrow = c(2, 2), cex = 0.7)
+    assetsQQNormPlot(myAssets)
+    assetsQQNormPlot(myAssets, which = 2, labels = FALSE, ylim = c(-50, 50))
+    ###
+
+
+################################################################################
+# Mahalanobis
+
+    # Data:
+    data(berndtInvest)
+    units = c("CONTIL", "MOBIL", "TEXACO", "GERBER")
+    myAssets = as.timeSeries(berndtInvest)[, units]
+    
+    
+    par (mfrow = c(2, 2), cex = 0.7)
+    
+    MV = assetsMeanCov(myAssets, "mcd")
+    X = mahalanobis(as.matrix(myAssets), center = MV$mu, cov = MV$Omega)
+    plot(X, type = "h", ylim = c(0, 200), pch = 19)
+    # points(X, pch=19, cex = 0.5, col = "steelblue")
+    
+    MV = assetsMeanCov(as.matrix(myAssets), "cov")
+    X = mahalanobis(as.matrix(myAssets), center = MV$mu, cov = MV$Sigma)
+    plot(X, type = "h", ylim = c(0, 200), pch = 19)
+    # points(X, pch=19, cex = 0.5, col = "red")
+    
+    
+   
+
+
+
+
+assetsBeta =
+function(x,  columns = NULL,  method = c("lm", "lms", "lts"))
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Estimation of Beta using classical regression or alternatively
+    #   regression to the good points in the dataset, thereby achieving 
+    #   a regression estimator with a high breakdown point.
+    
+    # Details:
+    #   Uses one of the functions lm() lmsreg() or ltsreg().
+    
+    # Example:
+    #   x = yahooSeries(symbols = c("^DJI", "IBM"), getReturns = TRUE)
+    #   assetsBeta(x, columns = c("^DJI.Close", "IBM.Close"))
+    #   assetsBeta(x)
+    
+    # FUNCTION:
+    
+    # Settings:
+    method = match.arg(method)
+    
+    # Columns
+    if (is.null(columns)) {
+        X = as.data.frame(x[, c(1, 2)])
+    } else {
+        X = as.data.frame(x[, columns])
+    }    
+    
+    # Allow formula expression for colnames with ^ prefix:
+    colnames(X) = UNITS = gsub("\\^", "", colnames(X))
+    
+    # Fit:
+    fun = c(lm = "lm", lms = "lmsreg", lts = "ltsreg")
+    FUN = match.fun(fun[method])
+    fit = FUN(formula = as.formula(paste(UNITS[1], "~", UNITS[2])), data = X)
+    fit$call = match.call()
+    Beta = c(Beta = fit$coef[2])
+    
+    # Add Control Attroibute:
+    attr(Beta, "control") = c(method = method)
+    
+    # Return Value:
+    Beta
+}
+
+
+
+
+assetsBeta(x)
+
+
+    # Data:
+    EVST = yahooSeries(symbol = "EVST", from = "1996-12-01", to = "2001-12-31", 
+        quote = "Close", aggregation = "m", getReturn = TRUE)
+    plot(EVST, type = "o")
+    
+    EVST@Data[EVST@Data>2, ] = 6.88
+    plot(EVST, type = "o", pch = 19, cex = 0.7, main = "EVST Returns",
+        ylab = "RETURNS", xlab = "YEAR")
+  
+
+    mean(EVST@Data)
+    sd(EVST@Data)
+    
+    EVST[EVST@Data>3.3, ]
+    
+    1-pnorm(max(EVST@Data), mean(EVST@Data), sd(EVST@Data))
+    pnorm(max(EVST@Data), mean(EVST@Data), sd(EVST@Data), lower.tail = FALSE)
+    
+    
+    qqnorm(EVST@Data, pch = 19, cex = 0.7)
+    qqline(EVST@Data)
+    grid()
+    
+    qqnorm(EVST@Data[EVST@Data < 2], pch = 19, cex = 0.7)
+    qqline(EVST@Data)
+    grid()
+    
+    median(EVST@Data)
+    mean(EVST@Data, trim = 0.1)
+    mean(EVST@Data[EVST@Data < 2])
+    
+    
+    
+    
+    # Chapter 6.4
+    
+    ZIF = yahooSeries(symbol = "ZIF", from = "1996-12-01", to = "2001-12-31", 
+        quote = "Close", aggregation = "m", getReturn = TRUE)
+    plot(ZIF, type = "o", pch = 19)
+    
+    
+    
+    assetsHistPlot(ZIF, labels = FALSE, xlim = c(-0.3, 0.3), n = 20)
+    
+    assetsMeanCov(ZIF, method = "cov")
+    assetsMeanCov(ZIF, method = "mve")
+    assetsMeanCov(ZIF, method = "mcd")
+
+    
+################################################################################
+# 6.5 Robust Betas 
+
+
+
