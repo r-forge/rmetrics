6147fdc8489913871b499901596c4b87037a7098
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-07-18 10:15:56 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@394 5d4a7380-5c0c-0410-b446-d4a5018c0119

---
+++ pkg/fCopulae/R/065A-ArchmaxCopulae.R
@@ -0,0 +1,309 @@
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General 
+# Public License along with this library; if not, write to the 
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# MA  02111-1307  USA
+
+# Copyrights (C)
+# for this R-port: 
+#   1999 - 2006, Diethelm Wuertz, GPL
+#   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
+#   info@rmetrics.org
+#   www.rmetrics.org
+# for the code accessed (or partly included) from other R-ports:
+#   see R's copyright and license files
+# for the code accessed (or partly included) from contributed R-ports
+# and other sources
+#   see Rmetrics's copyright file
+
+
+################################################################################
+#                       X     NOT YET IMPEMENTED
+# FUNCTION:                  ARCHMAX COPULAE PARAMETER:
+#  .archmaxParam        X     Sets parameters for an Archmax copula
+#  .archmaxRange        X     Returns the range of valid parameter values
+#  .archmaxCheck        X     Checks if the parameters are in the valid range
+# FUNCTION                   KENDALL'S TAU AND SPEARMAN'S RHO:
+#  archmaxTau           X      Returns Kendall's tau for Archmax copulae
+#  archmaxRho           X      Returns Spearman's rho for Archmax copulae
+# FUNCTION:                  ARCHMAX COPULAE TAIL DEPENDENCE:
+#  archmaxTailCoeff     X     Computes tail dependence for Archmax copulae
+#  archmaxTailPlot      X     Plots Archmax tail dependence function
+# FUNCTION:                  ARCHMAX COPULAE RANDOM VARIATES:
+#  rarchmaxCopula       X     Generates Archmax copula random variates 
+#  rarchmaxSlider       X     Generates interactive plots of random variates
+# FUNCTION:                  ARCHMAX COPULAE PROBABILITY:
+#  pellipticalCopula    X     Computes Archmax copula probability
+#  pellipticalSlider    X     Generates interactive plots of probability
+# FUNCTION:                  ARCHMAX COPULAE DENSITY:
+#  dellipticalCopula    X     Computes Archmax copula density 
+#  dellipticalSlider    X     Generates interactive plots of Archmax density
+# FUNCTION:                  ARCHIMEDEAN COPULA PARAMETER FITTING:
+#  archmaxCopulaSim     X     Simulates bivariate extreme value copula
+#  archmaxCopulaFit     X     Fits the paramter of an extreme value copula
+################################################################################
+
+
+.archmaxParam =
+function()
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+.archmaxRange =
+function()
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+.archmaxCheck =
+function()
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+################################################################################
+
+
+rarchmaxCopula =
+function(n, param = NULL, type = NULL)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+parchmaxCopula =
+function(u = 0.5, v = u, param = NULL, type = NULL, 
+output = c("vector", "list"))
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+darchmaxCopula =
+function(u = 0.5, v = u, param = NULL, type = NULL, 
+output = c("vector", "list"))   
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+rarchmaxSlider =
+function(B = 10)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+parchmaxSlider =
+function(type = c("persp", "contour"), B = 10)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+darchmaxSlider =
+function(type = c("persp", "contour"), B = 10)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+################################################################################
+
+
+archmaxTau =
+function()
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+archmaxRho =
+function()
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+################################################################################
+
+
+archmaxTailPlot =
+function(tail = c("Upper", "Lower"))
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+################################################################################
+
+
+archmaxCopulaSim =
+function(n)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+archmaxCopulaFit =
+function(u, v = NULL, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    
+    # Argumentes:
+    
+    # FUNCTION:
+    
+    # Return Value:
+    print("Sorry, Not Yet Implemented!")
+}
+
+
+################################################################################
+
