b4002c40644ae6e3f6c3b4475a02826015da4868
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-07-25 12:11:42 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@502 5d4a7380-5c0c-0410-b446-d4a5018c0119

---
+++ pkg/fBasics/test/runit1A.R
@@ -0,0 +1,218 @@
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU Library General Public License for more details.
+#
+# You should have received A copy of the GNU Library General 
+# Public License along with this library; if not, write to the 
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# MA  02111-1307  USA
+
+# Copyrights (C)
+# for this R-port: 
+#   1999 - 2004, Diethelm Wuertz, GPL
+#   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
+#   info@rmetrics.org
+#   www.rmetrics.org
+# for the code accessed (or partly included) from other R-ports:
+#   see R's copyright and license files
+# for the code accessed (or partly included) from contributed R-ports
+# and other sources
+#   see Rmetrics's copyright file
+
+
+################################################################################
+# FUNCTION:                    DESCRIPTION
+#  log                           log has become a generic function
+#  log.default                   log default method
+#  round                         round has become a generic function
+#  round.default                 round default method
+#  sample                        sample has become a generic function
+#  sample.default                sample default method
+#  sort                          sort has become a generic function
+#  sort.default                  sort default method
+#  var                           var has become a generic function
+#  var.default                   var default method
+# FUNCTION:                     DESCRIPTION:
+#  "rownames<-"                  rownames<- has become a generic function
+#  "rownames<-.default"          rownames<- default method
+#  "colnames<-"                  colnames<- has become a generic function
+#  "colnames<-.default"          colnames<- default method
+# FUNCTION:                     DESCRIPTION:
+#  as.matrix.ts                  Converts univariate ts to 1-column matrix
+#  as.matrix.mts                 Converts multivariate ts to matrix
+# FUNCTION:                     DESCRIPTION:
+#  .description                  Sets default description string
+#  .unirootNA                    Computes zero without error exit    
+#  .interp                       Does Akima Spline Interpolation
+# FUNCTION:                     DESCRIPTION:
+#  modify                        Modifies a 'timeSeries' object
+#  modify.default                Default Method
+#  atoms                         Extracts atoms from 'timeSeries' object
+#  atoms.default                 Default Method
+# FUNCTION:                     DESCRIPTION:
+#  .datax                        Loads timeSeries objects from demo files
+# FUNCTION/VALUE:               DESCRIPTION: 
+#  currentYear                   Sets date of the current year
+#  .currentYear                  Sets date of the current year
+#  myUnits                       Sets date units
+# DATA:                         DATA:
+#  MSFT                          Microsoft data set from Yahoo
+# FUNCTION:                     DESCRIPTION [REQUIRES DATE]:
+#  .fjulian                      Transform formatted dates to julian day numbers
+#  .julian                       Implements SPlus like 'julian'
+################################################################################
+
+
+test.helpFile = 
+function()
+{
+    # Help File:
+    helpFile = function() { 
+        example(ISO8601Standard); return() }
+    checkIdentical(
+        target = class(try(helpFile())),
+        current = "NULL")
+
+    # Return Value:
+    return()    
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+test.genfun =
+function()
+{
+    # Time Series:
+    MSFT.TS = as.timeSeries(MSFT)
+    print(head(log(MSFT.TS)))
+    print(tail(log(MSFT.TS)))
+    MEAN = mean(MSFT.TS@Data)
+    checkEquals(
+        target = MEAN, 
+        current = 8628750, 
+        tolerance = 1)
+    
+    # Return Value:
+    return() 
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+test.interp = 
+function()
+{
+    akima = list(
+    x = c(
+    11.16, 24.20, 12.85, 19.85, 10.35, 24.65, 19.72, 15.91,  0.00, 20.87, 
+     6.71,  3.45, 19.99, 14.26, 10.28,  4.51, 17.43, 22.80,  0.00,  7.58, 
+    16.70,  6.08,  1.99, 25.00, 14.90,  3.22,  0.00,  9.66,  2.56,  5.22, 
+    11.77, 17.25, 15.10, 25.00, 12.13, 25.00, 22.33, 11.52, 14.59, 15.20,  
+     7.54,  5.23, 17.32,  2.14,  0.51, 22.69, 25.00,  5.47, 21.67,  3.31),
+    y = c(
+     1.24, 16.23,  3.06, 10.72,  4.11,  2.40,  1.39,  7.74, 20.00, 20.00, 
+     6.26, 12.78,  4.62, 17.87, 15.16, 20.00,  3.46, 12.39,  4.48,  1.98, 
+    19.65,  4.58,  5.60, 11.87,  3.12, 16.78,  0.00, 20.00,  3.02, 14.66, 
+    10.47, 19.57, 17.19,  3.87, 10.79,  0.00,  6.21,  8.53,  8.71,  0.00, 
+    10.69, 10.72, 13.78, 15.03,  8.37, 19.63, 20.00, 17.13, 14.36,  0.13),
+    z = c(
+    22.15,  2.83, 22.11,  7.97, 22.33, 10.25, 16.83, 15.30, 34.60,  7.54,
+    30.97, 41.24, 14.72, 10.74, 21.59, 15.61, 18.60,  5.47, 61.77, 29.87,  
+     6.31, 35.74, 51.81,  4.40, 21.70, 39.93, 58.20,  4.73, 50.55, 40.36, 
+    13.62,  6.43, 12.57,  8.74, 13.71, 12.00, 10.25, 15.74, 14.81, 21.60, 
+    19.31, 26.50, 12.11, 53.10, 49.43,  3.25,  0.60, 28.63,  5.52, 44.08))
+
+    # Interpolation:
+    akima.lin = .interp(akima$x, akima$y, akima$z)
+    Z = mean(akima.lin$z)
+    checkEquals(
+        target = Z, 
+        current = 21.70316, 
+        tolerance = 0.00001)
+    
+    # Plots:
+    image  (akima.lin, add = FALSE)
+    contour(akima.lin, add = TRUE)
+    points (akima, pch = 19)
+    
+    # Return Value:
+    return()    
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+test.julian =
+function()
+{
+    # Requirement:
+    require(date)
+
+    # .fjulian -
+    fdates = c("8/11/73", "08-11-73", "August 11 1973", "Aug11/73")
+    FJ = .fjulian(fdates) 
+    print(FJ)
+    checkIdentical(
+        target = class(FJ), 
+        current = "numeric")
+    checkIdentical(
+        target = FJ, 
+        current = c(4971, 4971, 4971, 4971))
+    
+    # .fjulian -
+    fdates = c("11/8/73", "11-08-73", "11 August 1973", "11Aug73")
+    FJ = .fjulian(fdates, order = 'dmy')
+    print(FJ) 
+    checkIdentical(
+        target = class(FJ), 
+        current = "numeric")
+    checkIdentical(
+        target = FJ, 
+        current = c(4971, 4971, 4971, 4971))
+    
+    # .julian - 
+    # day.of.week -
+    # The number of days from January 1, 1990 to each of:
+    # January 15, 1990, February 15, 1991, March 15, 1992, etc.
+    JULIAN = .julian(1:12, rep(15,12), 1990+(0:11), origin = c(1, 1, 1990))
+    print(JULIAN)
+    COUNTS = c(14,410,804,1200,1595,1991,2387,2783,3179,3574,3971,4366)
+    checkIdentical(
+        target = JULIAN, 
+        current = COUNTS)
+    # November 12, 98, was a Wednesday.
+    DOW = .day.of.week(m = 11, d = 12, y = 98)
+    print(DOW)
+    COUNT = 3
+    checkIdentical(
+        target = DOW, 
+        current = COUNT)
+   
+    # Return Value:
+    return() 
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+if (FALSE) {
+    require(RUnit)
+    testResult <- runTestFile("C:/Rmetrics/SVN/trunk/fCalendar/test/runit0A.R")
+    printTextProtocol(testResult)
+}
+
+
+# ------------------------------------------------------------------------------
+
