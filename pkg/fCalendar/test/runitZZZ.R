696cc583e93b5d69e253466ebad1cc667c606d9d
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-10-07 08:01:09 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@761 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fCalendar/test/runitZZZ.R
+++ pkg/fCalendar/test/runitZZZ.R
@@ -31,9 +31,11 @@
 
 
 if (FALSE) {
+    require(fCalendar)
     require(RUnit)
     testIndex = c("1A", "2A", "3A", "3B", "3C", "3E", "4", "4B", "4C",
-        "4D", "5A", "5B", "6A")#, "Demo")
+        "4D", "5A", "5B", "6A")
+    # testIndex = c(testIndex, "Demo")
     File = "C:/Rmetrics/SVN/trunk/fCalendar/test/runit"
     Protocol = "runitCalendar.txt"
     write("fCalendar:", file = Protocol)
