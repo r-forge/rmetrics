10f7f7e328f3303f9e3aacf49002c56b4064b26a
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-12-20 15:34:56 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@923 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fEcofin/test/runit2B.R
+++ pkg/fEcofin/test/runit2B.R
@@ -28,7 +28,13 @@
 
 
 ################################################################################
-# WFE
+# FUNCTION CALL:    DESCRIPTION:
+#  data(wfe1)        Market capitalization of domestic companies 
+#  data(wfe2)        Total number of companies with shares listed 
+#  data(wfe3)        Total value of share trading 
+#  data(wfe4)        Market value of bonds listed 
+#  data(wfe5)        Total value of bond trading 
+#  data(wfe6)        Price earning ratio an gross dividend yield 
 ################################################################################
 
 
