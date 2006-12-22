04d5bbc7ee1668ffd83a7764deaccdebd94808ff
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-12-22 11:07:49 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@943 5d4a7380-5c0c-0410-b446-d4a5018c0119

---
+++ pkg/fEcofin/R/2C-WFEStatistics.R
@@ -0,0 +1,39 @@
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General 
+# Public License along with this library; if not, write to the 
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# MA  02111-1307  USA
+
+# Copyrights (C)
+# for this R-port: 
+#   1999 - 2006, Diethelm Wuertz, GPL
+#   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
+#   info@rmetrics.org
+#   www.rmetrics.org
+# for the code accessed (or partly included) from other R-ports:
+#   see R's copyright and license files
+# for the code accessed (or partly included) from contributed R-ports
+# and other sources
+#   see Rmetrics's copyright file
+
+
+################################################################################
+# FUNCTION CALL:    DESCRIPTION:
+#  data(wfe1)        Market capitalization of domestic companies 
+#  data(wfe2)        Total number of companies with shares listed 
+#  data(wfe3)        Total value of share trading 
+#  data(wfe4)        Market value of bonds listed 
+#  data(wfe5)        Total value of bond trading 
+#  data(wfe6)        Price earning ratio an gross dividend yield 
+################################################################################
+
