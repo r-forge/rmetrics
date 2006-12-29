17e019235986eaee8fcf28d24feaf60f8fbb33c9
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-12-29 00:03:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@1039 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fEcofin/R/4E-McKinnonPValues.R
+++ pkg/fEcofin/R/4E-McKinnonPValues.R
@@ -337,7 +337,7 @@ function (arg, nobs = 0, niv = 1, itt = 1, itv = 1, nc = 1)
                     as.double(wght), as.double(cval), as.double(size), 
                     as.double(precrt), as.integer(nobs), as.integer(model), 
                     as.integer(nreg), as.integer(np), as.integer(0),
-                    PACKAGE = "fSeries") 
+                    PACKAGE = "fEcofin") 
                 ans = c(ans, result[[5]]) 
             } 
         }
@@ -347,7 +347,7 @@ function (arg, nobs = 0, niv = 1, itt = 1, itv = 1, nc = 1)
                 as.double(probs), as.double(pval), as.double(stat), 
                 as.double(precrt), as.integer(nobs), as.integer(model), 
                 as.integer(nreg), as.integer(np), as.integer(0),
-                PACKAGE = "fSeries") 
+                PACKAGE = "fEcofin") 
             ans = c(ans, result[[5]]) 
         } 
     }
