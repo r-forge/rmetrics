e6bf40e5b978285a14b56ecf3a23c04127cb2a68
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2006-12-29 09:01:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@1055 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fEcofin/R/3A-BasicExtensions.R
+++ pkg/fEcofin/R/3A-BasicExtensions.R
@@ -53,6 +53,11 @@
 #  modify.default                Default Method
 #  atoms                         Extracts atoms from 'timeSeries' object
 #  atoms.default                 Default Method
+# FUNCTION:                     DESCRIPTION:
+#  as.POSIXlt                    Converts objects of class POSIXlt
+#  as.POSIXlt.default            Default Method
+#  as.matrix.ts                  Converts univariate ts to 1-column matrix
+#  as.matrix.mts                 Converts multivariate ts to matrix
 ################################################################################
 
 
@@ -573,6 +578,115 @@ function(x, ...)
     
     # Return Value:
     invisible(x)
+}
+
+################################################################################
+
+
+as.POSIXlt = 
+function(x, tz = "")
+{
+    # FUNCTION:
+    
+    # Return Value:
+    UseMethod("as.POSIXlt")
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+as.POSIXlt.default =
+function (x, tz = "") 
+{   
+    # FUNCTION:
+    
+    # As Posix:
+    fromchar <- function(x) {
+        xx <- x[1]
+        if (is.na(xx)) {
+            j <- 1
+            while (is.na(xx) && (j <- j + 1) <= length(x)) xx <- x[j]
+            if (is.na(xx)) 
+                f <- "%Y-%m-%d"
+        }
+        if (is.na(xx) || !is.na(strptime(xx, f <- "%Y-%m-%d %H:%M:%S")) || 
+            !is.na(strptime(xx, f <- "%Y/%m/%d %H:%M:%S")) || 
+            !is.na(strptime(xx, f <- "%Y-%m-%d %H:%M")) || !is.na(strptime(xx, 
+            f <- "%Y/%m/%d %H:%M")) || !is.na(strptime(xx, f <- "%Y-%m-%d")) || 
+            !is.na(strptime(xx, f <- "%Y/%m/%d"))) {
+            res <- strptime(x, f)
+            if (nchar(tz)) 
+                attr(res, "tzone") <- tz
+            return(res)
+        }
+        stop("character string is not in a standard unambiguous format")
+    }
+    if (inherits(x, "POSIXlt")) 
+        return(x)
+    if (inherits(x, "Date")) 
+        return(.Internal(Date2POSIXlt(x)))
+    tzone <- attr(x, "tzone")
+    if (inherits(x, "date") || inherits(x, "dates")) 
+        x <- as.POSIXct(x)
+    if (is.character(x)) 
+        return(fromchar(unclass(x)))
+    if (is.factor(x)) 
+        return(fromchar(as.character(x)))
+    if (is.logical(x) && all(is.na(x))) 
+        x <- as.POSIXct.default(x)
+    if (!inherits(x, "POSIXct")) 
+        stop(gettextf("do not know how to convert '%s' to class \"POSIXlt\"", 
+            deparse(substitute(x))))
+    if (missing(tz) && !is.null(tzone)) 
+        tz <- tzone[1]
+    .Internal(as.POSIXlt(x, tz))
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+as.matrix.ts = 
+function(x) 
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Coerces a "ts" object into a matrix
+    
+    # FUNCTION:
+    
+    # Transform: 
+    ans = as.matrix.default(unclass(x)) 
+    attr(ans, "tsp")<-NULL
+    rownames(ans)<-NULL
+    colnames(ans)<-NULL
+    
+    # Return Value:
+    ans
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+as.matrix.mts = 
+function(x) 
+{   # A function implemented by Diethelm Wuertz
+  
+    # Description:
+    #   Coerces a multivariate "ts" object into a matrix
+    
+    # FUNCTION:
+    
+    # Transform: 
+    ans = as.matrix.default(unclass(x)) 
+    attr(ans, "tsp")<-NULL
+    rownames(ans)<-NULL
+    colnames(ans)<-NULL
+    
+    # Return Value:
+    ans
 }
 
 
