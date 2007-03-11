38987feacef361c16a8d87493c37dc467ddf9467
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2007-03-11 13:59:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@1305 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fEcofin/R/0A-fEcofinUtilities.R
+++ pkg/fEcofin/R/0A-fEcofinUtilities.R
@@ -335,29 +335,51 @@ function(x, y, method = c("scatter", "hist"), ...)
 
 
 .circlesPlot = 
-function(x, y, size = 1, ...)
-{   # A function implemented by GKS
+function(x, y = NULL, z = NULL, scale = 1, points = TRUE, 
+labels = TRUE, ...)
+{   # A function implemented by Diethelm Wuertz
     
     # Description:  
-    #   Creates a scatterplot with circle size indexing a 
-    #   third variable.
+    #   Creates a scatterplot with circle size as third variable
     
     # Example:
-    #   .circlesPlot(x = rnorm(50), y = rnorm(50), size = abs(rnorm(50)))
+    #   circlesPlot(x=rnorm(50), y=rnorm(50), z=rnorm(50))
+    #   circlesPlot(x=rnorm(50), y=rnorm(50), z=rnorm(50), labels= FALSE)
  
     # FUNCTION:
     
-    # Settings:
-    stopifnot(length(y) == length(x))
-    if (length(size) != length(x))
-    size = rep(size[1], times = length(x))
+    # Transfor Input:
+    if (is.list(x)) x = matrix(unlist(x), ncol = 3)
+    if (is.data.frame(x)) x = as.matrix.data.frame(x)
+    if (is.matrix(x)) {
+        z = x[, 3]
+        y = x[, 2]
+        x = x[, 1]
+    }
+    nX = length(x)
+    nY = length(y)
+    # nZ = length(z)
+    stopifnot(nX == nY)
+    # stopifnot(nX == nZ || nX*nY == nZ)
     
-    # Circle Plot:
-    plot(x, y, type = "n", ...)
-    symbols(x, y, add = TRUE, circles = sqrt(size), inches = 0.25, ...)
+    # Create Circle Plot:
+    if (labels) {
+        plot(x, y, type = "n")
+    } else {
+        plot(x, y, xlab = "", ylab = "", type = "n")
+    }
+    symbols(x, y, add = TRUE, circles = abs(z)^scale, inches = 0.25, 
+        fg = "black", bg = "steelblue", ...) 
+    X = x[z < 0]
+    Y = y[z < 0]
+    Z = z[z < 0]
+    symbols(X, Y, add = TRUE, circles = abs(Z)^scale, inches = 0.25, 
+        fg = "black", bg = "orange", ...)
+    if (points) points(x, y, pch = 19)
+    grid()
     
     # Return Value:
-    invisible(data.frame(x = x, y = y, size = size))
+    invisible(NULL)
 }
 
 
