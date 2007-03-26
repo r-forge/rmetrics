5a4b6bd1e405812eaa97d162e7f3946114386a34
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2007-03-26 19:30:37 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@1456 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fPortfolio/R/1D-AssetsTests.R
+++ pkg/fPortfolio/R/1D-AssetsTests.R
@@ -78,6 +78,9 @@ function(x, Replicates = 99, title = NULL, description = NULL)
 {
     # Description:
     
+    # Note:
+    #   Requires Contributed R package energy
+    
     # Example:
     #   .mvenergyTest(x = assetsSim(100), 99)
     
@@ -88,7 +91,7 @@ function(x, Replicates = 99, title = NULL, description = NULL)
     x = as.matrix(x)
     
     # Test:
-    test = .mvnorm.etest(x = x, R = Replicates)
+    test = mvnorm.etest(x = x, R = Replicates)
     names(test$p.value) = ""
     class(test) = "list"
     
@@ -103,186 +106,6 @@ function(x, Replicates = 99, title = NULL, description = NULL)
         test = test,
         title = title,
         description = description)
-}
-
-
-################################################################################
-# Package: energy
-# Title: E-statistics (energy statistics) tests of fit, independence, 
-#   clustering
-# Version: 1.0-3
-# Date: 2005-10-02
-# Author: Maria L. Rizzo <rizzo@math.ohiou.edu> and 
-#   Gabor J. Szekely <gabors@bgnet.bgsu.edu>
-# Description: E-statistics (energy) tests for comparing distributions: 
-#   multivariate normality, multivariate k-sample test for equal 
-#   distributions, hierarchical clustering by e-distances, multivariate 
-#   independence test, Poisson test. Energy-statistics concept based on 
-#   a generalization of Newton's potential energy is due to Gabor J. Szekely.
-# Maintainer: Maria Rizzo <rizzo@math.ohiou.edu>
-# License: GPL 2.0 or later
-# Packaged: Sun Oct  2 16:55:19 2005; rizzo
-
-
-.mvnorm.etest = 
-function(x, R) 
-{
-    # Description:
-    #   Parametric bootstrap E-test for multivariate normality
-    
-    # Author: 
-    #   Maria L. Rizzo <rizzo@math.ohiou.edu> and 
-    #   Gabor J. Szekely <gabors@bgnet.bgsu.edu>
-
-    # FUNCTION:
-    
-    # Test:
-    if (is.vector(x)) {
-        n = length(x)
-        d = 1
-        bootobj = .mvnormBoot(x, statistic = .normal.e, R = R,  
-            ran.gen = function(x, y) {return(rnorm(n)) })
-    } else {
-        n = nrow(x)
-        d = ncol(x)
-        bootobj = .mvnormBoot(x, statistic = .mvnorm.e, R = R,  
-            ran.gen = function(x, y) { return(matrix(rnorm(n * d), 
-            nrow = n, ncol = d)) })
-    }
-    p = 1 - mean(bootobj$t < bootobj$t0)
-    names(bootobj$t0) = "E-statistic"
-    e = list(statistic = bootobj$t0,
-        p.value = p,
-        method = "Energy Test of Multivariate Normality",
-        data.name = paste("x, sample size ", n, ", dimension ", 
-            d, ", replicates ", R, sep = ""))
-    class(e) = "htest"    
-    
-    # Return Value:    
-    e                 
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-.mvnorm.e = 
-function(x) 
-{
-    # Description:
-    #   E-statistic for multivariate normality
-    
-    # Author: 
-    #   Maria L. Rizzo <rizzo@math.ohiou.edu> and 
-    #   Gabor J. Szekely <gabors@bgnet.bgsu.edu>
-    
-    # FUNCTION:
-    
-    # Statistic:
-    if (is.vector(x)) return(normal.e(x))
-    n = nrow(x)
-    d = ncol(x)
-    if (n < 2) return(normal.e(x))
-    # subtract column means and
-    z = scale(x, scale = FALSE) 
-    # compute S^(-1/2)    
-    ev = eigen(var(x), symmetric = TRUE)    
-    P = ev$vectors
-    lambda = ev$values    
-    y = z %*% (P %*% diag(1 / sqrt(lambda)) %*% t(P))
-    if (any(!is.finite(y))) return (NA)
-    stat = 0
-    e = .C("mvnEstat", y = as.double(t(y)), byrow = as.integer(TRUE),
-        nobs = as.integer(n), dim = as.integer(d), 
-        stat = as.double(stat), PACKAGE = "fPortfolio")$stat
-            
-    # Return Value:
-    e
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-.normal.e = 
-function(x) 
-{
-    # Description:
-    #   Statistic for univariate Normality
-    
-    # Authors: 
-    #   Maria L. Rizzo <rizzo@math.ohiou.edu> and 
-    #   Gabor J. Szekely <gabors@bgnet.bgsu.edu>
-    
-    # FUNCTION:
-    
-    # Statistic:
-    x = as.vector(x)
-    y = sort(x)
-    n = length(y)
-    if (y[1] == y[n]) return (NA)
-    y = scale(y) 
-    K = seq(1 - n, n - 1, 2)
-    e = 2 * (sum(2 * y * pnorm(y) + 2 * dnorm(y)) - n / sqrt(pi) - mean(K * y))
-   
-    # Return Value:
-    e
-}
-
-
-################################################################################
-# Package: boot
-# Priority: recommended
-# Version: 1.2-24
-# Date: 2005-12-09
-# Author: S original <http://statwww.epfl.ch/davison/BMA/library.html>
-#   by Angelo Canty <cantya@mcmaster.ca>.  
-#   R port by  Brian Ripley <ripley@stats.ox.ac.uk>.
-# Maintainer: Brian Ripley <ripley@stats.ox.ac.uk>
-# Description: functions and datasets for bootstrapping from the
-#   book "Bootstrap Methods and Their Applications" by A. C. Davison and 
-#   D. V. Hinkley (1997, CUP).
-# Title: Bootstrap R (S-Plus) Functions (Canty)
-# Depends: R (>= 2.0.0), graphics, stats
-# Suggests: survival
-# LazyData: yes
-# License: Unlimited distribution.
-# Packaged: Thu Dec  8 21:19:17 2005; ripley
-
-
-.mvnormBoot = 
-function(data, statistic, R, strata = rep(1, n), L = NULL, m = 0, 
-weights = NULL, ran.gen=function(d, p) d, mle = NULL, ...)
-{    
-    # Author: 
-    #   S original <http://statwww.epfl.ch/davison/BMA/library.html>
-    #   by Angelo Canty <cantya@mcmaster.ca>  
-    #   R port by  Brian Ripley <ripley@stats.ox.ac.uk>
-
-    # R replicates of bootstrap applied to  statistic(data)
-    # Various auxilliary functions find the indices to be used for the
-    # bootstrap replicates and then this function loops over those replicates.
-     
-    isBootMatrix = function(x) {length(dim(x)) == 2}
-    call = match.call()
-    if (!exists(".Random.seed", envir=.GlobalEnv, inherits = FALSE)) runif(1)
-    seed = get(".Random.seed", envir=.GlobalEnv, inherits = FALSE)
-    if (isBootMatrix(data)) n = nrow(data) else n = length(data)
-    temp.str = strata
-    strata = tapply(1:n,as.numeric(strata))
-    if ((n == 0) || is.null(n)) stop("no data in call to boot")  
-    t0 = statistic(data,...)
-    lt0 = length(t0)
-    t.star = matrix(NA, sum(R), lt0)
-    pred.i = NULL
-    for(r in 1:R) t.star[r,] = statistic(ran.gen(data, mle),...)      
-    dimnames(t.star) = NULL
-    if (is.null(weights)) weights = 1/tabulate(strata)[strata]
-    out = list(t0 = t0, t = t.star, R = R, data = data, seed = seed,
-        statistic = statistic, call = call)
-     out = c(out, list(ran.gen = ran.gen, mle=mle) )
-    class(out) = "boot"
-    out
 }
 
   
@@ -322,12 +145,12 @@ function(x, title = NULL, description = NULL)
     rng = rng[2] - rng[1]
     if (rng == 0)
     stop("all `U[]' are identical")
-    Us = apply(U,1,mean)
+    Us = apply(U, 1, mean)
     R = U-Us
-    M.1 = solve(R%*%t(R),tol=1e-18)
-    Rmax = diag(t(R)%*%M.1%*%R)
-    C = M.1%*%R[,which.max(Rmax)]
-    Z = t(C)%*%U
+    M.1 = solve(R %*% t(R), tol = 1e-18)
+    Rmax = diag(t(R) %*% M.1 %*% R)
+    C = M.1 %*% R[, which.max(Rmax)]
+    Z = t(C) %*% U
     test = shapiro.test(Z)
     names(test$p.value) = ""
     class(test) = "list"
--- pkg/fPortfolio/test/runit1C.R
+++ pkg/fPortfolio/test/runit1C.R
@@ -55,9 +55,61 @@ function()
 # ------------------------------------------------------------------------------
 
 
-test. =
+test.assetsSelectHClust =
 function()
 { 
+    # Hierarchical Clustering:
+    # Select the 4 most dissimilar assets from Berndt's data set
+
+    # The data set "berndtInvest" is from Berndt's textbook 
+    # "The Practice of Econometrics". It is a data.frame consisting
+    # of 18 columns with the following entries:
+    #  [1] %d/%B/%y "CITCRP" "CONED"  "CONTIL" "DATGEN" "DEC"      
+    #  [7] "DELTA"  "GENMIL" "GERBER" "IBM"    "MARKET" "MOBIL"    
+    # [13] "PANAM"  "PSNH"   "TANDY"  "TEXACO" "WEYER"  "RKFREE"  
+    # The first column holds the date, the 11th the market rate,
+    # and the last (the 18th) the risk free rate.
+        
+    # Load the Data and Create an Object of Class 'timeSeries':
+    data(berndtInvest)
+    berndtInvest = as.timeSeries(berndtInvest)
+    class(berndtInvest)
+    head(berndtInvest)
+        
+    # Exclude the Date, Market Returns and Interest Rate Columns 
+    # from the data frame, then multiply by 100 for percentual returns ...
+    allAssets = 100 * berndtInvest[, -c(1, 10, 17)]
+    class(allAssets)
+    head(allAssets)
+         
+    # Graph Frame:
+    par(mfrow = c(2, 1), cex = 0.7)   
+    
+    # Select the "n" Most Dissimilar Assets from 
+    # Hierarchical Clustering:
+    n = 4
+    args(assetsSelect)
+    clustered = assetsSelect(allAssets, doplot = TRUE)
+    # Create my Assets Set from the "n" selected Symbols:
+    myAssets = allAssets[, c(clustered$order[1:n])]
+    colnames(myAssets)
+    # Print the Column Return:
+    mu.vec = colAvgs(myAssets)
+    mu.vec
+    # or ...
+    mu.vec = colMeans(myAssets@Data)
+    mu.vec
+    # Print the Covariance Matrix:
+    cov.mat = cov(myAssets@Data)
+    cov.mat
+  
+    # Plot Cumulated Returns of the Assets:
+    ts.plot(colCumsums(myAssets), col = 1:4)
+    grid()
+    legend(0, 250, legend = colnames(myAssets), pch = "----", col = 1:4)
+    title(main = "Cumulated Returns", ylab = "Cumulated Returns")
+    abline(h = 0, lty = 3)
+    
     # Return Value:
     return()
 }
@@ -66,9 +118,22 @@ function()
 # ------------------------------------------------------------------------------
 
 
-test. =
+test.assetsSelectKMeans =
 function()
 { 
+    # K-Means Clustering:
+
+    # Load Data
+    berndtInvest = as.timeSeries(data(berndtInvest))
+    allAssets = 100 * berndtInvest[, -c(1, 10, 17)]
+    allAssets = as.matrix(allAssets)
+    head(allAssets)
+         
+    # assetsSelect(x, method = c("hclust", "kmeans"), kmeans.centers = 5, 
+    #   kmeans.maxiter = 10, doplot = TRUE, ...) 
+    clustered = assetsSelect(t(allAssets), method = "kmeans",
+        kmeans.centers = 4, doplot = TRUE)
+    
     # Return Value:
     return()
 }
@@ -77,119 +142,90 @@ function()
 # ------------------------------------------------------------------------------
 
 
-test. =
+test.assetsSelectKMeans =
 function()
 { 
-    # Return Value:
-    return()
-}
+    require(cluster)
+        
+        
+    .assetsSelect = 
+    function (x, k, 
+    method = c("hclust", "kmeans", "agnes", "diana", "pam", "clara"), 
+    doplot = TRUE, control = FALSE, ...) 
+    {
+        # Settings:
+        X = as.matrix(x)
+        method = match.arg(method)
+        
+        # Hierarchical Clustering:
+        if (method == "hclust") {
+            ans = hclust(dist(t(X)), ...)
+            index = rev(ans$order)[1:k]
+            if (doplot) plot(ans)
+        }
+        if (method == "agnes") {
+            ans = agnes(t(X), ...)
+            index = rev(ans$order)[1:k]
+            if (doplot) plot(ans)
+        }
+        if (method == "diana") {
+            ans = diana(t(X), ...)
+            index = rev(ans$order)[1:k]
+            if (doplot) plot(ans)
+        }
+        
+        # K-Means Clustering:
+        if (method == "kmeans") {
+            ans = kmeans(x = X, centers = k, ...)
+            Dist = rep(Inf, times = k)
+            index = rep(NA, times = k)
+            Center = ans$center
+            Cluster = ans$cluster
+            for (i in 1:length(Cluster)) {
+                j = Cluster[i]
+                d = as.vector(dist(rbind(Center[j,], X[,i])))
+                if (d < Dist[j]) {
+                    Dist[j] = d
+                    index[j] = i
+                }
+            }
+            if (doplot) {
+                plot(t(X), col = ans$cluster)
+                points(ans$centers, col = 1:k, pch = 8, cex = 2)
+            }
+        }
+        if (method == "pam") {
+            ans = pam(t(X), k, ...)
+            index = ans$id.med
+            if (doplot) plot(ans)
+        }
+        if (method == "clara") {
+            ans = clara(t(X), k, ...)
+            index = ans$i.med
+            if (doplot) plot(ans)
+        }
+        
+        # Select data and optionally add control:
+        data = x[, index] 
+        if (control) attr(data, "control")<-ans
+        
+        # Return Value:
+        data
+    }
 
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
-    # Return Value:
-    return()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
-    # Return Value:
-    return()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
-    # Return Value:
-    return()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
-    # Return Value:
-    return()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
-    # Return Value:
-    return()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
-    # Return Value:
-    return()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
-    # Return Value:
-    return()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
-    # Return Value:
-    return()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
-    # Return Value:
-    return()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-test. =
-function()
-{ 
+    # Data:
+    berndtInvest = as.timeSeries(data(berndtInvest))
+    X = 100 * berndtInvest[, -c(1, 10, 17)]       
+        
+    # Selection:
+    .assetsSelect(X, 4, "hclust", doplot = FALSE)
+    .assetsSelect(X, 4, "agnes", doplot = FALSE)
+    .assetsSelect(X, 4, "diana", doplot = FALSE)
+    
+    .assetsSelect(X, 4, "kmeans", doplot = FALSE)
+    .assetsSelect(X, 4, "pam", doplot = FALSE)
+    .assetsSelect(X, 4, "clara", doplot = FALSE)
+   
     # Return Value:
     return()
 }
