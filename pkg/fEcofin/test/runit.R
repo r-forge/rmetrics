6a92d6bd365935ada44e2dc8de15bfdee9765cdc
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2007-06-10 11:09:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@1565 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fEcofin/test/runit.R
+++ pkg/fEcofin/test/runit.R
@@ -37,11 +37,26 @@ if (FALSE) {
     require(RUnit)
     testIndex = c(
         "0A", 
-        "1A", "1B", "1C", 
-        "2A", "2B", "2C", 
-        "3A", "3B", "3C", "3D", "3E", "3F", 
-        "4A", "4B", "4C", "4D", "4E", 
-        "5A", "5B", "5C", 
+        "1A", 
+        "1B", 
+        "1C", 
+        "2A", 
+        "2B", 
+        "2C", 
+        "3A", 
+        "3B", 
+        "3C", 
+        "3D", 
+        "3E", 
+        "3F", 
+        "4A", 
+        "4B", 
+        "4C", 
+        "4D", 
+        "4E", 
+        "5A", 
+        "5B", 
+        "5C", 
         "6A")
     File = "C:/Rmetrics/SVN/trunk/fEcofin/test/runit"
     Protocol = "runitfEcofin.txt"
