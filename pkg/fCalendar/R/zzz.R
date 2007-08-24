c4dd6c1a0cc208ac8b0746b77204d05011670418
maechler <unknown@noreply.com>
2007-08-24 10:48:39 +0000

    Sys.getenv, cleanup and "standard" messages
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@1756 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fCalendar/R/zzz.R
+++ pkg/fCalendar/R/zzz.R
@@ -39,13 +39,20 @@
 function(lib, pkg)
 {   # A function implemented by Diethelm Wuertz
     
-    # Package:
-    cat("\nRmetrics, (C) 1999-2007, Diethelm Wuertz, GPL")
-    cat("\nfCalendar: Chronological Objects and Calendar Tools\n")
-    
+    MSG <- if(getRversion() >= "2.5") packageStartupMessage else message
+    dsc <- packageDescription(pkg)
+    if(interactive() || getOption("verbose")) { # not in test scripts
+	MSG(sprintf("\nPackage %s (%s) loaded.\n%s\n",
+		    pkg, dsc$Version, dsc$Title),
+	    "Rmetrics, (C) 1999-2007, Diethelm Wuertz, GPL\n")
+    }
+
     # Load dll:
     # library.dynam("fCalendar", pkg, lib) 
 }
+
+if(!exists("Sys.setenv", mode = "function")) # pre R-2.5.0, use "old form"
+    Sys.setenv <- Sys.putenv
 
 
 ################################################################################
