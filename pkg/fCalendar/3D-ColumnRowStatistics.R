158973507f2b7ded4d266e46da969f06910454ec
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2007-09-03 20:38:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@1960 5d4a7380-5c0c-0410-b446-d4a5018c0119

---
+++ pkg/fCalendar/3D-ColumnRowStatistics.R
@@ -0,0 +1,541 @@
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA  02111-1307  USA
+
+# Copyrights (C)
+# for this R-port:
+#   1999 - 2007, Diethelm Wuertz, GPL
+#   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
+#   info@rmetrics.org
+#   www.rmetrics.org
+# for the code accessed (or partly included) from other R-ports:
+#   see R's copyright and license files
+# for the code accessed (or partly included) from contributed R-ports
+# and other sources
+#   see Rmetrics's copyright file
+
+
+# fEcofin::ColumnRowStatistics.R
+################################################################################
+# FUNCTION:                 ROW STATISTICS:
+#  rowStats                  Computes sample statistics by row
+#  rowAvgs                   Computes sample mean by row
+#  rowVars                   Computes sample variance by row
+#  rowStdevs                 Computes sample variance by row
+#  rowSkewness               Computes sample skewness by row
+#  rowKurtosis               Computes sample kurtosis by row
+#  rowCumsums                Computes sample cumulated sums by row
+# FUNCTION:                 COLUMN STATISTICS:
+#  colStats                  Computes sample statistics by column
+#  colAvgs                   Computes sample mean by column
+#  colVars                   Computes sample variance by column
+#  colStdevs                 Computes sample variance by column
+#  colSkewness               Computes sample skewness by column
+#  colKurtosis               Computes sample kurtosis by column
+#  colCumsums                Computes sample cumulated sums by column
+################################################################################
+
+
+################################################################################
+# FUNCTION:                 ROW STATISTICS:
+#  rowStats                  Computes sample statistics by row
+#  rowAvgs                   Computes sample mean by row
+#  rowVars                   Computes sample variance by row
+#  rowStdevs                 Computes sample variance by row
+#  rowSkewness               Computes sample skewness by row
+#  rowKurtosis               Computes sample kurtosis by row
+#  rowCumsums                Computes sample cumulated sums by row
+
+
+rowStats =
+function(x, FUN, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample statistics by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 1, FUN = FUN, ...)
+    } else {
+        result = apply(X, MARGIN = 1, FUN = FUN, ...)
+    }
+
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+rowSums.timeSeries =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample sum by row
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 1, FUN = sum, ...)
+    } else {
+        result = apply(X, MARGIN = 1, FUN = sum, ...)
+    }
+    result = t(t(result))
+    colnames(result) = "Sum"
+
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+rowMeans.timeSeries =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample mean by row
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 1, FUN = mean, ...)
+    } else {
+        result = apply(X, MARGIN = 1, FUN = mean, ...)
+    }
+    result = t(t(result))
+    colnames(result) = "Mean"
+
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+rowAvgs =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample mean by column
+
+    # Note:
+    #   R's base package comes already with a colMeans!
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 1, FUN = mean, ...)
+    } else {
+        result = apply(X, MARGIN = 1, FUN = mean, ...)
+    }
+    result = t(t(result))
+    colnames(result) = "Mean"
+
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+rowVars =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample variance by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 1, FUN = var, ...)
+    } else {
+        result = apply(X, MARGIN = 1, FUN = var, ...)
+    }
+    result = t(t(result))
+    colnames(result) = "Variance"
+
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+rowStdevs =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample standard deviation by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = sqrt(apply(na.omit(X), MARGIN = 1, FUN = var, ...))
+    } else {
+        result = sqrt(apply(X, MARGIN = 1, FUN = var, ...))
+    }
+    result = t(t(result))
+    colnames(result) = "StDev"
+
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+rowSkewness =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample skewness by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 1, FUN = skewness, ...)
+    } else {
+        result = apply(X, MARGIN = 1, FUN = skewness, ...)
+    }
+    result = t(t(result))
+    colnames(result) = "Skewness"
+
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+rowKurtosis =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample kurtosis by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 1, FUN = kurtosis, ...)
+    } else {
+        result = apply(X, MARGIN = 1, FUN = kurtosis, ...)
+    }
+    result = t(t(result))
+    colnames(result) = "Kurtosis"
+
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+rowCumsums =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample cumulated sums by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 2, FUN = cumsum, ...)
+    } else {
+        result = apply(X, MARGIN = 2, FUN = cumsum, ...)
+    }
+    colnames(result) = paste(1:ncol(x))
+
+    # Return Value:
+    result
+}
+
+
+################################################################################
+# FUNCTION:                 COLUMN STATISTICS:
+#  colStats                  Computes sample statistics by column
+#  colAvgs                   Computes sample mean by column
+#  colVars                   Computes sample variance by column
+#  colStdevs                 Computes sample variance by column
+#  colSkewness               Computes sample skewness by column
+#  colKurtosis               Computes sample kurtosis by column
+#  colCumsums                Computes sample cumulated sums by column
+
+
+colStats =
+function(x, FUN, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample statistics by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    apply(if(na.rm) na.omit(X) else X, 2, FUN, ...)
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+colSums.timeSeries =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample sum by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 2, FUN = sum, ...)
+    } else {
+        result = apply(X, MARGIN = 2, FUN = sum, ...)
+    }
+    result = t(t(result))
+    colnames(result) = "Sum"
+
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+colMeans.timeSeries =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample mean by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    if (na.rm) {
+        result = apply(na.omit(X), MARGIN = 2, FUN = mean, ...)
+    } else {
+        result = apply(X, MARGIN = 2, FUN = mean, ...)
+    }
+    result = t(t(result))
+    colnames(result) = "Mean"
+
+    # Return Value:
+    result
+}
+
+
+
+# ------------------------------------------------------------------------------
+
+
+colAvgs =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample mean by column
+
+    # Note:
+    #   R's base package comes already with a colMeans!
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    apply(if(na.rm) na.omit(X) else X, 2, mean, ...)
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+colVars =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample variance by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    apply(if(na.rm) na.omit(X) else X, 2, var, ...)
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+colStdevs =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample standard deviation by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    sqrt(apply(if(na.rm) na.omit(X) else X, 2, var, ...) )
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+colSkewness =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample skewness by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    apply(if(na.rm) na.omit(X) else X, 2, skewness, ...)
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+colKurtosis =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample kurtosis by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    apply(if(na.rm) na.omit(X) else X, 2, kurtosis, ...)
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+colCumsums =
+function(x, na.rm = FALSE, ...)
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes sample cumulated sums by column
+
+    # FUNCTION:
+
+    # Transform:
+    X = as.matrix(x)
+
+    # Statistics:
+    result <- apply(if(na.rm) na.omit(X) else X,
+                    2, cumsum, ...)
+
+    # Time Series Input ?
+    if (is(x, "timeSeries")) {
+        x@Data = result
+        result = x
+    }
+
+    # Return Value:
+    result
+}
+
+
+################################################################################
+
---
+++ pkg/fCalendar/3F-HeavisideFunction.R
@@ -0,0 +1,255 @@
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General 
+# Public License along with this library; if not, write to the 
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# MA  02111-1307  USA
+
+# Copyrights (C)
+# for this R-port: 
+#   1999 - 2007, Diethelm Wuertz, GPL
+#   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
+#   info@rmetrics.org
+#   www.rmetrics.org
+# for the code accessed (or partly included) from other R-ports:
+#   see R's copyright and license files
+# for the code accessed (or partly included) from contributed R-ports
+# and other sources
+#   see Rmetrics's copyright file
+
+
+# fEcofin::3F-HeavisideFunction.R
+################################################################################
+# FUNCTION:                 HEAVISIDE AND RELATED FUNCTIONS:
+#  Heaviside                 Computes Heaviside unit step function
+#  Sign                      Another signum function
+#  Delta                     Computes delta function
+#  Boxcar                    Computes boxcar function
+#  Ramp                      Computes ramp function
+# FUNCTION:                 HEAVISIDE SLIDER:
+#  .heavisideSlider          Displays Heaviside and related functions
+################################################################################
+
+
+################################################################################
+# FUNCTION:                 HEAVISIDE AND RELATED FUNCTIONS:
+#  Heaviside                 Computes Heaviside unit step function
+#  Sign                      Another signum function
+#  Delta                     Computes delta function
+#  Boxcar                    Computes boxcar function
+#  Ramp                      Computes ramp function
+
+
+Heaviside = 
+function(x, a = 0) 
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes the Heaviside or unit step function.
+    
+    # Arguments:
+    #   x - a numeric vector.
+    #   a - the location of the break.
+    
+    # Details:
+    #   The Heaviside step function is 1 for x>a, 1/2 for x=a,
+    #   and 0 for x<a.
+    
+    # Notes:
+    #   Heaviside Unit Step Function and Related Functions
+    #   See:  http://mathworld.wolfram.com/HeavisideStepFunction.html
+    #   Note: sign(x) is part of R's base package
+       
+    # FUNCTION:
+    
+    # Compute H:
+    result = (sign(x-a) + 1)/2
+    
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+Sign =
+function(x, a = 0) 
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes the (modofied) Sign function.
+    
+    # Arguments:
+    #   x - a numeric vector.
+    #   a - the location of the break.
+    
+    # Details:
+    #   The Sign function is 1 for x>a, 0 for x=a,
+    #   and -1 for x<a.
+     
+    # FUNCTION:
+    
+    # Compute Sign:
+    result = sign(x-a)
+    
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+Delta = 
+function(x, a = 0) 
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes the delta function.
+    
+    # Arguments:
+    #   x - a numeric vector.
+    #   a - the location of the break.
+    
+    # Details:
+    #   The delta function is defined as: delta(x) = d/dx Heaviside(x-a)
+
+    # FUNCTION:
+    
+    # Compute delta:
+    result = 1/sign(abs(x-a))-1
+    
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+Boxcar =
+function(x, a = 0.5) 
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes the boxcar function.
+    
+    # Arguments:
+    #   x - a numeric vector.
+    #   a - the location of the break.
+    
+    # Details:
+    #   The boxcar function is defined as: 
+    #       Pi(x) = Heaviside(x+a) - Heaviside(x-a)
+
+    # FUNCTION:
+    
+    # Compute boxcar:
+    result = Heaviside(x-a) - Heaviside(x+a)
+    
+    # Return Value:
+    result
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+Ramp =
+function(x, a = 0) 
+{   # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Computes the ramp function.
+    
+    # Arguments:
+    #   x - a numeric vector.
+    #   a - the location of the break.
+    
+    # Details:
+    #   The ramp function is defined as: R(x)= (x-a)*Heaviside(x-a)
+
+    # FUNCTION:
+    
+    # Compute ramp:
+    result = (x-a) * Heaviside(x-a)
+    
+    # Return Value:
+    result
+}
+
+
+################################################################################
+# FUNCTION:             HEAVISIDE SLIDER:
+#  .heavisideSlider      Displays Heaviside and related functions
+
+
+.heavisideSlider =
+function()
+{   # A function implemented by Diethelm Wuertz
+
+    # Description
+    #   Displays Heaviside and related functions
+
+    # FUNCTION:
+    
+    # Internal Function:
+    refresh.code = function(...)
+    {
+        # Sliders:
+        a = .sliderMenu(no = 1)
+        by = .sliderMenu(no = 2)
+        
+        # Frame:
+        par(mfrow = c(2, 2), cex = 0.7)
+        
+        # FGN TimeSeries:
+        x = seq(-10, 10, by = by)
+        H  = Heaviside(x, a)
+        Sign  = Sign(x, a)
+        Delta  = Delta(x, a)
+        Boxcar  = Boxcar(x, a)
+        Ramp  = Ramp(x, a)
+        plot(x, H, type = "b", ylim = c(-1, 1), 
+            col = "steelblue", pch = 19)
+        title(main = paste("Heaviside | a =", a))
+        grid()
+        plot(x, Sign, type = "b", ylim = c(-1, 1), 
+            col = "steelblue", pch = 19)
+        title(main = paste("Sign | a =", a))
+        grid()
+        plot(x, Boxcar, type = "b", ylim = c(-1, 1), 
+            col = "steelblue", pch = 19)
+        title(main = paste("Boxcar | a =", a))
+        grid()
+        plot(x, Ramp, type = "b", 
+            col = "steelblue", pch = 19)
+        title(main = paste("Ramp | a =", a))
+        grid()
+        
+        # Reset Frame:
+        par(mfrow = c(1, 1), cex = 0.7)
+    }
+  
+    # Open Slider Menu:
+    .sliderMenu(refresh.code,
+       names =       c(  "a", "by"),
+       minima =      c(  -10,  0.1),
+       maxima =      c(   10,  1.0),
+       resolutions = c(  0.1,  0.1),
+       starts =      c(    0,  0.2))
+}
+
+
+################################################################################
+
