4e046628fdbbeb6cad71a1e3a64832e3b3ef3e8a
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2007-09-05 21:15:21 +0000

    OX Path changed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2173 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fGarch/R/GarchOxInterface.R
+++ pkg/fGarch/R/GarchOxInterface.R
@@ -36,7 +36,7 @@
 ################################################################################
 
 
-.OXPATH = "C:\\Ox\\Ox3"
+.OXGARCHPATH = "C:\\Ox\\Ox3"
 
 
 # ------------------------------------------------------------------------------
@@ -248,8 +248,8 @@ description = NULL)
     
     # Calculate:    
     command = paste(
-        .OXPATH, "\\bin\\oxl.exe ",  
-        .OXPATH, "\\lib\\GarchOxModelling.ox", sep = "")
+        .OXGARCHPATH, "\\bin\\oxl.exe ",  
+        .OXGARCHPATH, "\\lib\\GarchOxModelling.ox", sep = "")
     fit$ox = system(command, show.output.on.console = trace, invisible = TRUE)
     fit$model = selected
     fit$call = match.call()
