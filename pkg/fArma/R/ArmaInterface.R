c0e84c733012e314f930f14cf00610ace612d4dd
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2007-09-30 17:11:01 +0000

    .modelSeries internal function moved here
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2298 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fArma/R/ArmaInterface.R
+++ pkg/fArma/R/ArmaInterface.R
@@ -49,6 +49,8 @@
 #  coefficients.fARMA      S3: Synonyme for coef.fARMA
 #  fitted.fARMA            S3: Returns fitted values from a fitted ARMA object
 #  residuals.fARMA         S3: Returns residuals from a fitted ARMA object
+# FUNCTION:
+#  .modelSeries         Models a timeSeries object to use formulas
 ################################################################################
 
        
@@ -1305,6 +1307,69 @@ function(object, ...)
     
     # Return Value:
     ans
+}
+
+
+################################################################################
+# FUNCTION:
+#  .modelSeries         Models a timeSeries object to use formulas
+
+
+.modelSeries = 
+function(formula, data, fake = FALSE, lhs = FALSE)
+{   # A function implemented by Diethelm Wuertz
+
+    # Arguments:
+    #   data - a timeSeries, a data.frame or a numeric vector
+    
+    # Details:
+    #   Time Series Modelling
+    #   Regression Modelling
+    #   Data Management
+    
+    
+    # If no respnonse is pecified:
+    if (length(formula) == 2) {
+        formula = as.formula(paste("x", formula[1], formula[2], collapse = ""))
+        stopifnot(!missing(data))
+    }
+
+    # Isf data is missing, take the first data set from the search path:
+    if (missing(data)) {
+        data = eval(parse(text = search()[2]), parent.frame())
+    }
+    
+    if (is.numeric(data)) {
+        data = data.frame(data)
+        colnames(data) = all.vars(formula)[1]
+        lhs = TRUE
+    }
+    
+    # If we consider a faked formula:
+    if (fake) {
+        response = as.character(formula)[2]
+        Call = as.character(match.call()[[2]][[3]])
+        method = Call[1]
+        predictors = Call[2]
+        formula = as.formula(paste(response, "~", predictors))
+    }
+    
+    # If only left hand side is required:
+    if (lhs) {
+        response = as.character(formula)[2]
+        formula = as.formula(paste(response, "~", 1))
+    } 
+    
+    # Create Model Data:
+    x = model.frame(formula, data)
+    
+    # Convert:
+    if (class(data) == "timeSeries") x = timeSeries(x)
+    if (fake) attr(x, "control") <- method
+    
+    # Return value:
+    x
+    
 }
 
 
