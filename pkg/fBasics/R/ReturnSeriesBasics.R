ae0deac133da5e81fe03aa541bb78413b622e370
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2007-10-09 06:25:51 +0000

    R Files untangled
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2337 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fBasics/R/ReturnSeriesBasics.R
+++ pkg/fBasics/R/ReturnSeriesBasics.R
@@ -28,113 +28,21 @@
 
 
 ################################################################################
-# FUNCTION:               BASIC STATISTICS:
-#  basicStats              Returns a basic statistics summary
-# FUNCTION:               TAILORED PLOT FUNCTIONS:     
+# FUNCTION:               TAILORED SERIES PLOTS:     
 #  seriesPlot              Returns a tailored return series plot
 #  cumulatedPlot           Returns a tailored cumulated return series plot
+# FUNCTION:               TAILORED DENSITY FUNCTIONS:
 #  histPlot                Returns a tailored histogram plot
 #  densityPlot             Returns a tailored kernel density estimate plot
+# FUNCTION:               TAILORED QUANTILE PLOTS:
 #  qqnormPlot              Returns a tailored normal quantile-quantile plot
 #  qqnigPlot               Returns a tailored NIG quantile-quantile plot
-# FUNCTION:               BOX PLOTS:
+# FUNCTION:               TAILORED BOX PLOTS:
 #  boxPlot                 Produces a side-by-side standard box plot
 #  boxPercentilePlot       Produces a side-by-side box-percentile plot
 # FUNCTION:               GRAPHICAL USER INTERFACE:
 #  returnSeriesGUI         Opens a GUI for return series plots
 ################################################################################
-
-
-################################################################################
-# FUNCTION:               BASIC STATISTICS:
-#  basicStats              Returns a basic statistics summary
-
-
-basicStats = 
-function(x, ci = 0.95) 
-{   # A function implemented by Diethelm Wuertz
-    
-    # Description:
-    #   Calculates Basic Statistics
-    
-    # Arguments:
-    #   x - an object which can be transformed by the function
-    #       as.matrix() into an object of class matrix. 
-    #   ci - a numeric value setting the confidence interval.
-    
-    # Value:
-    #   a two-column data frame, where the first column takes the 
-    #   value of the statistics, and the second its name, e.g.
-    #   "nobs", "NAs",  "Minimum", "Maximum", "1. Quartile",  
-    #   "3. Quartile",  "Mean", "Median", "Sum",  "SE Mean", 
-    #   "LCL Mean", "UCL Mean", "Variance", "Stdev", "Skewness", 
-    #   "Kurtosis")
-
-    # FUNCTION:
-    
-    # Univariate/Multivariate:
-    y = as.matrix(x)
-    
-    
-    # Handle Column Names:
-    if (is.null(colnames(y))) {
-        Dim = dim(y)[2]
-        if (Dim == 1) {
-            colnames(y) = paste(substitute(x), collapse = ".")
-        } else if (Dim > 1) {
-            colnames(y) = 
-                paste(paste(substitute(x), collapse = ""), 1:Dim, sep = "")
-        }
-    }
-    
-    # Internal Function - CL Levels:    
-    cl.vals = function(x, ci) {
-        x = x[!is.na(x)]
-        n = length(x)
-        if(n <= 1) return(c(NA, NA))
-        se.mean = sqrt(var(x)/n)
-        t.val = qt((1 - ci)/2, n - 1)
-        mn = mean(x)
-        lcl = mn + se.mean * t.val
-        ucl = mn - se.mean * t.val
-        c(lcl, ucl)
-    }        
-    
-    # Basic Statistics:
-    nColumns = dim(y)[2]
-    ans = NULL
-    for (i in 1:nColumns) {
-        X = y[, i]     
-        # Observations:
-        X.length = length(X)
-        X = X[!is.na(X)]
-        X.na = X.length - length(X)
-        # Basic Statistics:
-        z = c(
-            X.length, X.na, min(X), max(X),
-            as.numeric(quantile(X, prob = 0.25, na.rm = TRUE)), 
-            as.numeric(quantile(X, prob = 0.75, na.rm = TRUE)), 
-            mean(X), median(X), sum(X), sqrt(var(X)/length(X)), 
-            cl.vals(X, ci)[1], cl.vals(X, ci)[2], var(X), 
-            sqrt(var(X)), skewness(X), kurtosis(X) )    
-        # Row Names:
-        znames = c(
-            "nobs", "NAs",  "Minimum", "Maximum", 
-            "1. Quartile",  "3. Quartile",  "Mean", "Median", 
-            "Sum",  "SE Mean", "LCL Mean", "UCL Mean", 
-            "Variance", "Stdev", "Skewness", "Kurtosis")   
-        # Output as data.frame
-        result = matrix(z, ncol = 1)
-        row.names(result) = znames    
-        ans = cbind(ans, result) 
-    }
-    
-    # Column Names:
-    colnames(ans) = colnames(y)
-    
-    # Return Value:
-    data.frame(round(ans, digits = 6))
-}
 
 
 ################################################################################    
