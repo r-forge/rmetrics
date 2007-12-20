40be2310f7b732b916c52bec99036104e2c1303d
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2007-12-20 21:35:38 +0000

    fedSeries updated
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2374 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fImport/R/FedReserve.R
+++ pkg/fImport/R/FedReserve.R
@@ -40,10 +40,10 @@
 #  fredSeries            Easy to use download from St. Louis FED 
 
 
-fredImport = 
-function(query, file = "tempfile", 
-source = "http://research.stlouisfed.org/fred2/series/", 
-frequency = "daily", save = FALSE, sep = ";", try = TRUE) 
+fredImport <-  
+    function(query, file = "tempfile", 
+    source = "http://research.stlouisfed.org/fred2/series/", 
+    frequency = "daily", save = FALSE, sep = ";", try = TRUE) 
 {   # A function implemented by Diethelm Wuertz
 
     # Description:
@@ -144,111 +144,9 @@ frequency = "daily", save = FALSE, sep = ";", try = TRUE)
 # ------------------------------------------------------------------------------
 
 
-forecastsImport = 
-function(query, file = "tempfile", 
-source = "http://www.forecasts.org/data/data/", save = FALSE, try = TRUE) 
-{   # A function implemented by Diethelm Wuertz
-
-    # Description:
-    #   Downloads Monthly Market Data, Indices and Benchmarks from the 
-    #   Financial Forecast Center, "www.forecasts.org".
-    
-    # Value:
-    #   An One Column data frame with row names denoting the dates
-    #   given in the POSIX format "%Y%m%d".
-        
-    # Examples:
-    #   forecastsImport(query = "GOLD")
-    #   forecastsImport(query = "MDISCRT")
-    #   forecastsImport(query = "EXJPUS")
-    #   forecastsImport(query = "GS3M")
-    #   forecastsImport(query = "FEDFUNDS")
-    
-    # Note:
-    #   This function is not written for monthly data sets.
-    #   Some example data sets include:  
-    #   Indices:
-    #     djiaM     sp500M   sp100M     nysecompM  nasdcompM  djcompM  
-    #     djtransM  djutilM  spmc400M   spsc600M   r1000M     r2000M    
-    #     r3000M    w5000M   valuM   
-    #     nik225M  daxM      hangsengM  ftse100M   tse300M    mtM
-    #   Ohter:  
-    #     MDISCRT      
-    #     EXJPUS        
-    #     GS3M
-
-    # FUNCTION:
-    
-    # Download:
-    if (try) {
-        # Try for Internet Connection:
-        z = try(forecastsImport(file = file, source = source, query = query, 
-            save = save, try = FALSE))
-        if (inherits(z, "try-error") || inherits(z, "Error")) {
-            return("No Internet Access")
-        } else {
-            return(z) 
-        } 
-    } else { 
-        # File Name:
-        queryFile = paste(query, ".htm", sep = "")
-        
-        # Construct URL:
-        url = paste(source, queryFile, sep = "")
-        
-        # Download file:
-        download.file(url, file) 
-        
-        # Scan the file:
-        x = scan(file, what = "", sep = "\n")
-        
-        # Extract dates ^19XX and ^20XX:
-        x = x[regexpr("^[12][90]", x) > 0]
-        # Write back to file:
-        write(x, file)  
-        
-        # Read as data frame:
-        x = read.table(file)
-        
-        # Two types of date strings are used %Y-%m-%d and %Y.%m
-        # transform to %Y%m and paste the 28th to the format string:
-        x[, 1] = substr(gsub("-", ".", as.vector(x[, 1])), 1, 7)
-        charvec = as.character(10000*as.numeric(x[, 1]) + 1)
-        rowNames = as.character(timeLastDayInMonth(charvec, format = "%Y%m%d"))
-        x = data.frame(cbind(rowNames, as.numeric(x[, -1])))
-        colnames(x) = c("DATE", query)
-        
-        # Save Download ?
-        if (save) {
-            write.table(paste("%Y%m%d;", query, sep = ""), file, 
-                quote = FALSE, row.names = FALSE, col.names = FALSE)
-            write.table(x, file, quote = FALSE, append = TRUE, 
-                col.names = FALSE, sep=";") 
-        } else {
-            unlink(file) 
-        }  
-        
-        # Return Value:
-        ans = new("fWEBDATA",     
-            call = match.call(),
-            param = c("Instrument Query" = query),
-            data = x,
-            title = "Web Data Import from Forecasts", 
-            description = .description() )
-        return(ans)
-    }
-    
-    # Return Value:
-    invisible()
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-fredSeries = 
-function(query = "DPRIME", frequency = "daily", returnClass = c("timeSeries", 
-"ts", "matrix", "data.frame"), getReturns = FALSE, ...)
+fredSeries <-  
+    function(query = "DPRIME", frequency = "daily", returnClass = 
+    c("timeSeries", "ts", "matrix", "data.frame"), getReturns = FALSE, ...)
 {   # A function implemented by Diethelm Wuertz
 
     # Description:
@@ -259,6 +157,7 @@ function(query = "DPRIME", frequency = "daily", returnClass = c("timeSeries",
     #       symbol name.
     #   frequency - a character string value, the frquency of the
     #       data records.
+    #   ... - arguments passed to the function returns
     
     # Examples:
     #   fredSeries("DPRIME")
@@ -274,14 +173,16 @@ function(query = "DPRIME", frequency = "daily", returnClass = c("timeSeries",
     colnames(X)<-colnames(Y)[-1]
     
     # Compute Return Series ?
-    if (getReturns) X = returnSeries(X, ...)  
+    if (getReturns) X = returns(X, ...)  
     
     # Return as Object ?
     if (returnClass == "matrix") {
-        X = X@data
-    } else if (returnClass == "data.frame") {
-        X = data.frame(X@Data)
-    } else if (returnClass == "ts") {
+        X = X@data 
+    }
+    if (returnClass == "data.frame") {
+        X = data.frame(X@Data) 
+    }
+    if (returnClass == "ts") {
         X = as.ts(X@Data)
     }
     
