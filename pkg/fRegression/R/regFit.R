93e40ab1e70e3d281d4483a5b7ab28d5368e61b2
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2008-01-02 17:50:36 +0000

    gregFit moved to regFit.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2475 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fRegression/R/regFit.R
+++ pkg/fRegression/R/regFit.R
@@ -209,4 +209,98 @@ regFit <-
 
 
 ################################################################################
+# FUNCTION:             PARAMETER ESTIMATION:
+#  gregFit               Wrapper Function for Generalized Regression Models
+#  .glmFit                Generalized Linear Model
+#  .gamFit                Generalized Additive Model
+################################################################################
+
+
+################################################################################
+# MODEL:        PACKAGE     print   plot   summary   print     predict
+#                                    persp           summary
+#   lm          stats       x       x      x         x         x
+#   rlm         MASS
+#   glm         stats       x       -      x         x         x
+#   gam         mgcv        x       x      x         x         x
+#   ppr         modreg      x       x      x         x         x
+#   polymars*   polspline   -       xx     x         -         x
+#   nnet        nnet        x       -      x         x         x
+#
+#   *BUILTIN:
+#    polspline  required!
+#   *IMPORTANT NOTE:
+#    Both packages r-cran-mda and r-cran-polspline are not available on the
+#    Debian Server, therefore we made them accessible as Builtin functions
+################################################################################
+
+
+gregFit <-  
+    function (formula, family, data, use = c("glm", "gam"), 
+    trace = TRUE, title = NULL, description = NULL, ...) 
+{   
+    # A function implemented by Diethelm Wuertz
+
+    # Description:
+    #   Common function call for generalized regression models.
+    
+    # Details:
+    #   This is a wrapper function for the following regrssion models:
+    #   GLM         Generalized Linear Modelling
+    #   GAM         Generalized Additive Modelling
+    
+    # Notes:
+    #   Available Methods are
+    #   "print", "plot", "summary", and "predict" method
+    #   "coef", "residuals" "fitted", "vcov" method
+    
+    # FUNCTION:
+    
+    # Transform data into a dataframe
+    Data = data
+    data = as.data.frame(data)
+    
+    # Function to be called:
+    fun = use = match.arg(use)
+
+    # Title:
+    if (is.null(title)) {
+        if (use == "glm") title = "Generalized Linear Modelling"
+        if (use == "gam") title = "Generalized Additive Modelling"
+    }  
+    
+    # Description:
+    if (is.null(description)) {
+        description  = .description()
+    }  
+    
+    # Evaluate:
+    cmd = match.call()
+    if (!is.null(cmd$use)) cmd = cmd[-match("use", names(cmd), 0)]    
+    cmd[[1]] <- as.name(fun)
+    fit <- eval(cmd, parent.frame()) 
+        
+    # Add to Fit:
+    fit$residuals = as.vector(fit$residuals)    
+    fit$fitted.values = as.vector(fit$fitted.values)
+    fit$parameters = fit$coef
+    class(fit) = c("list", class(fit))
+    
+    # Return Value:
+    new("fREG",     
+        call = as.call(match.call()),
+        formula = as.formula(formula), 
+        family = as.character(gaussian()),
+        method = as.character(use),
+        data = list(),
+        fit = fit,
+        residuals = fit$residuals,
+        fitted = fit$fitted.values,
+        title = as.character(title), 
+        description = .description() 
+    )
+}
+
+
+################################################################################
 
