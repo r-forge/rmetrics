8860d618bd4ce92d76b6974f0d842f7a4d5d46e9
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2008-01-07 21:46:39 +0000

    plot methods fixed for faked formula and data input
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2511 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fGarch/R/plot-methods.R
+++ pkg/fGarch/R/plot-methods.R
@@ -208,8 +208,14 @@ setMethod(f = "plot", signature(x = "fGARCH", y = "missing"), definition =
     #   Internal plot function
     
     # 3. Series with 2 Conditional SD Superimposed:
-    xseries = x@data$Data
-    xcsd = x@sigma.t
+    if(class(x@data$Data) == "timeSeries") {
+        xseries = x@data$Data
+        xseries@data= as.matrix(x@data$data)
+        xcsd = volatility(x, "sigma")
+    } else {
+        xseries = x@data$data
+        xcsd = x@sigma.t
+    }    
     ci = 2
     plot(xseries, type = "l", col = "steelblue", ylab = "x",
         main = "Series with 2 Conditional SD Superimposed")
@@ -232,7 +238,7 @@ setMethod(f = "plot", signature(x = "fGARCH", y = "missing"), definition =
     #   Internal plot function
     
     # 4. ACF of the Observations:
-    xseries = x@data$Data
+    xseries = as.vector(x@data$data)
     n = length(xseries)
     lag.max = as.integer(10*log10(n))
     acf(xseries, lag.max = lag.max, xlab = "Lags", col = "steelblue", 
@@ -252,7 +258,7 @@ setMethod(f = "plot", signature(x = "fGARCH", y = "missing"), definition =
     #   Internal plot function
     
     # 5. ACF of the Squared Observations:
-    xseries = x@data$Data
+    xseries = as.vector(x@data$data)
     xseries2 = xseries^2
     n = length(xseries)
     lag.max = as.integer(10*log10(n))
@@ -273,7 +279,7 @@ setMethod(f = "plot", signature(x = "fGARCH", y = "missing"), definition =
     #   Internal plot function
     
     # 6. Cross Correlation between x^2 and x:
-    xseries = slot(x, "data")$Data
+    xseries = as.vector(x@data$data)
     xseries2 = xseries^2
     n = length(xseries)
     lag.max = as.integer(10*log10(n))
