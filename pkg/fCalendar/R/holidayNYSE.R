b1e7544b4d9ab399ca72f72c132a6aa0c3f5b5ff
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-02-04 09:53:45 +0000

    Whenever a holiday falls on a Saturday, the exchange observes the holiday on the preceding Friday, except if the preceding Friday closes an accounting period for 2007-2009.
    This code has two obvious bugs:
    1) Holidays on Sundays, instead of Saturdays, are eligible to be moved 
    to the previous day:
        # instead of: ans = ans - [...] & as.POSIXlt(ans@Data)$wday == 0 & [...]
        # use:        ans = ans - [...] & as.POSIXlt(ans@Data)$wday == 6 & [...]
    2) We have to check if the day preceding the holiday (and not the 
    holiday itself) is a last friday of the month:
        # instead of: posix = as.POSIXlt(ans@Data)
        # use:        posix = as.POSIXlt(ans@Data - 24*3600 )
        # instead of: ans = ans - [...] ans != ExceptOnLastFriday [...]
        # use:        ans = ans - [...] (ans - 24*3600)!= ExceptOnLastFriday 
    [...]
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2750 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fCalendar/R/holidayNYSE.R
+++ pkg/fCalendar/R/holidayNYSE.R
@@ -6,16 +6,16 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - 2008, Diethelm Wuertz, Rmetrics Foundation, GPL
 #   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
 #   info@rmetrics.org
@@ -33,104 +33,107 @@
 ################################################################################
 
 
-holidayNYSE <- 
+holidayNYSE <-
     function(year = currentYear)
-{   
-    # A function implemented by Diethelm Wuertz
+{   # A function implemented by Diethelm Wuertz
 
     # Description:
-    #   Returns 'timeDate' object for full-day NYSE holidays 
-    
+    #   Returns 'timeDate' object for full-day NYSE holidays
+
     # Arguments:
     #   year - an integer variable or vector for the year(s)
     #       ISO-8601 formatted as "CCYY" where easter or
     #       easter related feasts should be computed.
-    
+
     # Value:
-    #   Returns the holiday calendar for the NYSE formatted as 
+    #   Returns the holiday calendar for the NYSE formatted as
     #   'timeDate' object.
-    
+
     # Details:
     #   The "New York Stock Exchange" calendar starts from year 1885.
     #   The rules are listed at the web site http://www.nyse.com.
-    
+
     # Example:
     #   > holiday.NYSE(2004)
     #   [1] "America/New_York"
     #   [1] [2004-01-01] [2004-01-19] [2004-02-16] [2004-04-09]
     #   [5] [2004-05-31] [2004-07-05] [2004-09-06] [2004-11-25]
-    
+
+    # Changes:
+    #
+
     # FUNCTION:
-    
+
     #  Settings:
-    holidays = NULL
-    
+    holidays <- NULL
+
     # Iterate years:
-    for (y in year ) { 
-        if (y >= 1885) 
-            holidays = c(holidays, as.character(USNewYearsDay(y)))
-        if (y >= 1885) 
-            holidays = c(holidays, as.character(USIndependenceDay(y))) 
-        if (y >= 1885) 
-            holidays = c(holidays, as.character(USThanksgivingDay(y)))
+    for (y in year ) {
         if (y >= 1885)
-            holidays = c(holidays, as.character(USChristmasDay(y)))
+            holidays <- c(holidays, as.character(USNewYearsDay(y)))
+        if (y >= 1885)
+            holidays <- c(holidays, as.character(USIndependenceDay(y)))
+        if (y >= 1885)
+            holidays <- c(holidays, as.character(USThanksgivingDay(y)))
+        if (y >= 1885)
+            holidays <- c(holidays, as.character(USChristmasDay(y)))
         if (y >= 1887)
-            holidays = c(holidays, as.character(USLaborDay(y)))
+            holidays <- c(holidays, as.character(USLaborDay(y)))
         if (y != 1898 & y != 1906 & y != 1907)
-            holidays = c(holidays, as.character(USGoodFriday(y)))
-        if (y >= 1909 & y <= 1953) 
-            holidays = c(holidays, as.character(USColumbusDay(y)))   
+            holidays <- c(holidays, as.character(USGoodFriday(y)))
+        if (y >= 1909 & y <= 1953)
+            holidays <- c(holidays, as.character(USColumbusDay(y)))
         if (y >= 1998)
-            holidays = c(holidays, as.character(USMLKingsBirthday(y)))       
-        if (y >= 1896 & y <= 1953) 
-            holidays = c(holidays, as.character(USLincolnsBirthday(y)))
-        if (y <= 1970) 
-            holidays = c(holidays, as.character(USWashingtonsBirthday(y)))
-        if (y >= 1970) 
-            holidays = c(holidays, as.character(USPresidentsDay(y))) 
-        if (y == 1918 | y == 1921 | (y >= 1934 & y <= 1953)) 
-            holidays = c(holidays, as.character(USVeteransDay(y)))       
-        if (y <= 1968 | y == 1972 | y == 1976 | y == 1980) 
-            holidays = c(holidays, as.character(USElectionDay(y)))       
-        if (y <= 1970) 
-            holidays = c(holidays, as.character(USDecorationMemorialDay(y)))   
-        if (y >= 1971) 
-            holidays = c(holidays, as.character(USMemorialDay(y)))
-    }  
+            holidays <- c(holidays, as.character(USMLKingsBirthday(y)))
+        if (y >= 1896 & y <= 1953)
+            holidays <- c(holidays, as.character(USLincolnsBirthday(y)))
+        if (y <= 1970)
+            holidays <- c(holidays, as.character(USWashingtonsBirthday(y)))
+        if (y > 1970)
+            holidays <- c(holidays, as.character(USPresidentsDay(y)))
+        if (y == 1918 | y == 1921 | (y >= 1934 & y <= 1953))
+            holidays <- c(holidays, as.character(USVeteransDay(y)))
+        if (y <= 1968 | y == 1972 | y == 1976 | y == 1980)
+            holidays <- c(holidays, as.character(USElectionDay(y)))
+        if (y <= 1970)
+            holidays <- c(holidays, as.character(USDecorationMemorialDay(y)))
+        if (y >= 1971)
+            holidays <- c(holidays, as.character(USMemorialDay(y)))
+    }
 
     # Sort and Convert to 'timeDate':
-    holidays = sort(holidays)
-    ans = timeDate(holidays)
-    
+    holidays <- sort(holidays)
+    ans <- timeDate(holidays)
+
     # Move Sunday Holidays to Monday:
-    ans = ans + as.integer(as.POSIXlt(ans@Data)$wday==0) * 24 * 3600
-   
+    ans <- ans + as.integer(as.POSIXlt(ans@Data)$wday==0) * 24 * 3600
+
     # After July 3, 1959, move Saturday holidays to Friday
-    # ... except if at the end of monthly/yearly accounting period 
+    # ... except if at the end of monthly/yearly accounting period
     # this is the last business day of a month.
-    posix = as.POSIXlt(ans@Data)
-    y = posix$year + 1900
-    m = posix$mon + 1
-    lastday = as.POSIXlt((timeCalendar(y = y+(m+1)%/%13, 
-        m = m+1-(m+1)%/%13*12, d = 1)-24*3600)@Data)$mday
-    ExceptOnLastFriday = timeDate(as.character(
-        .last.of.nday(year = y, month = m, lastday = lastday, nday = 5)))
-    ans = ans - as.integer(
-        ans >= timeDate("1959-07-03") &
-        as.POSIXlt(ans@Data)$wday == 0  &
-        ans != ExceptOnLastFriday ) * 24 * 3600 
-    
+    posix <- as.POSIXlt(ans@Data - 24*3600)
+    y <- posix$year + 1900
+    m <- posix$mon + 1
+    lastday <- as.POSIXlt((timeCalendar(y = y+(m+1)%/%13,
+                                        m = m+1-(m+1)%/%13*12,
+                                        d = 1)
+                           - 24*3600)@Data)$mday
+    ExceptOnLastFriday <- timeDate(as.character(.last.of.nday(year = y,
+                                                              month = m,
+                                                              lastday = lastday,
+                                                              nday = 5)))
+    ans <- ans - as.integer(ans >= timeDate("1959-07-03") &
+                            as.POSIXlt(ans@Data)$wday == 6  &
+                            (ans - 24*3600) != ExceptOnLastFriday ) * 24 * 3600
+
     # Remove Remaining Weekend Dates:
-    ans = ans[
-        !(as.POSIXlt(ans@Data)$wday == 0 | 
-        as.POSIXlt(ans@Data)$wday == 6)]
-    ans@FinCenter = "NewYork"
-    
+    ans <- ans[ !(as.POSIXlt(ans@Data)$wday == 0 |
+                  as.POSIXlt(ans@Data)$wday == 6)]
+    ans@FinCenter <- "NewYork"
+
     # Return Value:
     ans
 }
-
 
 ################################################################################
 
