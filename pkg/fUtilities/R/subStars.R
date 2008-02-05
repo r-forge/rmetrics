b047caa18f4abbf35007651953b8ea06b333d249
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2008-02-05 23:14:01 +0000

    header made unique
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2780 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fUtilities/R/subStars.R
+++ pkg/fUtilities/R/subStars.R
@@ -1,11 +1,43 @@
 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Library General Public License for more details.
+#
+# You should have received A copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA  02111-1307  USA
 
+# Copyrights (C)
+# for this R-port:
+#   1999 - 2008, Diethelm Wuertz, Rmetrics Foundation, GPL
+#   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
+#   www.rmetrics.org
+# for the code accessed (or partly included) from other R-ports:
+#   see R's copyright and license files
+# for the code accessed (or partly included) from contributed R-ports
+# and other sources
+#   see Rmetrics's copyright file
+
+
+################################################################################
+# FUNCTION:                 SLIDER MENU:
+#  .subStars                 Creates a sub stars plot
 ################################################################################
 
 
 .subStars <- 
     function (x, keyOffset = c(0, 0), loc = NULL, palette = NULL) 
 {
+    # Description:
+    #   Creates a sub stars plot
+    
     # Details:
     #   this is the stars::graphics() function modified in such a
     #   way, that it works together with the subplot builtin.
