e192414176f7e4df0acf91499069abe6ddd3095f
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2008-02-05 23:18:00 +0000

    headers made unique
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2784 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fUtilities/R/krigeInterp.R
+++ pkg/fUtilities/R/krigeInterp.R
@@ -18,7 +18,6 @@
 # for this R-port: 
 #   1999 - 2008, Diethelm Wuertz, Rmetrics Foundation, GPL
 #   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
-#   info@rmetrics.org
 #   www.rmetrics.org
 # for the code accessed (or partly included) from other R-ports:
 #   see R's copyright and license files
