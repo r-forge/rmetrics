de9433f2a722d343d1fd15a5cb627a7e6cbcb07f
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2008-02-05 22:11:10 +0000

    heaviside disentangled
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2772 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fUtilities/R/Heaviside.R
+++ pkg/fUtilities/R/Heaviside.R
@@ -34,18 +34,7 @@
 #  Delta                     Computes delta function
 #  Boxcar                    Computes boxcar function
 #  Ramp                      Computes ramp function
-# FUNCTION:                 HEAVISIDE SLIDER:
-#  .heavisideSlider          Displays Heaviside and related functions
 ################################################################################
-
-
-################################################################################
-# FUNCTION:                 HEAVISIDE AND RELATED FUNCTIONS:
-#  Heaviside                 Computes Heaviside unit step function
-#  Sign                      Another signum function
-#  Delta                     Computes delta function
-#  Boxcar                    Computes boxcar function
-#  Ramp                      Computes ramp function
 
 
 Heaviside <- 
@@ -190,69 +179,6 @@ Ramp <-
     
     # Return Value:
     result
-}
-
-
-################################################################################
-# FUNCTION:             HEAVISIDE SLIDER:
-#  .heavisideSlider      Displays Heaviside and related functions
-
-
-.heavisideSlider <- 
-    function()
-{   
-    # A function implemented by Diethelm Wuertz
-
-    # Description
-    #   Displays Heaviside and related functions
-
-    # FUNCTION:
-    
-    # Internal Function:
-    refresh.code = function(...)
-    {
-        # Sliders:
-        a = .sliderMenu(no = 1)
-        by = .sliderMenu(no = 2)
-        
-        # Frame:
-        par(mfrow = c(2, 2), cex = 0.7)
-        
-        # FGN TimeSeries:
-        x = seq(-10, 10, by = by)
-        H  = Heaviside(x, a)
-        Sign  = Sign(x, a)
-        Delta  = Delta(x, a)
-        Boxcar  = Boxcar(x, a)
-        Ramp  = Ramp(x, a)
-        plot(x, H, type = "b", ylim = c(-1, 1), 
-            col = "steelblue", pch = 19)
-        title(main = paste("Heaviside | a =", a))
-        grid()
-        plot(x, Sign, type = "b", ylim = c(-1, 1), 
-            col = "steelblue", pch = 19)
-        title(main = paste("Sign | a =", a))
-        grid()
-        plot(x, Boxcar, type = "b", ylim = c(-1, 1), 
-            col = "steelblue", pch = 19)
-        title(main = paste("Boxcar | a =", a))
-        grid()
-        plot(x, Ramp, type = "b", 
-            col = "steelblue", pch = 19)
-        title(main = paste("Ramp | a =", a))
-        grid()
-        
-        # Reset Frame:
-        par(mfrow = c(1, 1), cex = 0.7)
-    }
-  
-    # Open Slider Menu:
-    .sliderMenu(refresh.code,
-       names =       c(  "a", "by"),
-       minima =      c(  -10,  0.1),
-       maxima =      c(   10,  1.0),
-       resolutions = c(  0.1,  0.1),
-       starts =      c(    0,  0.2))
 }
 
 
