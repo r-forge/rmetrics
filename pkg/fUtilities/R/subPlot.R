82112408807258bafc6d7d20b8670691aeb66de0
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2008-02-05 23:08:19 +0000

    headers made unique
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2776 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fUtilities/R/subPlot.R
+++ pkg/fUtilities/R/subPlot.R
@@ -1,12 +1,52 @@
 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Library General Public License for more details.
+#
+# You should have received A copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA  02111-1307  USA
+
+# Copyrights (C)
+# for this R-port:
+#   1999 - 2008, Diethelm Wuertz, Rmetrics Foundation, GPL
+#   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
+#   www.rmetrics.org
+# for the code accessed (or partly included) from other R-ports:
+#   see R's copyright and license files
+# for the code accessed (or partly included) from contributed R-ports
+# and other sources
+#   see Rmetrics's copyright file
+
+
+################################################################################
+# FUNCTION:                 DESCRIPTION:
+#  .emptyPlot                Creates an empty plot page
+#  .subPlot                  Creates a sub plot
+#  .cnvrt.coords             Converts coordinates
 ################################################################################
 
 
-.emptyPlot =
-function()
+.emptyPlot <- 
+    function()
 {
+    # Description:
+    #   Creates an empty plot page
+    
+    # FUNCTION:
+    
+    # Plot:
     plot(c(0, 1), c(0, 1), type = "n", xlab = "", ylab = "", 
         ann = TRUE, axes = FALSE)
+    
+    # Return Value:
     invisible()
 }
 
@@ -41,8 +81,15 @@ function()
 
 
 .subPlot <- 
-function(fun, x, y = NULL, size = c(1, 1), vadj = 0.5, hadj = 0.5, pars = NULL)
+    function(fun, x, y = NULL, size = c(1, 1), vadj = 0.5, hadj = 0.5, 
+    pars = NULL)
 {
+    # Description:
+    #   Creates a sub plot
+    
+    # FUNCTION:
+    
+    # Par:
     old.par <- par(no.readonly=TRUE)
     on.exit(par(old.par))
     
@@ -78,133 +125,137 @@ function(fun, x, y = NULL, size = c(1, 1), vadj = 0.5, hadj = 0.5, pars = NULL)
 .cnvrt.coords <- 
     function(x,y=NULL,input=c('usr','plt','fig','dev','tdev')) 
 {
-
-  input <- match.arg(input)
-  xy <- xy.coords(x,y)
+    # Description:
+    #   Converts Coordinates
+    
+    # FUNCTION:
+    
+    input <- match.arg(input)
+    xy <- xy.coords(x,y)
   
-  cusr <- par('usr')
-  cplt <- par('plt')
-  cfig <- par('fig')
-  cdin <- par('din')
-  comi <- par('omi')
-  cdev <- c(comi[2]/cdin[1],(cdin[1]-comi[4])/cdin[1],
+    cusr <- par('usr')
+    cplt <- par('plt')
+    cfig <- par('fig')
+    cdin <- par('din')
+    comi <- par('omi')
+    cdev <- c(comi[2]/cdin[1],(cdin[1]-comi[4])/cdin[1],
             comi[1]/cdin[2],(cdin[2]-comi[3])/cdin[2])
   
-  if(input=='usr'){
-    usr <- xy
+    if(input=='usr'){
+        usr <- xy
+        
+        plt <- list()
+        plt$x <- (xy$x-cusr[1])/(cusr[2]-cusr[1])
+        plt$y <- (xy$y-cusr[3])/(cusr[4]-cusr[3])
+        
+        fig <- list()
+        fig$x <- plt$x*(cplt[2]-cplt[1])+cplt[1]
+        fig$y <- plt$y*(cplt[4]-cplt[3])+cplt[3]
+        
+        dev <- list()
+        dev$x <- fig$x*(cfig[2]-cfig[1])+cfig[1]
+        dev$y <- fig$y*(cfig[4]-cfig[3])+cfig[3]
+        
+        tdev <- list()
+        tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
+        tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]
+        
+        return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
+    }
+
+    if(input=='plt') {
+
+        plt <- xy
+        
+        usr <- list()
+        usr$x <- plt$x*(cusr[2]-cusr[1])+cusr[1]
+        usr$y <- plt$y*(cusr[4]-cusr[3])+cusr[3]
+        
+        fig <- list()
+        fig$x <- plt$x*(cplt[2]-cplt[1])+cplt[1]
+        fig$y <- plt$y*(cplt[4]-cplt[3])+cplt[3]
+        
+        dev <- list()
+        dev$x <- fig$x*(cfig[2]-cfig[1])+cfig[1]
+        dev$y <- fig$y*(cfig[4]-cfig[3])+cfig[3]
+        
+        tdev <- list()
+        tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
+        tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]
+        
+        return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
+    }
+
+    if(input=='fig') {
+
+        fig <- xy
+        
+        plt <- list()
+        plt$x <- (fig$x-cplt[1])/(cplt[2]-cplt[1])
+        plt$y <- (fig$y-cplt[3])/(cplt[4]-cplt[3])
+        
+        usr <- list()
+        usr$x <- plt$x*(cusr[2]-cusr[1])+cusr[1]
+        usr$y <- plt$y*(cusr[4]-cusr[3])+cusr[3]
+        
+        dev <- list()
+        dev$x <- fig$x*(cfig[2]-cfig[1])+cfig[1]
+        dev$y <- fig$y*(cfig[4]-cfig[3])+cfig[3]
+        
+        tdev <- list()
+        tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
+        tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]
+        
+        return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
+    }
+
+    if(input=='dev'){
+        dev <- xy
+        
+        fig <- list()
+        fig$x <- (dev$x-cfig[1])/(cfig[2]-cfig[1])
+        fig$y <- (dev$y-cfig[3])/(cfig[4]-cfig[3])
+        
+        plt <- list()
+        plt$x <- (fig$x-cplt[1])/(cplt[2]-cplt[1])
+        plt$y <- (fig$y-cplt[3])/(cplt[4]-cplt[3])
+        
+        usr <- list()
+        usr$x <- plt$x*(cusr[2]-cusr[1])+cusr[1]
+        usr$y <- plt$y*(cusr[4]-cusr[3])+cusr[3]
+        
+        tdev <- list()
+        tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
+        tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]  
+        
+        return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
+    }
+
+    if(input=='tdev'){
+        tdev <- xy
     
-    plt <- list()
-    plt$x <- (xy$x-cusr[1])/(cusr[2]-cusr[1])
-    plt$y <- (xy$y-cusr[3])/(cusr[4]-cusr[3])
-
-    fig <- list()
-    fig$x <- plt$x*(cplt[2]-cplt[1])+cplt[1]
-    fig$y <- plt$y*(cplt[4]-cplt[3])+cplt[3]
-
-    dev <- list()
-    dev$x <- fig$x*(cfig[2]-cfig[1])+cfig[1]
-    dev$y <- fig$y*(cfig[4]-cfig[3])+cfig[3]
+        dev <- list()
+        dev$x <- (tdev$x-cdev[1])/(cdev[2]-cdev[1])
+        dev$y <- (tdev$y-cdev[3])/(cdev[4]-cdev[3])
+        
+        fig <- list()
+        fig$x <- (dev$x-cfig[1])/(cfig[2]-cfig[1])
+        fig$y <- (dev$y-cfig[3])/(cfig[4]-cfig[3])
     
-    tdev <- list()
-    tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
-    tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]
+        plt <- list()
+        plt$x <- (fig$x-cplt[1])/(cplt[2]-cplt[1])
+        plt$y <- (fig$y-cplt[3])/(cplt[4]-cplt[3])
     
-    return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
-  }
-
-  if(input=='plt') {
-
-    plt <- xy
-
-    usr <- list()
-    usr$x <- plt$x*(cusr[2]-cusr[1])+cusr[1]
-    usr$y <- plt$y*(cusr[4]-cusr[3])+cusr[3]
-
-    fig <- list()
-    fig$x <- plt$x*(cplt[2]-cplt[1])+cplt[1]
-    fig$y <- plt$y*(cplt[4]-cplt[3])+cplt[3]
-
-    dev <- list()
-    dev$x <- fig$x*(cfig[2]-cfig[1])+cfig[1]
-    dev$y <- fig$y*(cfig[4]-cfig[3])+cfig[3]
-
-    tdev <- list()
-    tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
-    tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]
+        usr <- list()
+        usr$x <- plt$x*(cusr[2]-cusr[1])+cusr[1]
+        usr$y <- plt$y*(cusr[4]-cusr[3])+cusr[3]
     
-    return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
-  }
-
-  if(input=='fig') {
-
-    fig <- xy
-
-    plt <- list()
-    plt$x <- (fig$x-cplt[1])/(cplt[2]-cplt[1])
-    plt$y <- (fig$y-cplt[3])/(cplt[4]-cplt[3])
-
-    usr <- list()
-    usr$x <- plt$x*(cusr[2]-cusr[1])+cusr[1]
-    usr$y <- plt$y*(cusr[4]-cusr[3])+cusr[3]
-
-    dev <- list()
-    dev$x <- fig$x*(cfig[2]-cfig[1])+cfig[1]
-    dev$y <- fig$y*(cfig[4]-cfig[3])+cfig[3]
-
-    tdev <- list()
-    tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
-    tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]
+        tdev <- list()
+        tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
+        tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]  
     
-    return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
-  }
-
-  if(input=='dev'){
-    dev <- xy
-
-    fig <- list()
-    fig$x <- (dev$x-cfig[1])/(cfig[2]-cfig[1])
-    fig$y <- (dev$y-cfig[3])/(cfig[4]-cfig[3])
-
-    plt <- list()
-    plt$x <- (fig$x-cplt[1])/(cplt[2]-cplt[1])
-    plt$y <- (fig$y-cplt[3])/(cplt[4]-cplt[3])
-
-    usr <- list()
-    usr$x <- plt$x*(cusr[2]-cusr[1])+cusr[1]
-    usr$y <- plt$y*(cusr[4]-cusr[3])+cusr[3]
-
-    tdev <- list()
-    tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
-    tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]  
-
-    return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
-  }
-
-  if(input=='tdev'){
-    tdev <- xy
-
-    dev <- list()
-    dev$x <- (tdev$x-cdev[1])/(cdev[2]-cdev[1])
-    dev$y <- (tdev$y-cdev[3])/(cdev[4]-cdev[3])
-    
-    fig <- list()
-    fig$x <- (dev$x-cfig[1])/(cfig[2]-cfig[1])
-    fig$y <- (dev$y-cfig[3])/(cfig[4]-cfig[3])
-
-    plt <- list()
-    plt$x <- (fig$x-cplt[1])/(cplt[2]-cplt[1])
-    plt$y <- (fig$y-cplt[3])/(cplt[4]-cplt[3])
-
-    usr <- list()
-    usr$x <- plt$x*(cusr[2]-cusr[1])+cusr[1]
-    usr$y <- plt$y*(cusr[4]-cusr[3])+cusr[3]
-
-    tdev <- list()
-    tdev$x <- dev$x*(cdev[2]-cdev[1])+cdev[1]
-    tdev$y <- dev$y*(cdev[4]-cdev[3])+cdev[3]  
-
-    return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
-  }
+        return( list( usr=usr, plt=plt, fig=fig, dev=dev, tdev=tdev ) )
+    }
   
 }
 
