97ea331faf02b9e124073736db4e443113da3884
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-02-08 14:23:36 +0000

    [.timeSeries accepts now date object as argument
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2834 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fSeries/R/subset.R
+++ pkg/fSeries/R/subset.R
@@ -6,16 +6,16 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - 2007, Diethelm Wuertz, GPL
 #   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
 #   info@rmetrics.org
@@ -34,78 +34,82 @@
 #  windows.timeSeries        Windows a piece from a 'timeSeries' object.
 #  head.timeSeries           Returns the head of a 'timeSeries' object
 #  tail.timeSeries           Returns the tail of a 'timeSeries' object
-#  outlier.timeSeries        Removes outliers from a 'timeSeries' object  
+#  outlier.timeSeries        Removes outliers from a 'timeSeries' object
 ################################################################################
 
 
 "[.timeSeries" =
-function(x, i = min(1, nrow(x@Data)):nrow(x@Data), 
+    function(x, i = min(1, nrow(x@Data)):nrow(x@Data),
 j = min(1, ncol(x@Data)):ncol(x@Data))
 {   # A function implemented by Diethelm Wuertz
 
     # Description:
     #   Extracts or replaces subsets from 'timeSeries' objects
-    
+
     # Arguments:
     #   x - a 'timeSeries' object
     #   i, j - subset indexes.
-    
+
     # Value:
     #   Returns a subset from an object 'timeSeries'.
 
     # FUNCTION:
-    
+
+
+
     # Check Timezone:
-    TZ = Sys.getenv("TZ")  
+    TZ = Sys.getenv("TZ")
     if (TZ[[1]] != "GMT") {
         Sys.setenv(TZ = "GMT")
         TZ.RESET = TRUE
     } else {
         TZ.RESET = FALSE
     }
-    
+
     # Subsets:
     if(missing(i)) { i <- min(1, nrow(x@Data)):nrow(x@Data) }
     if(missing(j)) { j <- min(1, ncol(x@Data)):ncol(x@Data) }
-        
+
+    if (!is.numeric(i)) i <- as.character(i)
+
     # Subset:
     subx <- x@Data[i, j, drop = FALSE]
     x@Data = subx
     x@positions = x@positions[i]
     x@units = colnames(subx)
-    
+
     # Record IDs:
     if (sum(dim(x@recordIDs)) > 0) {
         x@recordIDs <- x@recordIDs[i, , drop = FALSE]
     }
-        
-    
+
+
     # Return Value:
     if (TZ.RESET) Sys.setenv(TZ = TZ)
     x
-}         
+}
 
 
 # ------------------------------------------------------------------------------
 
 
-cut.timeSeries = 
-function (x, from, to, ...) 
+cut.timeSeries =
+function (x, from, to, ...)
 {   # A function implemented by Diethelm Wuertz
 
     # Description:
     #   Cuts out a piece from a 'timeSeries' object.
-    
+
     # Arguments:
     #   x - a 'timeSeries' object
-    #   from, to - two 'timeDate' position vectors which size the 
+    #   from, to - two 'timeDate' position vectors which size the
     #       blocks
-    
+
     # Value:
     #   Returns a S4 object of class 'timeSeries'.
 
     # FUNCTION:
-    
+
     from = timeDate(from)
     to = timeDate(to)
     Positions = seriesPositions(x)
@@ -119,48 +123,48 @@ function (x, from, to, ...)
     x@units = Units
     x@recordIDs = data.frame()
     colnames(x@Data) = colNames
-    
+
     # Return value:
     x
 }
 
 
-.cut.timeSeries = 
+.cut.timeSeries =
 function(x, from, to, ...)
 {   # A function implemented by Diethelm Wuertz
 
     # Description:
     #   Cuts out a piece from a 'timeSeries' object.
-    
+
     # Arguments:
     #   x - a 'timeSeries' object
-    #   from, to - two 'timeDate' position vectors which size the 
+    #   from, to - two 'timeDate' position vectors which size the
     #       blocks
-    
+
     # Value:
     #   Returns a S4 object of class 'timeSeries'.
 
     # FUNCTION:
-    
+
     # Check:
     stopifnot(is.timeSeries(x))
     if (!is(from, "timeDate"))
         from = as.timeDate(x, zone = x@FinCenter, FinCenter = x@FinCenter)
     if (!is(to, "timeDate"))
         to = as.timeDate(x, zone = x@FinCenter, FinCenter = x@FinCenter)
-    
-    Positions = seriesPositions(x)   
+
+    Positions = seriesPositions(x)
     if (missing(from)) from = Positions[1]
     if (missing(to)) to = rev(Positions)[1]
     Positions = as.POSIXct(Positions, tz = "GMT")
     from = as.POSIXct(from, tz = "GMT")
     to = as.POSIXct(to, tz = "GMT")
-    
+
     # Cut:
     test = (Positions >= from & Positions <= to)
     Index = (1:length(test))[test]
     if (length(Index) == 0) return()
-    
+
     # Return value:
     x[Index, ]
 }
@@ -169,26 +173,26 @@ function(x, from, to, ...)
 # ------------------------------------------------------------------------------
 
 
-window.timeSeries =  
-function (x, from, to, ...) 
+window.timeSeries =
+function (x, from, to, ...)
 {   # A function implemented by Diethelm Wuertz
 
     # Description:
     #   Windows a piece from a 'timeSeries' object.
-    
+
     # Arguments:
     #   x - a 'timeSeries' object
-    #   from, to - two 'timeDate' position vectors which size the 
+    #   from, to - two 'timeDate' position vectors which size the
     #       blocks
-    
+
     # Details:
     #   from and to, are both included in the window.
-    
+
     # Value:
     #   Returns a S4 object of class 'timeSeries'.
 
     # FUNCTION:
-    
+
     from = timeDate(from)
     to = timeDate(to)
     Positions = seriesPositions(x)
@@ -202,7 +206,7 @@ function (x, from, to, ...)
     x@units = Units
     x@recordIDs = data.frame()
     colnames(x@Data) = colNames
-    
+
     # Return value:
     x
 }
@@ -211,32 +215,32 @@ function (x, from, to, ...)
 # ------------------------------------------------------------------------------
 
 
-head.timeSeries = 
+head.timeSeries =
 function(x, n = 6, recordIDs = FALSE, ...)
 {   # A function implemented by Diethelm Wuertz
-    
+
     # Description:
     #   Returns the head of a 'timeSeries' object
-    
+
     # Arguments:
     #   x - a 'timeSeries' object.
-    
+
     # Value:
     #   Returns the head of an object of class 'timeSeries'.
 
     # FUNCTION:
-    
+
     # Head:
     if (recordIDs) {
         if (dim(x@Data)[1] == dim(x@recordIDs)[1]) {
             Head = head(cbind(x@Data, as.matrix(x@recordIDs)), n = n, ...)
         } else {
             Head = head(x@Data, n = n, ...)
-        }  
+        }
     } else {
         Head = head(x@Data, n = n, ...)
     }
-    
+
     # Return Value:
     Head
 }
@@ -245,32 +249,32 @@ function(x, n = 6, recordIDs = FALSE, ...)
 # ------------------------------------------------------------------------------
 
 
-tail.timeSeries = 
+tail.timeSeries =
 function(x, n = 6, recordIDs = FALSE, ...)
 {   # A function implemented by Diethelm Wuertz
-    
+
     # Description:
     #   Returns the tail of a 'timeSeries' object
-    
+
     # Arguments:
     #   x - a 'timeSeries' object.
-    
+
     # Value:
     #   Returns the tail of an object of class 'timeSeries'.
- 
+
     # FUNCTION:
-    
+
     # Tail:
     if (recordIDs) {
         if (dim(x@Data)[1] == dim(x@recordIDs)[1]) {
             Tail = tail(cbind(x@Data, as.matrix(x@recordIDs)), n = n, ...)
         } else {
             Tail = tail(x@Data, n = n, ...)
-        }  
+        }
     } else {
         Tail = tail(x@Data, n = n, ...)
     }
-    
+
     # Return Value:
     Tail
 }
@@ -279,28 +283,28 @@ function(x, n = 6, recordIDs = FALSE, ...)
 # ------------------------------------------------------------------------------
 
 
-outlier.timeSeries = 
-function(x, sd = 10, complement = TRUE, ...) 
+outlier.timeSeries =
+function(x, sd = 10, complement = TRUE, ...)
 {   # A function implemented by Diethelm Wuertz
-    
+
     # Description:
     #   Returns outliers in a timeSeries object or the complement
-    
+
     # Arguments:
     #   x - a 'timeSeries' object.
     #   sd - a numeric value of standard deviations, e.g. 10
-    #       means that values larger or smaller tahn ten 
+    #       means that values larger or smaller tahn ten
     #       times the standard deviation of the series will
     #       be removed.
     #   complement - a logical flag, should the outler series
     #       or its complement be returns.
 
     # FUNCTION:
-    
+
     # Check if univariate Series:
-    if (dim(x@Data)[2] != 1) 
+    if (dim(x@Data)[2] != 1)
         stop("Supports only univariate timeSeries Objects")
-    
+
     # Find Outliers:
     SD = sd * sd(x@Data)
     if (complement) {
@@ -308,7 +312,7 @@ function(x, sd = 10, complement = TRUE, ...)
     } else {
         x = x[abs(x@Data) > SD]
     }
-    
+
     # Return Value:
     x
 }
