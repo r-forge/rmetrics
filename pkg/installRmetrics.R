c14e69bb72afa1d0f274737ba9a93400d9f33ca1
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-02-19 08:58:55 +0000

    updated installRmetrics script with url to CRAN repository and with installation of suggested packages for Rmetrics.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@2896 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/installRmetrics.R
+++ pkg/installRmetrics.R
@@ -10,10 +10,12 @@
 ##
 ## This installs all Rmetrics packages from source and installs dependent
 ## packages from CRAN server if they are not already installed.
+## Suggested packages are installed if "suggests = TRUE"
 ################################################################################
 
 installRmetrics  <-
-    function(repoRmetrics = NULL, ...)
+    function(repoRmetrics = NULL, repoCRAN = "http://stat.ethz.ch/CRAN/",
+             suggests = TRUE)
 {
 
     file <- "Rmetrics/DESCRIPTION"
@@ -25,44 +27,38 @@ installRmetrics  <-
     pkgsDepends <- NULL
     for (i in seq(length(fileDepends))) {
         pkgInfoDepends <-
-           tools:::.split_description(tools:::.read_description(fileDepends[i]))
+            tools:::.split_description(tools:::.read_description(fileDepends[i]))
         pkgsDepends <- c(pkgsDepends, names(pkgInfoDepends$Depends))
+        if (suggests)
+            pkgsDepends <- c(pkgsDepends, names(pkgInfoDepends$Suggests))
     }
 
-    # remove Rmetrics packages
-    pkgsDepends <- pkgsDepends[!(pkgsDepends %in% pkgs)]
-
-    # remove double entries
-    # ...
+    # remove Rmetrics packages and double entries
+    pkgsDepends <- unique(pkgsDepends[!(pkgsDepends %in% pkgs)])
 
     # Remove Rdonlp2 from list because it is installed from local directory
     pkgsDepends <- pkgsDepends[(pkgsDepends != "Rdonlp2")]
 
     # disable unnecessary warning message when package is not installed
     ow <- options("warn")
-
-    # install third packages only not already installed
+    options(warn = -1)
+    # install third packages if not already installed
     for (i in seq(length(pkgsDepends))) {
-        options(warn = -1)
         if (!require(pkgsDepends[i], character.only = TRUE, quietly = TRUE)) {
-            options(ow)
-            install.packages(pkgsDepends[i], ...)
+            install.packages(pkgsDepends[i], repos = repoCRAN)
         }
     }
     # Note Rdonlp2 is not part of Rmetrics !!
-    options(warn = -1)
     if (!require(Rdonlp2, quietly = TRUE)) {
-        options(ow)
-        install.packages("Rdonlp2", repos = repoRmetrics, type = "source", ...)
+        install.packages("Rdonlp2", repos = repoRmetrics, type = "source")
     }
-
     options(ow) # set default warning option
 
     # install Rmetrics packages from local files
-    install.packages(pkgs, repos = repoRmetrics, type = "source", ...)
+    install.packages(pkgs, repos = repoRmetrics, type = "source")
 
     # install Rmetrics package
-    install.packages("Rmetrics", repos = repoRmetrics, type = "source", ...)
+    install.packages("Rmetrics", repos = repoRmetrics, type = "source")
 
     # Return
     return (require("Rmetrics"))
