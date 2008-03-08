bf147503346f3271ab5f75197be4a7346a2ca0f3
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-03-08 10:27:18 +0000

    changed seriesPosition with function time
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3030 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fImport/R/yahooImport.R
+++ pkg/fImport/R/yahooImport.R
@@ -6,16 +6,16 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - 2008, Diethelm Wuertz, Rmetrics Foundation, GPL
 #   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
 #   info@rmetrics.org
@@ -29,8 +29,8 @@
 
 ################################################################################
 # FUNCTION:             DESCRIPTION:
-#  yahooImport           Downloads market data from Yahoo's web site  
-#  yahooSeries           Easy to use download from Yahoo  
+#  yahooImport           Downloads market data from Yahoo's web site
+#  yahooSeries           Easy to use download from Yahoo
 # FUNCTION:             IMPORT STATISTICS - EXPERIMENTAL:
 #  keystatsImport        Downloads key statistics from Yahoo's web site
 #  briefingImport        Downloads briefings from Yahoo's Internet site
@@ -39,27 +39,27 @@
 
 ################################################################################
 # FUNCTION:             IMPORT DATA FUNCTIONS:
-#  yahooImport           Downloads market data from Yahoo's web site 
+#  yahooImport           Downloads market data from Yahoo's web site
 #  yahooSeries           Easy to use download from Yahoo
 
 
-yahooImport <-  
-    function (query, file = "tempfile", 
-    source = "http://chart.yahoo.com/table.csv?", save = FALSE, sep = ";", 
+yahooImport <-
+    function (query, file = "tempfile",
+    source = "http://chart.yahoo.com/table.csv?", save = FALSE, sep = ";",
     swap = 20, try = TRUE)
-{   
+{
     # A function implemented by Diethelm Wuertz
 
     # Description:
-    #   Downloads market data from Yahoo's web site  
-    
+    #   Downloads market data from Yahoo's web site
+
     # Example:
     #   IBM SHARES, test 19/20 century change 01-12-1999 -- 31-01-2000:
     #   yahooImport("s=IBM&a=11&b=1&c=1999&d=0&e=31&f=2000&g=d&x=.csv")
 
     # Notes:
     #   Requires: fields() cuts a string in fields
-    #   Yahoo Token Description:           
+    #   Yahoo Token Description:
     #   s     Selected Ticker-Symbol
     #   a     First Quote starts with Month (mm): 0-11, Jan-Dec
     #   b     First Quote starts with Day (dd)
@@ -69,159 +69,159 @@ yahooImport <-
     #   f     Last Quote ends with Year (yy): as CCYY
     #   r     Aggregation Level
     #   z     Selected Ticker-Symbol [optional]
-    
+
     # Changes:
     #   2007-02-18 DW: Update to new %Y-%m-%d format
-    
+
     # FUNCTION:
-    
+
     # Download:
     if (try) {
         # First try if the Internet can be accessed:
-        z = try(yahooImport(file = file, source = source, 
+        z = try(yahooImport(file = file, source = source,
             query = query, save = save, try = FALSE))
         if (inherits(z, "try-error") || inherits(z, "Error")) {
-            return("No Internet Access") 
+            return("No Internet Access")
         } else {
-            return(z) 
+            return(z)
         }
     } else {
         # For S-Plus Compatibility:
         method <- if(is.R()) NULL else "wget"
-    
+
         # Download the file:
         url = paste(source, query, sep = "")
         download.file(url = url, destfile = file, method = method)
-        
+
         # Read data and revert:
         x = X = read.table(file, header = TRUE, sep = ",")
         n = dim(x)[1]
         x = x[n:1, ]
-        
+
         # Result:
         colnames(x)[1] <- "%Y-%m-%d"
         ### rownames(x) = 1:n
         z = data.frame(x)
-   
+
         # Save Download ?
         colNames = colnames(z)[-1]
         if (save) {
             # Header:
-            write.table(t(c("%Y-%m-%d", colNames)), file, quote = FALSE, 
+            write.table(t(c("%Y-%m-%d", colNames)), file, quote = FALSE,
                 row.names = FALSE, col.names = FALSE, sep = ";")
             # Data:
-            write.table(z, file, quote = FALSE, append = TRUE, 
-                col.names = FALSE, row.names = FALSE, sep = ";") 
+            write.table(z, file, quote = FALSE, append = TRUE,
+                col.names = FALSE, row.names = FALSE, sep = ";")
             # Check:
             # read.table(file, header = TRUE, sep = ";")
         } else {
-            unlink(file) 
-        } 
-        
+            unlink(file)
+        }
+
         # Return Value:
-        ans = new("fWEBDATA",     
+        ans = new("fWEBDATA",
             call = match.call(),
             param = c("Instrument Query" = query),
-            data = z, 
-            title = "Web Data Import from Yahoo", 
+            data = z,
+            title = "Web Data Import from Yahoo",
             description = as.character(date()) )
         return(ans)
     }
-    
+
     # Return Value:
     invisible()
 }
 
-    
+
 # ------------------------------------------------------------------------------
 
 
-yahooSeries <-  
-    function(symbols = c("^DJI", "IBM"), from = NULL, to = NULL, 
-    nDaysBack = 365, quote = c("Open", "High", "Low", "Close", "Volume"), 
-    aggregation = c("d", "w", "m"), returnClass = c("timeSeries", "ts", 
+yahooSeries <-
+    function(symbols = c("^DJI", "IBM"), from = NULL, to = NULL,
+    nDaysBack = 365, quote = c("Open", "High", "Low", "Close", "Volume"),
+    aggregation = c("d", "w", "m"), returnClass = c("timeSeries", "ts",
     "matrix", "data.frame"), getReturns = FALSE, ...)
-{   
+{
     # A function implemented by Diethelm Wuertz
 
     # Description:
     #   Downloads easily time series data from Yahoo
-    
+
     # Arguments:
-    #   symbols - a character string value or vector, the Yahoo 
+    #   symbols - a character string value or vector, the Yahoo
     #       symbol name(s).
-    #   from - an ISO-8601 formatted character string of the starting 
+    #   from - an ISO-8601 formatted character string of the starting
     #       date, e.g. "2005-01-01".
     #   to - ISO formatted character string of the end date,
     #       e.g. "2005-12-31".
-    #   ndaysBack - an integer giving the length of the download 
+    #   ndaysBack - an integer giving the length of the download
     #       period in number of days starting n days back from today.
     #       Only in use if 'from' and 'to' are not specified.
-    #   quote - a character value or vector of strings giving the 
+    #   quote - a character value or vector of strings giving the
     #       column names of those instruments to be extracted from
-    #       the download. 
+    #       the download.
     #   aggregation - a character string denoting the aggregation
-    #       level of the downloaded data records, 'd' for daily, 'w' 
+    #       level of the downloaded data records, 'd' for daily, 'w'
     #       for weekly and 'm' for monthly data records.
     #   returns - a logical flag. Should return values be computed
     #       using the function 'returnSeries'?
     #   returnClass = a character string which decides how the downloaded
     #       time series will be returned. By default an object of
     #       class 'timeSeries' will be returned .
-    
+
     # Examples:
     #   yahooSeries(symbols = "IBM", aggregation = "w")
     #   yahooSeries(symbols = c("^DJI", "IBM"))
     #   yahooSeries(symbols = c("^DJI", "IBM"), aggregation = "w")
     #   yahooSeries(aggregation = "m", nDaysBack = 10*366)
     #   yahooSeries(returnSeries = TRUE)
-    
+
     # FUNCTION:
-    
+
     # Match Arguments:
     returnClass = match.arg(returnClass)
     aggregation = match.arg(aggregation)
-    
+
     # Internal Univariate Download Function:
-    # symbol = "IBM", from = NULL, to = NULL, nDaysBack = 365, 
-    # quote = "Close", aggregation = c("d", "w", "m"), 
-    # returnClass = c("timeSeries", "zoo", "ts")) 
- 
-    # Automatic Selection of From / To: 
+    # symbol = "IBM", from = NULL, to = NULL, nDaysBack = 365,
+    # quote = "Close", aggregation = c("d", "w", "m"),
+    # returnClass = c("timeSeries", "ts", "matrix", "data.frame")
+
+    # Automatic Selection of From / To:
     if (is.null(from) & is.null(to)) {
-        to = Sys.Date() 
+        to = Sys.Date()
         from = as.character(to - nDaysBack)
         to = as.character(to) }
-    
+
     # Extract Atoms - From:
     yearFrom = substring(from, 1, 4)
     monthFrom = as.character(as.integer(substring(from, 6,7))-1)
     dayFrom = substring(from, 9, 10)
-    
+
     # Extract Atoms - To:
     yearTo = substring(to, 1, 4)
     monthTo = as.character(as.integer(substring(to, 6,7))-1)
     dayTo = substring(to, 9, 10)
-    
+
     # Download:
     for (i in 1:length(symbols)) {
-        query = paste("s=", symbols[i], "&a=", monthFrom, "&b=", dayFrom, 
-            "&c=", yearFrom, "&d=", monthTo, "&e=", dayTo, "&f=", yearTo, 
-            "&g=", aggregation[1], "&x=.csv", sep = "")  
+        query = paste("s=", symbols[i], "&a=", monthFrom, "&b=", dayFrom,
+            "&c=", yearFrom, "&d=", monthTo, "&e=", dayTo, "&f=", yearTo,
+            "&g=", aggregation[1], "&x=.csv", sep = "")
         imported = yahooImport(query)@data
         charvec = as.character(imported[, 1])
         data = imported[, quote]
         Y = timeSeries(data, charvec)
         UNITS = paste(symbols[i], ".", quote, sep = "")
-        if (aggregation == "d") Y = alignDailySeries(Y, ...) 
+        if (aggregation == "d") Y = alignDailySeries(Y, ...)
         Y@units = UNITS
         colnames(Y@Data) = UNITS
         if (i == 1) X = Y else X = merge(X, Y)
     }
-    
+
     # Compute Return Series ?
-    if (getReturns) X = returnSeries(X, ...)  
-    
+    if (getReturns) X = returnSeries(X, ...)
+
     # Return as Object ?
     if (returnClass == "matrix") {
         X = X@data
@@ -230,7 +230,7 @@ yahooSeries <-
     } else if (returnClass == "ts") {
         X = as.ts(X@Data)
     }
-    
+
     # Return Value:
     X
 }
@@ -238,18 +238,18 @@ yahooSeries <-
 
 ################################################################################
 # FUNCTION:             IMPORT STATISTICS - EXPERIMENTELL:
-#  keystatsImport       Downloads key statistics from Yahoo's web site 
+#  keystatsImport       Downloads key statistics from Yahoo's web site
 
 
-keystatsImport <-   
-    function (query, file = "tempfile", source = 
-    "http://finance.yahoo.com/q/ks?s=", save = FALSE, try = TRUE) 
-{   
+keystatsImport <-
+    function (query, file = "tempfile", source =
+    "http://finance.yahoo.com/q/ks?s=", save = FALSE, try = TRUE)
+{
     # A function implemented by Diethelm Wuertz and Matthew C.Keller
 
     # Description:
     #   Downloads Key Statistics on shares from Yahoo's Internet site
-    
+
     # Example:
     #   keystatsImport("YHOO")
     #   keystatsImport("IBM")
@@ -257,16 +257,16 @@ keystatsImport <-
     #       query = "IBM"
     #       file = "tempfile"; source = "http://finance.yahoo.com/q/ks?s="
     #       save = FALSE; try = TRUE; method = NULL
-    
+
     # Changes:
     #   2006-08-26 update by MCK
-    
+
     # FUNCTION:
-    
+
     # Download:
     if (try) {
         # First try if the Internet can be accessed:
-        z = try(keystatsImport(file = file, source = source, 
+        z = try(keystatsImport(file = file, source = source,
             query = query, save = save, try = FALSE))
         if (class(z) == "try-error" || class(z) == "Error") {
             return("No Internet Access")
@@ -281,15 +281,15 @@ keystatsImport <-
         } else {
             method = "wget"
         }
-        
+
         # Download and Scan:
         url = paste(source, query, sep = "")
         download.file(url = url, destfile = file, method = method)
         x = scan(file, what = "", sep = "\n")
-        
+
         # Extract Data Records:
         x = x[grep("datamodoutline1", x)]
-        
+
         # Clean up HTML:
         x = gsub("/", "", x, perl = TRUE)
         x = gsub(" class=.yfnc_datamodoutline1.", "", x, perl = TRUE)
@@ -318,22 +318,22 @@ keystatsImport <-
         if (length(Index) > 0) x = x[-Index]
         x = gsub(" $", "", x, perl = TRUE)
         x = gsub(":$", ":NA", x, perl = TRUE)
-        
+
         # If there are two ":" in a line ...
         x = sub(":", "@", x)
         x = sub(":", "/", x)
-        
+
         # Convert to matrix:
         x = matrix(unlist(strsplit(x, "@" )), byrow = TRUE, ncol = 2)
-        
+
         # Add Current Date:
         stats = as.character(Sys.Date())
         x = rbind(c("Symbol", query), c("Date", stats), x)
         X = as.data.frame(x[, 2])
-        rownames(X) = x[, 1] 
+        rownames(X) = x[, 1]
         colnames(X) = "Value"
     }
-    
+
     # Return Value:
     X
 }
@@ -342,15 +342,15 @@ keystatsImport <-
 # ------------------------------------------------------------------------------
 
 
-briefingImport <-   
-    function (query, file = "tempfile", source = 
-    "http://finance.yahoo.com/q/ud?s=", save = FALSE, try = TRUE) 
-{   
+briefingImport <-
+    function (query, file = "tempfile", source =
+    "http://finance.yahoo.com/q/ud?s=", save = FALSE, try = TRUE)
+{
     # A function implemented by Diethelm Wuertz and Matthew C.Keller
 
     # Description:
     #   Downloads Key Statistics on shares from Yahoo's Internet site
-    
+
     # Example:
     #   briefingImport("YHOO")
     #   briefingImport("IBM")
@@ -358,13 +358,13 @@ briefingImport <-
     #       query = "IBM"
     #       file = "tempfile"; source = "http://finance.yahoo.com/q/ks?s="
     #       save = FALSE; try = TRUE; method = NULL
-    
+
     # FUNCTION:
-    
+
     # Download:
     if (try) {
         # First try if the Internet can be accessed:
-        z = try(briefingImport(file = file, source = source, 
+        z = try(briefingImport(file = file, source = source,
             query = query, save = save, try = FALSE))
         if (class(z) == "try-error" || class(z) == "Error") {
             return("No Internet Access")
@@ -379,12 +379,12 @@ briefingImport <-
         } else {
             method = "wget"
         }
-        
+
         # Download:
         url = paste(source, query, sep = "")
         download.file(url = url, destfile = file, method = method)
         x = scan(file, what = "", sep = "\n")
-        
+
         # Extract Data Records:
         x = x[grep("Briefing.com", x)]
 
@@ -405,9 +405,9 @@ briefingImport <-
         x = gsub("<td nowrap", "", x, perl = TRUE)
         x = gsub("<td height=....", "", x, perl = TRUE)
         x = gsub("&amp;", "&", x, perl = TRUE)
-        
+
         x = unlist(strsplit(x, ">"))
-        
+
         x = x[ grep("-...-[90]", x, perl = TRUE) ]
         nX = length(x)
         # The last record has an additional @, remove it ...
@@ -420,11 +420,11 @@ briefingImport <-
         x = x[nrow(x):1, ]
         X = as.data.frame(x)
     }
-    
+
     # Return Value:
     X
-}       
-        
+}
+
 
 ################################################################################
 
