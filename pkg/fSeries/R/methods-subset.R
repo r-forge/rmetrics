e87c35942f1aa8d43bdc8ebefa55ac528f76f09d
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-04-01 14:49:51 +0000

    subbseting a timeSeries now behaves more or less the same as a matrix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3127 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fSeries/R/methods-subset.R
+++ pkg/fSeries/R/methods-subset.R
@@ -24,147 +24,11 @@
 #  outlier.timeSeries        Removes outliers from a 'timeSeries' object
 ################################################################################
 
-## "[.timeSeries" <-
-##     function(x, i , j)
-## {   # A function implemented by Diethelm Wuertz
-##     # Modified by Yohan Chalabi
+# ------------------------------------------------------------------------------
+# index
 
-##     # Description:
-##     #   Extracts or replaces subsets from 'timeSeries' objects
-
-##     # Arguments:
-##     #   x - a 'timeSeries' object
-##     #   i, j - subset indexes.
-
-##     # Value:
-##     #   Returns a subset from an object 'timeSeries'.
-
-##     # FUNCTION:
-
-##     # Check Timezone:
-##     TZ = Sys.getenv("TZ")
-##     if (TZ[[1]] != "GMT") {
-##         Sys.setenv(TZ = "GMT")
-##         TZ.RESET = TRUE
-##     } else {
-##         TZ.RESET = FALSE
-##     }
-
-##     # Subsets:
-##     if(missing(i)) { i <- min(1, nrow(x@Data)):nrow(x@Data) }
-##     if(missing(j)) { j <- min(1, ncol(x@Data)):ncol(x@Data) }
-
-##     if (is.timeSeries(i) && is.logical(i@Data))
-##         i <- as.logical(i@Data)
-
-##     if (!is.numeric(i) && !is.logical(i))
-##         i <- as.character(i)
-
-##     # Subset:
-##     subx <- x@Data[i, j, drop = FALSE]
-##     x@Data = subx
-##     x@positions = x@positions[i]
-##     x@units = as.character(colnames(subx))
-##     # YC as.character important if colnames == NULL
-
-##     # Record IDs:
-##     if (sum(dim(x@recordIDs)) > 0) {
-##         x@recordIDs <- x@recordIDs[i, , drop = FALSE]
-##     }
-
-
-##     # Return Value:
-##     if (TZ.RESET) Sys.setenv(TZ = TZ)
-##     x
-## }
-
-setMethod("[", signature(x = "timeSeries", i = "missing", j = "missing", drop = "ANY"), function(x, i, j, drop, ...) x)
-
-setMethod("[", signature(x = "timeSeries", i = "index", j = "missing", drop = "missing"), function(x, i, j, drop, ...)
-      {
-          if(nargs() == 2) { ## e.g. M[0] , M[TRUE],  M[1:2]
-              if(any(as.logical(i)) || prod(dim(x)) == 0)
-                   as.vector(x)[i]
-          } else {
-              callGeneric(x, i=i, drop = FALSE)
-          }
-      })
-
-## select columns
-setMethod("[", signature(x = "timeSeries", i = "missing", j = "index", drop = "missing"), function(x,i, j, drop, ...) callGeneric(x, j = j, drop = FALSE))
-
-setMethod("[", signature(x = "timeSeries", i = "index", j = "index", drop = "missing"), function(x,i,j, drop, ...) callGeneric(x, i = i, j = j, drop = FALSE))
-
-setMethod("[", signature(x = "timeSeries", i = "ANY", j = "ANY", drop = "ANY"), function(x,i,j, drop, ...) stop("invalid or not-yet-implemented 'timeSeries' subsetting"))
-
-setMethod("[", signature(x = "timeSeries", i = "matrix", j = "missing", drop = "missing"), function(x, i, j, drop, ...) callGeneric(x, i = i, drop=FALSE))
-
-setMethod("[", signature(x = "timeSeries", i = "matrix", j = "missing", drop = "logical"),
-          function(x, i, j, drop, ...)
-      {
-          i <- as(i, "vector")
-
-          if (!is.logical(i))
-              stop("matrix used as an index must be logical")
-
-          if (NROW(i) == NROW(x))
-              callGeneric(x, i = i , drop = drop, ...)
-          else
-              as.vector(x)[i]
-      })
-
-setMethod("[", signature(x = "timeSeries", i = "timeSeries", j = "missing", drop = "missing"), function(x, i, j, drop, ...) callGeneric(x, i = i, drop=FALSE))
-
-setMethod("[", signature(x = "timeSeries", i = "timeSeries", j = "missing", drop = "logical"),
-          function(x, i, j, drop, ...)
-      {
-          if (x@format != "counts" &&
-              i@format != "counts" &&
-              finCenter(x) != finCenter(i))
-              stop("FinCenter of timeSeries and subset do not match")
-
-          i <- as(i, "vector")
-
-          if (!is.logical(i))
-              stop("timeSeries used as an index must be logical")
-
-          if (NROW(i) == NROW(x))
-              callGeneric(x, i = i , drop = drop, ...)
-          else
-              as.vector(x)[i]
-      })
-
-setMethod("[", signature(x = "timeSeries", i = "timeDate", j = "missing", drop = "missing"), function(x, i, j, drop, ...)
-          callGeneric(x, i=i, j = min(1, NCOL(x)):NCOL(x), drop=FALSE, ...))
-
-setMethod("[", signature(x = "timeSeries", i = "timeDate", j = "index", drop = "missing"), function(x, i, j, drop, ...) callGeneric(x, i=i, j=j, drop=FALSE, ...))
-
-setMethod("[", signature(x = "timeSeries", i = "timeDate", j = "index", drop = "logical"),
-          function(x, i, j, drop, ...)
-      {
-          # series settings
-          format = x@format
-          if (format != "counts") {
-              FinCenter <- finCenter(x)
-              # convert FinCenter of index to FinCenter of timeSeries
-              i <- timeDate(i, zone = i@FinCenter, FinCenter = FinCenter)
-          }
-
-          i <- as(i, "character")
-          callGeneric(x, i=i, j=j, drop=drop)
-      })
-
-setMethod("[", signature(x = "timeSeries", i = "index", j = "missing",
-                         drop = "logical"),
-          function(x, i, j, drop, ...)
-          callGeneric(x, i = i, j = min(1, NCOL(x)):NCOL(x), drop = drop))
-
-setMethod("[", signature(x = "timeSeries", i = "missing", j = "index", drop = "logical"), function(x, i, j, drop, ...)
-          callGeneric(x, i = min(1, NROW(x)):NROW(x), j = j, drop = drop))
-
-setMethod("[", signature(x = "timeSeries", i = "index", j = "index",
-                         drop = "logical"),
-          function(x, i, j, drop, ...)
+setMethod("[", signature(x = "timeSeries", i = "index", j = "index"),
+          function(x, i, j, ..., drop = FALSE)
       {
           # series settings
           format = x@format
@@ -173,7 +37,11 @@ setMethod("[", signature(x = "timeSeries", i = "index", j = "index",
 
           # subset data and positions
           data <- series(x)[i, j, drop = drop]
-          charvec = rownames(data)
+          charvec <- rownames(data)
+
+          # to handle special case when
+          # series(x)[i, j, drop = drop] returns numeric(0)
+          if (!NROW(data) || !NCOL(data)) data <- NULL
 
           # Record IDs:
           df <- x@recordIDs
@@ -184,6 +52,94 @@ setMethod("[", signature(x = "timeSeries", i = "index", j = "index",
               zone = FinCenter, FinCenter = FinCenter,
               recordIDs = recordIDs)
       })
+
+setMethod("[", signature(x = "timeSeries", i = "index", j = "missing"),
+          function(x, i, j, ..., drop = FALSE)
+      {
+          if(nargs() == 2) {
+              if(any(as.logical(i)) || prod(dim(x)) == 0)
+                   as.vector(x)[i]
+          } else {
+              callGeneric(x, i = i, j = min(1, NCOL(x)):NCOL(x),
+                          drop = drop, ...)
+          }
+      })
+
+setMethod("[", signature(x = "timeSeries", i = "missing", j = "index"),
+          function(x, i, j, ..., drop = FALSE)
+          callGeneric(x, i = min(1, NROW(x)):NROW(x), j = j, drop = drop, ...))
+
+# ------------------------------------------------------------------------------
+# timeDate
+
+setMethod("[", signature(x = "timeSeries", i = "timeDate", j = "index"),
+          function(x, i, j, ..., drop = FALSE)
+      {
+          # series settings
+          format = x@format
+          if (format != "counts") {
+              FinCenter <- finCenter(x)
+              # convert FinCenter of index to FinCenter of timeSeries
+              i <- timeDate(i, zone = i@FinCenter, FinCenter = FinCenter)
+          }
+          i <- as(i, "character")
+          callGeneric(x, i=i, j=j, drop=drop, ...)
+      })
+
+setMethod("[", signature(x = "timeSeries", i = "timeDate", j = "missing"),
+          function(x, i, j, ..., drop = FALSE)
+          callGeneric(x, i = i, j = min(1, NCOL(x)):NCOL(x), drop = drop, ...))
+
+# ------------------------------------------------------------------------------
+# matrix
+
+setMethod("[", signature(x = "timeSeries", i = "matrix", j = "index"),
+          function(x, i, j, ..., drop = FALSE)
+          callGeneric(x, i = as.vector(i), j = j, drop = drop, ...))
+
+setMethod("[", signature(x = "timeSeries", i = "matrix", j = "missing"),
+          function(x, i, j, ..., drop = FALSE)
+      {
+          if(nargs() == 2) {
+              if(any(as.logical(i)) || prod(dim(x)) == 0)
+                   as.vector(x)[i]
+          } else {
+              callGeneric(x, i = i, j = min(1, NCOL(x)):NCOL(x),
+                          drop = drop, ...)
+          }
+      })
+
+# ------------------------------------------------------------------------------
+# timeSeries
+
+setMethod("[", signature(x = "timeSeries", i = "timeSeries", j = "index"),
+          function(x, i, j, ..., drop = FALSE)
+      {
+          if (x@format != "counts" &&
+              i@format != "counts" &&
+              finCenter(x) != finCenter(i))
+              stop("FinCenter of timeSeries and subset do not match")
+          callGeneric(x, i = series(i), j = j, drop = drop, ...)
+      })
+
+setMethod("[", signature(x = "timeSeries", i = "timeSeries", j = "missing"),
+          function(x, i, j, ..., drop = FALSE)
+      {
+          if(nargs() == 2) {
+              if(any(as.logical(i)) || prod(dim(x)) == 0)
+                   as.vector(x)[series(i)]
+          } else {
+              callGeneric(x, i = i, j = min(1, NCOL(x)):NCOL(x),
+                          drop = drop, ...)
+          }
+      })
+
+# ------------------------------------------------------------------------------
+# ANY
+
+setMethod("[", signature(x = "timeSeries", i = "ANY", j = "ANY"),
+          function(x,i,j, ..., drop = FALSE)
+          stop("invalid or not-yet-implemented 'timeSeries' subsetting"))
 
 # ------------------------------------------------------------------------------
 
