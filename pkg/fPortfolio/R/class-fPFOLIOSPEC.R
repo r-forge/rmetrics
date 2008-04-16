761a2f27ef6fed95e7334f123f3c73d5c00a6a3f
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-04-16 12:50:35 +0000

    new params and control list argument in optim slot of portfolioSpec
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3213 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fPortfolio/R/class-fPFOLIOSPEC.R
+++ pkg/fPortfolio/R/class-fPFOLIOSPEC.R
@@ -6,16 +6,16 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR Description. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR Description. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA 02111-1307 USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - Diethelm Wuertz, GPL
 #   2007 - Rmetrics Foundation, GPL
 #   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
@@ -29,12 +29,13 @@
 ################################################################################
 
 
-setClass("fPFOLIOSPEC", 
-    representation(
-        model = "list",
-        portfolio = "list",
-        optim = "list")  
-)
+setClass("fPFOLIOSPEC",
+         representation(
+                        model = "list",
+                        portfolio = "list",
+                        optim = "list",
+                        messages = "list")
+         )
 
 
 ################################################################################
--- pkg/fPortfolio/R/portfolioSpec.R
+++ pkg/fPortfolio/R/portfolioSpec.R
@@ -6,16 +6,16 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR Description. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR Description. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA 02111-1307 USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - Diethelm Wuertz, GPL
 #   2007 - Rmetrics Foundation, GPL
 #   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
@@ -29,88 +29,90 @@
 ################################################################################
 
 
-portfolioSpec <- 
+portfolioSpec <-
     function(
-    model = list(
-        type = "MV",                     # Alternatives: "LPM", "CVaR"
-        optimize = "minRisk",            # Alternatives: "maxReturn"
-        estimator = "covEstimator",      # Alternatives: "shrinkEstimator", ...
-                                         #               "lpmEstimator", ...
-        tailRisk = list(),  
-        params = list(alpha = 0.05, a = 1)),
-    portfolio = list(
-        weights = NULL, 
-        targetReturn = NULL, 
-        targetRisk = NULL,
-        riskFreeRate = 0, 
-        nFrontierPoints = 50,
-        status = 0),
-    optim = list(
-        solver = "solveRquadprog",        # Alt: "solveRdonlp2" "solveRlpSolve"
-        trace = FALSE))
-{   
+             model = list(
+             type = "MV", # Alternatives: "LPM", "CVaR"
+             optimize = "minRisk", # Alternatives: "maxReturn"
+             estimator = "covEstimator", # Alt: "shrinkEstimator", "lpmEstimator"
+             tailRisk = list(),
+             params = list(alpha = 0.05, a = 1)),
+             portfolio = list(
+             weights = NULL,
+             targetReturn = NULL,
+             targetRisk = NULL,
+             riskFreeRate = 0,
+             nFrontierPoints = 50,
+             status = 0),
+             optim = list(
+             solver = "solveRquadprog",
+             # Alt: "solveRdonlp2" "solveRlpSolve", "solveRsocp"
+             params = list(),
+             control = list(),
+             trace = FALSE))
+{
     # A function implemented by Rmetrics
 
     # Description:
     #   Specifies a portfolio to be optimized
-    
+
     # Example:
     #   portfolioSpec(portfolio = list(targetReturn = 1.5))
-    
+
     # FUNCTION:
-    
+
     # Compose Checklists:
     # model.type = c("MV", "CVaR")
     # model.estimator.mean = "mean"
     # model.estimator.cov = c("cov", "mcd", "Mcd", "shrink")
     # optim.solver = c("quadprog", "Rdonlp2", "lpSolve")
     # optim.trace = FALSE
-    
+
     # Check Arguments:
     # stopifnot(model$type %in% model.type)
     # stopifnot(model$estimator[1] %in% model.estimator.mean)
     # stopifnot(model$estimator[2] %in% model.estimator.cov)
     # stopifnot(optim$solver %in% optim.solver)
-    
+
     # Model Slot:
     Model = list(
-        type = "MV", 
+        type = "MV",
         optimize = "minRisk",
         estimator = "covEstimator",
         tailRisk = NULL,
         params = list())
     model$type = model$type[1]
     Model[(Names <- names(model))] <- model
-    
+
     # Portfolio Slot:
     Portfolio = list(
-        weights = NULL, 
-        targetReturn = NULL, 
-        targetRisk = NULL, 
-        riskFreeRate = 0, 
+        weights = NULL,
+        targetReturn = NULL,
+        targetRisk = NULL,
+        riskFreeRate = 0,
         nFrontierPoints = 50,
         status = 0)
     Portfolio[(Names <- names(portfolio))] <- portfolio
-    
+
     # Check Portfolio - weights, targetReturn, targetRisk:
     # ... at least two of them must be set to NULL!
     checkPortfolio = 0
     if(!is.null(portfolio$weights)) checkPortfolio = checkPortfolio + 1
     if(!is.null(portfolio$targetReturn)) checkPortfolio = checkPortfolio + 1
     stopifnot(checkPortfolio <= 1)
-  
+
     # Optim Slot:
     Optim = list(
-        solver = "solveRquadprog", 
+        solver = "solveRquadprog",
         trace = FALSE)
     Optim[(Names <- names(optim))] <- optim
-    
+
     # Return Value:
-    new("fPFOLIOSPEC", 
+    new("fPFOLIOSPEC",
         model = Model,
         portfolio = Portfolio,
-        optim = Optim)    
-} 
+        optim = Optim)
+}
 
 
 ################################################################################
