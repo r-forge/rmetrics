5b93c055b5e0fed03623be99889f1e78b66761a1
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-05-05 10:53:04 +0000

    added stopifnot in dayOfWeek
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3254 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fCalendar/R/dayOfWeek.R
+++ pkg/fCalendar/R/dayOfWeek.R
@@ -6,16 +6,16 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - Diethelm Wuertz, GPL
 #   2007 - Rmetrics Foundation, GPL
 #   Diethelm Wuertz <wuertz@phys.ethz.ch>
@@ -35,30 +35,31 @@
 
 dayOfWeek <-
     function(x)
-{   
+{
     # A function implemented by Diethelm Wuertz
 
     # Description:
     #   Returns day of week for time date objects
-    
+
     # Arguments:
     #   x - an object of class "timeDate"
-    
+
     # Example:
     #   weekDay(Sys.timeDate())
     #   weekDay(timeSequence("2005-05-15", "2005-07-15"))
-    
+
     # FUNCTION:
-    
+    stopifnot(is(x, "timeDate"))
+
     # Get Day of Week:
     wd = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
     n = as.POSIXlt(x@Data)$wday + 1
     wdays = wd[n]
     names(wdays) = as.character(x@Data)
-    
+
     # Return Value:
     wdays
-}    
+}
 
 
 ################################################################################
