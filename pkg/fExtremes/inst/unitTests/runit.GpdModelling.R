6088b463b6c0f589e23970d3f8d573b45e36caf0
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-06-10 10:39:12 +0000

    renamed files in unitTests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3332 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fExtremes/inst/unitTests/runit.GpdModelling.R
+++ pkg/fExtremes/inst/unitTests/runit.GpdModelling.R
@@ -6,16 +6,16 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - 2007, Diethelm Wuertz, GPL
 #   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
 #   info@rmetrics.org
@@ -40,20 +40,20 @@
 ################################################################################
 
 
-test.gpdSim = 
+test.gpdSim =
 function()
 {
     # Generate Artificial Data Set:
     x = gpdSim(model = list(xi = 0.25, mu = 0, beta = 1), n = 1000, seed = 4711)
-    class(x) 
-    
+    class(x)
+
     # Plot Series:
     par(mfrow = c(2, 1), cex = 0.7)
     par(ask = FALSE)
     seriesPlot(x)
-      
+
     # Return Value:
-    return()    
+    return()
 }
 
 
@@ -65,7 +65,7 @@ function()
 {
     # Slot names:
     slotNames("fGPDFIT")
-    # [1] "call"        "method"      "parameter"   "data"        "fit"        
+    # [1] "call"        "method"      "parameter"   "data"        "fit"
     # [6] "residuals"   "title"       "description"
 
     # Return Value:
@@ -77,98 +77,98 @@ function()
 # ------------------------------------------------------------------------------
 
 
-test.gpdFit = 
+test.gpdFit =
 function()
 {
     # Generate Artificial Data Set:
     model = list(xi = -0.25, mu = 0, beta = 1)
-    ts = gpdSim(model = model, n = 5000, seed = 4711) 
+    ts = gpdSim(model = model, n = 5000, seed = 4711)
     class(ts)
-    
+
     # Transform As timeSeries:
     tS = as.timeSeries(ts)
     class(tS)
-    
+
     # As numeric vector:
     x = as.vector(ts)
     class(x)
-      
+
     # GPD Fit:
-    # gpdFit(x, u = quantile(x, 0.95), type = c("mle", "pwm"), 
-    #   information = c("observed", "expected"), title = NULL, 
+    # gpdFit(x, u = quantile(x, 0.95), type = c("mle", "pwm"),
+    #   information = c("observed", "expected"), title = NULL,
     #   description = NULL, ...)
-    
+
     # PWM Fit:
-    fit = gpdFit(tS, u = min(tS@Data), "pwm") 
+    fit = gpdFit(tS, u = min(series(tS)), "pwm")
     print(fit)
-    fit = gpdFit(ts, u = min(ts), "pwm") 
+    fit = gpdFit(ts, u = min(ts), "pwm")
     print(fit)
-    fit = gpdFit(x, u = min(x), "pwm") 
+    fit = gpdFit(x, u = min(x), "pwm")
     print(fit)
-       
+
     # MLE Fit:
-    fit = gpdFit(tS, u = min(tS@Data), "mle") 
+    fit = gpdFit(tS, u = min(series(tS)), "mle")
     print(fit)
-    fit = gpdFit(ts, u = min(ts), "mle") 
+    fit = gpdFit(ts, u = min(ts), "mle")
     print(fit)
-    fit = gpdFit(x, u = min(x), "mle") 
+    fit = gpdFit(x, u = min(x), "mle")
     print(fit)
-    
+
     # Information:
-    fit = gpdFit(tS, u = min(tS@Data), type = "mle", information = "observed") 
+    fit = gpdFit(tS, u = min(series(tS)), type = "mle", information = "observed")
     print(fit)
-    fit = gpdFit(tS, u = min(tS@Data), type = "mle", information = "expected") 
+    fit = gpdFit(tS, u = min(series(tS)), type = "mle", information = "expected")
     print(fit)
-                                                     
-    
+
+
     # Return Value:
-    return()    
+    return()
 }
 
 
 # ------------------------------------------------------------------------------
 
 
-test.plot = 
+test.plot =
 function()
 {
     # Artificial Data Set:
     model = list(xi = -0.25, mu = 0, beta = 1)
-    ts = gpdSim(model = model, n = 5000, seed = 4711) 
-    class(ts)   
+    ts = gpdSim(model = model, n = 5000, seed = 4711)
+    class(ts)
 
-    # Fit: 
-    fit = gpdFit(ts, u = min(ts), type = "mle") 
-    print(fit) 
+    # Fit:
+    fit = gpdFit(ts, u = min(ts), type = "mle")
+    print(fit)
     par(mfrow = c(2, 2), cex = 0.7)
     par(ask = FALSE)
-    plot(fit, which = "all") 
-    
+    plot(fit, which = "all")
+
     # Try:
     # plot(fit, which = "ask")
-    
+
     # Return Value:
-    return()    
+    return()
 }
 
 
 # ------------------------------------------------------------------------------
 
 
-test.summary = 
+test.summary =
 function()
 {
     # Artificial Data Set:
     model = list(xi = -0.25, mu = 0, beta = 1)
-    ts = gpdSim(model = model, n = 5000, seed = 4711) 
-    class(ts)   
+    ts = gpdSim(model = model, n = 5000, seed = 4711)
+    class(ts)
 
-    # Fit: 
-    fit = gpdFit(ts, u = min(ts), type = "mle") 
-    summary(fit, doplot = FALSE)  
-    
+    # Fit:
+    fit = gpdFit(ts, u = min(ts), type = "mle")
+    summary(fit, doplot = FALSE)
+
     # Return Value:
-    return()    
+    return()
 }
 
 
