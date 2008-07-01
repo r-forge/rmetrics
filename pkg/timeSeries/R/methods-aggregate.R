b843209ea74d1a6457a234d4e98866dda79d94aa
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2008-07-01 06:20:46 +0000

    failed when trying to pass arguments, now fixed, e.g. aggregate( ... FUN = tail, n=1)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3377 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/timeSeries/R/methods-aggregate.R
+++ pkg/timeSeries/R/methods-aggregate.R
@@ -18,58 +18,65 @@
 #  aggregate                 Aggregates a 'timeSeries' object
 ################################################################################
 
+
 setMethod("aggregate",
-          "timeSeries",
-          function(x, by, FUN, ...)
-      {   # A function implemented by Yohan Chalabi and  Diethelm Wuertz
+      "timeSeries",
+      function(x, by, FUN, ...)
+{   # A function implemented by Yohan Chalabi and  Diethelm Wuertz
 
-          # Description:
-          #   Aggregates a 'timeSeries' object
+    # Description:
+    #   Aggregates a 'timeSeries' object
 
-          # Details:
-          #   This function can be used to aggregate and coursen a
-          #   'timeSeries' object.
+    # Details:
+    #   This function can be used to aggregate and coursen a
+    #   'timeSeries' object.
 
-          # Arguments:
-          #   x - a 'timeSeries' object to be aggregated
-          #   by - calendarical block, only active when both 'from'
-          #       and 'to' are NULL
-          #   FUN - function to be applied, by default 'colMeans'
-          #   units - a character vector with column names, allows to
-          #       overwrite the column names of the input 'timeSeries'
-          #       object.
+    # Arguments:
+    #   x - a 'timeSeries' object to be aggregated
+    #   by - calendarical block, only active when both 'from'
+    #       and 'to' are NULL
+    #   FUN - function to be applied, by default 'colMeans'
+    #   units - a character vector with column names, allows to
+    #       overwrite the column names of the input 'timeSeries'
+    #       object.
 
-          # Value:
-          #   Returns a S4 object of class 'timeSeries'.
+    # Value:
+    #   Returns a S4 object of class 'timeSeries'.
 
-          # Examples:
-          #  ts = timeSeries(charvec = timeCalendar(2008))
-          #  by = timeSequence(from = "2008-04-01",  to = "2009-01-01", by = "quarter")
-          #  aggregate(ts, by, mean)
-          #
-          #  dates = timeSequence(from = "2008-01-01",  to = "2008-02-01", by = "day")
-          #  ts = timeSeries(matrix(rnorm(2*length(dates)), ncol = 2), charvec = dates)
-          #  by = timeSequence(from = "2008-01-01",  to = "2008-02-01", by = "week")
-          #  aggregate(ts, by, function(x) log(abs(sum(x))))
+    # Examples:
+    #  ts = timeSeries(charvec = timeCalendar(2008))
+    #  by = timeSequence(from = "2008-04-01",  to = "2009-01-01", by = "quarter")
+    #  aggregate(ts, by, mean)
+    #
+    #  dates = timeSequence(from = "2008-01-01",  to = "2008-02-01", by = "day")
+    #  ts = timeSeries(matrix(rnorm(2*length(dates)), ncol = 2), charvec = dates)
+    #  by = timeSequence(from = "2008-01-01",  to = "2008-02-01", by = "week")
+    #  aggregate(ts, by, function(x) log(abs(sum(x))))
 
-          # FUNCTION:
+    # FUNCTION:
 
-          # Check object:
-          stopifnot(class(time(x)) == class(by))
+    # Check object:
+    stopifnot(class(time(x)) == class(by))
 
-          x <- sort(x)
+    x <- sort(x)
 
-###           INDEX <- sapply(as.POSIXct(time(x)),
-###                           function(x) which(x <= as.POSIXct(by))[1])
+    ### INDEX <- sapply(as.POSIXct(time(x)),
+    ###     function(x) which(x <= as.POSIXct(by))[1])
 
-          INDEX <- sapply(time(x), function(x) which(x <= by)[1])
+    INDEX <- sapply(time(x), function(x) which(x <= by)[1])
 
-          data <- apply(x, 2, tapply, INDEX, FUN)
+    # DW: fixed now you can pass arguments ...
+    #   e.g. take the last
+    #   aggregate(x, from = start(x), to = end(x),
+    #       by = timeSequence(start(x), end(x), by = "month"), tail, n = 1)
+    data <- apply(x, 2, tapply, INDEX, FUN, ...)
+    # Note, maybe now the ... may conflict with dots in timeSeries() 
 
-          # Results
-          timeSeries(data, charvec = by[unique(na.omit(INDEX))],
-                     FinCenter = by@FinCenter, ...)
-      })
+    # Return Value:
+    timeSeries(data, charvec = by[unique(na.omit(INDEX))],
+        FinCenter = by@FinCenter, ...)
+})
+
 
 ################################################################################
 
