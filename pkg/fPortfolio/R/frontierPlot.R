840eabe0a8834c6b2a272dbdd8c227fc22894d2d
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2008-10-03 06:23:44 +0000

    frontierPlotControl moved to frontierPlot
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3564 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fPortfolio/R/frontierPlot.R
+++ pkg/fPortfolio/R/frontierPlot.R
@@ -28,6 +28,8 @@
 #   twoAssetsLines                Adds EF for all combinations of two assets
 #   sharpeRatioLines              Adds Sharpe ratio line
 #   monteCarloPoints              Adds randomly produced feasible portfolios
+# FUNCTION:                    DESCRIPTION:            
+#  frontierPlotControl          Sets frontier plot control parameters
 # FUNCTION:                    DESCRIPTION:
 #   .weightsWheel               Adds a pie of weights to frontier plot
 #   .attributesWheel            Adds a pie of attributes to frontier plot
@@ -670,7 +672,93 @@ monteCarloPoints <-
 }
 
 
-# ------------------------------------------------------------------------------
+################################################################################
+
+
+frontierPlotControl <- 
+    function( 
+    
+    # Colors:
+    sharpeRatio.col   = "blue",
+    minvariance.col   = "red",
+    tangency.col      = "steelblue",
+    cml.col           = "green",
+    equalWeights.col  = "blue",
+    singleAsset.col   = "topo.colors",
+    twoAssets.col     = "grey",
+    monteCarlo.col    = "black",
+ 
+    # Point Sizes:
+    minvariance.cex   = 1.25,
+    tangency.cex      = 1.25,
+    cml.cex           = 1.25,
+    equalWeights.cex  = 1.25,
+    singleAsset.cex   = 1.25,
+    twoAssets.cex     = 0.01,
+    monteCarlo.cex    = 0.01,
+    sharpeRatio.cex   = 0.1,
+    
+    # Limits:
+    xlim              = NULL,
+    ylim              = NULL,
+    
+    # MC Steps:
+    mcSteps           = 5000,
+    
+    # Pie Settings:
+    pieR              = NULL, 
+    piePos            = NULL, 
+    pieOffset         = NULL
+    )   
+{
+    # A function implemented by Diethelm Wuertz
+    
+    # Description:
+    #   Sets frontier plot control parameters
+    
+    # FUNCTION:
+    
+    # Return Value:
+    list(
+    
+        # Colors:
+        sharpeRatio.col  = sharpeRatio.col,
+        minvariance.col  = minvariance.col,
+        tangency.col     = tangency.col,
+        cml.col          = cml.col,
+        equalWeights.col = equalWeights.col,
+        singleAsset.col  = singleAsset.col,
+        twoAssets.col    = twoAssets.col,
+        monteCarlo.col   = monteCarlo.col,
+     
+        # Point Sizes:
+        minvariance.cex  = minvariance.cex,
+        tangency.cex     = tangency.cex,
+        cml.cex          = cml.cex,
+        equalWeights.cex = equalWeights.cex,
+        singleAsset.cex  = singleAsset.cex ,
+        twoAssets.cex    = twoAssets.cex,
+        monteCarlo.cex   = monteCarlo.cex,
+        sharpeRatio.cex  = sharpeRatio.cex,
+        
+        # Limits:
+        xlim             = xlim,
+        ylim             = ylim,
+        
+        # MC Steps:
+        mcSteps          = 5000,
+        
+        # Pie Settings:
+        pieR             = pieR, 
+        piePos           = piePos, 
+        pieOffset        = pieOffset
+        
+        )
+              
+}
+
+
+################################################################################
 
 
 .weightsWheel <-
