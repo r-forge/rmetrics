3ec9690b175b19f15e71c21088c0094d16d20629
fgochez (Mr ) <fjgochez@gmail.com>
2008-11-08 14:35:26 +0000

    Accidentally uploaded this to /pkg, deleting
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3649 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/R/unittests.r
+++
@@ -1,6 +0,0 @@
-runBLCOPTestSuite <- function(testPath = BLCOPOptions("unitTestPath"))
-{
-    BLTestSuite <- defineTestSuite(name = "Black-Litterman / COP unit tests", dirs = testPath) 
-    testResults <- runTestSuite(BLTestSuite)
-    summary(testResults)                                  
-}
--- pkg/R/zzz.r
+++
@@ -1,7 +0,0 @@
-  .BLEnv <- new.env()
-.onLoad <- function(libname, pkgname)
-{
-
-  .BLEnv$settings <- list(gWidgetsToolkit = "tcltk", regFunc = "lm", 
-            numSimulations = 50000, unitTestPath = system.file("RUnit", package = "BLCOP"))
-}
