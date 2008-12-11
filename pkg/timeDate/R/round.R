ff7ca666d4587480c712629f81ae6e4600bdfcd9
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-12-11 16:27:25 +0000

    make sure that trunc() and round() are done in FinCenter time zone and not only in GMT
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3697 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/timeDate/R/round.R
+++ pkg/timeDate/R/round.R
@@ -6,16 +6,16 @@
 #
 # This R package is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this R package; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this R package; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - Diethelm Wuertz, GPL
 #   2007 - Rmetrics Foundation, GPL
 #   Diethelm Wuertz <wuertz@phys.ethz.ch>
@@ -30,26 +30,26 @@
 ################################################################################
 # MEHODS:                   DESCRIPTION:
 #  round.timeDate            Rounds objects of class 'timeDate'
-#  trunc.timeDate            Truncates objects of class 'timeDate' 
+#  trunc.timeDate            Truncates objects of class 'timeDate'
 ################################################################################
 
 
 round.timeDate =
-function(x, digits = c("days", "hours", "mins"))     
+function(x, digits = c("days", "hours", "mins"))
 {   # A function implemented by Diethelm Wuertz
-    
+
     # Note:
-    #   round.timeDate(x, units = c("days", "hours", "mins"))    # FAILS !!!  
-    
+    #   round.timeDate(x, units = c("days", "hours", "mins"))    # FAILS !!!
+
     # FUNCTION:
-    
+
     # Get Units:
     units = match.arg(digits)
-  
+
     # Use:
-    lt = round.POSIXt(x@Data, units = units)
+    lt <- round.POSIXt(as.POSIXlt(format(x), tz = "GMT"), units = units)
     ans = timeDate(lt, zone = x@FinCenter, FinCenter = x@FinCenter)
-    
+
     # Return Value:
     ans
 }
@@ -59,27 +59,27 @@ function(x, digits = c("days", "hours", "mins"))
 
 
 trunc.timeDate =
-function(x, units = c("days", "hours", "mins"), ...) 
+function(x, units = c("days", "hours", "mins"), ...)
 {   # A function implemented by Diethelm Wuertz
 
     # FUNCTION:
-    
+
     # Get Units:
     units = match.arg(units)
-    
+
     # Sorting under GMT is not what we want!
     # GMT = timeDate(x, zone = x@FinCenter, FinCenter = "GMT")
     # lt = trunc.POSIXt(GMT@Data, units = units)
     # ans = timeDate(lt, zone = "GMT", FinCenter = x@FinCenter)
-    
+
     # Use:
-    lt = trunc.POSIXt(x@Data, units = units)
+    lt <- trunc.POSIXt(as.POSIXlt(format(x), tz = "GMT"), units = units)
     ans = timeDate(lt, zone = x@FinCenter, FinCenter = x@FinCenter)
-    
+
     # Return Value:
     ans
 }
 
-    
+
 ################################################################################
 
