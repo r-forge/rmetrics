35b26c9a67a78eca224db3d4d96a4f9c3c4b8aac
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2008-12-27 22:09:46 +0000

    fixed subset-ing with "characters" and added subset-ing methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3751 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/timeDate/R/subset.R
+++ pkg/timeDate/R/subset.R
@@ -32,43 +32,48 @@
 #  [.timeDate                Extracts/replaces subsets from 'timeDate' objects
 ################################################################################
 
+# functions implemented by Yohan Chalabi and Diethelm Wuertz
 
-"[.timeDate" <-
-    function(x, ..., drop = TRUE)
-{
-    # A function implemented by Diethelm Wuertz
+setMethod("[", signature(x="timeDate", i="missing", j="missing", drop="ANY"),
+          function(x, i, j, ..., drop = TRUE) x)
 
-    # Description:
-    #   Extracts or replaces subsets from 'timeDate' objects
+setMethod("[", signature(x="timeDate", i="numeric", j="missing", drop="ANY"),
+          function(x, i, j, ..., drop = TRUE)
+      {
+          x@Data <- callGeneric(x@Data, i)
+          x
+      })
 
-    # Arguments:
-    #   x - a 'timeDate' object
+setMethod("[", signature(x="timeDate", i="logical", j="missing", drop="ANY"),
+          function(x, i, j, ..., drop = TRUE)
+      {
+          x@Data <- callGeneric(x@Data, i)
+          x
+      })
 
-    # Value:
-    #   Returns a subset from a 'timeDate' object.
+setMethod("[", signature(x="timeDate", i="character", j="missing", drop="ANY"),
+          function(x, i, j, ..., drop = TRUE)
+      {
 
-    # FUNCTION:
+          if (length(i) > 1) {
+              lt <- lapply(i, function(i, x) "["(x, i), x)
+              num <- unlist(lapply(lt, function(td) unclass(td@Data)))
+              return(timeDate(num, zone = "GMT", FinCenter = x@FinCenter))
+          }
+          if (.subsetCode(i) == "SPAN") {
+              # Subsetting by Span Indexing:
+              return(.subsetBySpan(x, i))
+          } else {
+              # Subsetting by Python Indexing:
+              return(.subsetByPython(x, i))
+          }
+      })
 
-    # Character Type Subsetting:
-    subset = I(...)
-    if (is.character(subset)){
-        if (.subsetCode(subset) == "SPAN") {
-            # Subsetting by Span Indexing:
-            return(.subsetBySpan(x, subset))
-        } else {
-            # Subsetting by Python Indexing:
-            return(.subsetByPython(x, subset))
-        }
-    }
-
-    # Simple and fast subset:
-    x@Data <- x@Data[...]
-    x
-}
-
+setMethod("[", signature(x="timeDate", i="ANY", j="missing", drop="ANY"),
+          function(x, i, j, ..., drop = TRUE)
+          stop("Not Yet implemented"))
 
 #-------------------------------------------------------------------------------
-
 
 "[<-.timeDate" <-
     function(x, ..., value)
@@ -102,9 +107,7 @@
         FinCenter = x@FinCenter)
 }
 
-
 ################################################################################
-
 
 .subsetCode <-
 function(subset)
@@ -153,10 +156,7 @@ function(subset)
     code
 }
 
-
 # ------------------------------------------------------------------------------
-
-
 
 .subsetByPython <-
 function(x = timeCalendar(), subset = "::")
@@ -179,6 +179,7 @@ function(x = timeCalendar(), subset = "::")
     #   .subsetByPython(subset = "::2008-06")
 
     # FUNCTION:
+    stopifnot(length(subset) == 1)
 
     # Subset Code:
     code = .subsetCode(subset)
@@ -190,7 +191,14 @@ function(x = timeCalendar(), subset = "::")
     date = strsplit(subset, "::")[[1]]
 
     # 1. DATE
-    if(code == "00000") ans = x[grep(date, format(x))]
+    if(code == "00000") {
+        # should return NA if no match found
+        idx = grep(date, format(x))
+        if (!length(idx))
+            ans@Data <- as.POSIXct(NA)
+        else
+            ans <- x[idx]
+    }
 
     # 2. ::
     if(code == "00010") ans = x
@@ -252,6 +260,7 @@ function(x = timeCalendar(), subset = "last 3 Months")
     #   .subsetBySpan(timeCalendar(), "last 62 days")
 
     # FUNCTION:
+    stopifnot(length(subset) == 1)
 
     # Get Code:
     code = .subsetCode(subset)
