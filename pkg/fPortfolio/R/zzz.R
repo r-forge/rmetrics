583ebe6278d1e26ce4653ed6e0f5efcc6b24de97
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2009-01-16 08:50:16 +0000

    fixed warning with new Rd parser and changes data set form csv to timeSeries format
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3796 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fPortfolio/R/zzz.R
+++ pkg/fPortfolio/R/zzz.R
@@ -16,46 +16,19 @@
 ################################################################################
 
 
-.First.lib =
-function(lib, pkg)
-{
-###     # Startup Mesage and Desription:
-###     MSG <- if(getRversion() >= "2.5") packageStartupMessage else message
-###     dsc <- packageDescription(pkg)
-###     if(interactive() || getOption("verbose")) {
-###         # not in test scripts
-###         MSG(sprintf("Rmetrics Package %s (%s) loaded.", pkg, dsc$Version))
-###     }
+## .First.lib =
+## function(lib, pkg)
+## {
+## ###     # Startup Mesage and Desription:
+## ###     MSG <- if(getRversion() >= "2.5") packageStartupMessage else message
+## ###     dsc <- packageDescription(pkg)
+## ###     if(interactive() || getOption("verbose")) {
+## ###         # not in test scripts
+## ###         MSG(sprintf("Rmetrics Package %s (%s) loaded.", pkg, dsc$Version))
+## ###     }
 
-    # Load dll:
-    library.dynam("fPortfolio", pkg, lib)
+## }
 
-}
-
-    data(GCCINDEX.DF, package = "fPortfolio")
-    data(SPISECTOR.DF, package = "fPortfolio") 
-    data(SWX.DF, package = "fPortfolio")
-    data(LPP2005.RET.DF, package = "fPortfolio") 
-    data(SMALLCAP.RET.DF, package = "fPortfolio") 
-    
-    
-    GCCINDEX <- as.timeSeries(GCCINDEX.DF)
-    SPISECTOR <- as.timeSeries(SPISECTOR.DF)
-    SWX <- as.timeSeries(SWX.DF)
-    LPP2005 <- cumulated(as.timeSeries(LPP2005.RET.DF))
-    SMALLCAP <- cumulated(as.timeSeries(SMALLCAP.RET.DF))
-    
-    
-    GCCINDEX.RET <- returns(as.timeSeries(GCCINDEX.DF))
-    SPISECTOR.RET <- returns(as.timeSeries(SPISECTOR.DF))
-    SWX.RET <- returns(as.timeSeries(SWX.DF))
-    LPP2005.RET <- as.timeSeries(LPP2005.RET.DF)
-    SMALLCAP.RET <- as.timeSeries(SMALLCAP.RET.DF)
-
-     
-    if(!exists("Sys.setenv", mode = "function")) # pre R-2.5.0, use "old form"
-        Sys.setenv <- Sys.putenv
-        
 
 ################################################################################
 
