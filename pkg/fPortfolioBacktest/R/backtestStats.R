2e40d27d97f9a4d2e830c9cb6bc1923214264032
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2009-02-23 18:02:03 +0000

    ! adapted backtestStats to new fPortfolio classes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3925 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fPortfolioBacktest/R/backtestStats.R
+++ pkg/fPortfolioBacktest/R/backtestStats.R
@@ -38,16 +38,16 @@
 ################################################################################
 
 
-backtestStats = 
+backtestStats =
     function(object, stats = "rollingSigma", ...)
 {
 	# Extract the portfolios into a list:
-	portfolios = object$strategyList
-	
+	portfolios <- object$strategyList
+
 	# perfrorm stats:
-	backtestFun = match.fun(stats)
-	ans = backtestFun(portfolios, ...)
-	
+	backtestFun <- match.fun(stats)
+	ans <- backtestFun(portfolios, ...)
+
 	# return
 	ans
 }
@@ -55,14 +55,19 @@ backtestStats =
 # ------------------------------------------------------------------------------
 
 
-rollingSigma = 
+rollingSigma =
     function(portfolios)
 {
-	ans = sapply(portfolios, 
-	    function(x) getTargetRisk(x)[, "Sigma"])
-	dates = sapply(portfolios, 
-	    function(x) as.character(rev(rownames(getSeries(x)))[1]))
-	
+
+    # quick fix ... there is some confusion with getTargetRisk of
+    # @portfolio and @spec
+    prtval <- lapply(portfolios, slot, "portfolio")
+    ans <- sapply(prtval,
+                  function(x) getTargetRisk(x)["Sigma"])
+
+    dates <- sapply(portfolios,
+                    function(x) as.character(rev(time(getSeries(x)))[1]))
+
 	# return
 	timeSeries(ans, charvec = dates, units = "Sigma")
 }
@@ -71,121 +76,121 @@ rollingSigma =
 # ------------------------------------------------------------------------------
 
 
-rollingVaR = 
+rollingVaR =
     function(portfolios, alpha = 0.05)
 {
 	# calculate VaR for one portfolio:
   	.var = function(x, alpha){
-  		R = getSeries(x) %*% t(getWeights(x))
+  		R = getSeries(x) %*% getWeights(x)
 		quantile.default(R, probs = alpha)}
-	
+
 	# Calculates VaR for all portfolios:
 	ans = sapply(portfolios, FUN = .var, alpha = alpha)
-	
+
 	# Extracts the dates:
-	dates = sapply(portfolios, 
+	dates = sapply(portfolios,
 	    function(x) as.character(rev(rownames(getSeries(x)))[1]))
 
 	# Return:
-	timeSeries(ans, charvec = dates, units = paste("VaR",alpha,sep = "."))
-} 
+	timeSeries(ans, charvec = dates, units = paste("VaR", alpha, sep = "."))
+}
 
 # ------------------------------------------------------------------------------
 
 
-rollingCVaR = 
+rollingCVaR =
     function(portfolios, alpha = 0.05)
 {
 	# Calculate CVaR for one portfolio:
-  	.cvar = 
+  	.cvar =
   	  function(x, alpha){
-  		R = getSeries(x) %*% t(getWeights(x))
+  		R = getSeries(x) %*% getWeights(x)
 		z = quantile.default(R, probs = alpha)
 		mean(as.numeric(R)[R <= z], na.rm = TRUE)
 		}
-  	
-	# Calculate CVaR for all portfolios:  	
+
+	# Calculate CVaR for all portfolios:
 	ans = sapply(portfolios, FUN = .cvar, alpha = alpha)
-	
+
 	# Extract the dates:
-	dates = sapply(portfolios, 
+	dates = sapply(portfolios,
 	    function(x) as.character(rev(rownames(getSeries(x)))[1]))
-	
+
 	# Return:
 	timeSeries(ans, charvec = dates, units = paste("CVaR",alpha, sep = "."))
-} 
+}
 
 
 # ------------------------------------------------------------------------------
 
 
-rollingDaR = 
+rollingDaR =
     function(portfolios, alpha = 0.05)
 {
 	# calculate VaR for one portfolio:
   	.dar = function(x, alpha){
-  		R = getSeries(x) %*% t(getWeights(x))
+  		R = getSeries(x) %*% getWeights(x)
   		dd = drawdowns(as.timeSeries(R)/100)
 		quantile.default(dd, probs = alpha)}
-	
+
 	# Calculates DaR for all portfolios:
 	ans = sapply(portfolios, FUN = .dar, alpha = alpha)
-	
+
 	# Extracts the dates:
-	dates = sapply(portfolios, 
+	dates = sapply(portfolios,
 	    function(x) as.character(rev(rownames(getSeries(x)))[1]))
 
 	# Return:
 	timeSeries(ans, charvec = dates, units = paste("DaR",alpha, sep = "."))
-} 
+}
 
 # ------------------------------------------------------------------------------
 
 
-rollingCDaR = 
+rollingCDaR =
   function(portfolios, alpha = 0.05){
-  	
+
 	# Calculate CDaR for one portfolio:
-  	.cdar = 
+  	.cdar =
   	  function(x, alpha){
-  		R = getSeries(x) %*% t(getWeights(x))
+  		R = getSeries(x) %*% getWeights(x)
   		dd = drawdowns(as.timeSeries(R)/100)
 		z = quantile.default(dd, probs = alpha)
 		mean(as.numeric(R)[R <= z], na.rm = TRUE)
 		}
-  	
-	# Calculate CVaR for all portfolios:  	
+
+	# Calculate CVaR for all portfolios:
 	ans = sapply(portfolios, FUN = .cdar, alpha = alpha)
-	
+
 	# Extract the dates:
-	dates = sapply(portfolios, 
+	dates = sapply(portfolios,
 	    function(x) as.character(rev(rownames(getSeries(x)))[1]))
-	
+
 	# Return:
 	timeSeries(ans, charvec = dates, units = paste("CDaR", alpha, sep = "."))
-} 
+}
 
 
 # ------------------------------------------------------------------------------
 
 
-rollingRiskBudgets = 
+rollingRiskBudgets =
 	function(portfolios){
-		
+
 	nPortfolios = length(portfolios)
 	assetNames = colnames(getSeries(portfolios[[1]]))
-	
+
 	ans = NULL
 	for (i in 1:nPortfolios){
 		ans = rbind(ans, getCovRiskBudgets(portfolios[[i]]))
-		}	
-		
+		}
+
 	# Extract the dates:
-	dates = sapply(portfolios, 
+	dates = sapply(portfolios,
 	    function(x) as.character(rev(rownames(getSeries(x)))[1]))
-	
+
 	# Return:
-	timeSeries(ans, charvec = dates, units = assetNames)				
+	timeSeries(ans, charvec = dates, units = assetNames)
 }
 
 
