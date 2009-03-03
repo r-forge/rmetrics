90eb80c5fb812534828574b9d3d28830f6a9796b
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2009-03-03 12:39:01 +0000

    ! added initialize,timeSeries-method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@3938 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/timeSeries/R/AllClass.R
+++ pkg/timeSeries/R/AllClass.R
@@ -50,10 +50,11 @@
 
 ################################################################################
 
+# Note if slots are added or removed, don't forget to edit
+# getDataPart,timeSeries-method and setDataPart,timeSeries-method !!
 
 setClass("timeSeries",
-         representation(
-                        .Data = "matrix",
+         representation(.Data = "matrix",
                         units = "character",
                         positions = "numeric",
                         format = "character",
@@ -70,3 +71,60 @@ setClass("timeSeries",
                  return("length of '@units' not equal to '@.Data' extent")
              TRUE
          })
+
+# ------------------------------------------------------------------------------
+
+setMethod("initialize", "timeSeries",
+          function(.Object,
+                   .Data = new("matrix"),
+                   units = character(0),
+                   positions = numeric(0),
+                   format = character(0),
+                   FinCenter = character(0),
+                   recordIDs = data.frame(),
+                   title = character(0),
+                   documentation = character(0),
+                   check = TRUE)
+      {
+          .Object <- setDataPart(.Object, value = .Data, check = check)
+          `slot<-`(.Object, "units", check = check, value = units)
+          `slot<-`(.Object, "positions", check = check, value = positions)
+          `slot<-`(.Object, "format", check = check, value = format)
+          `slot<-`(.Object, "FinCenter", check = check, value = FinCenter)
+          `slot<-`(.Object, "recordIDs", check = check, value = recordIDs)
+          `slot<-`(.Object, "title", check = check, value = title)
+          `slot<-`(.Object, "documentation", check = check, value = documentation)
+          .Object
+      })
+
+################################################################################
+
+## # before init methods
+## system.time(new("timeSeries", .Data = as.matrix(1:1e6L), units = "hello", positions = 1:1e6L))
+## ###    user  system elapsed
+## ###   0.230   0.180   0.405
+
+## system.time(timeSeries(1:1e6L, units = "hello", positions = 1:1e6L))
+###    user  system elapsed
+###    0.40    0.10    0.49
+
+## # after
+## system.time(new("timeSeries", .Data = as.matrix(1:1e6L), units = "hello", positions = 1:1e6L))
+## ###    user  system elapsed
+## ###    0.15    0.12    0.27
+###    user  system elapsed
+###    0.06    0.09    0.15
+
+## system.time(timeSeries(1:1e6L, units = "hello", positions = 1:1e6L))
+###    user  system elapsed
+###   0.200   0.050   0.273
+
+## system.time(timeSeries(as.matrix(1:1e6L), units = "hello", positions = 1:1e6L))
+## ###   user  system elapsed
+## ###   0.150   0.120   0.267
+
+## # twice faster !!
+## # and without check
+## system.time(new("timeSeries", .Data = as.matrix(1:1e6L), units = "hello", positions = 1:1e6L, check = FALSE))
+## # does not seem faster ....
+
