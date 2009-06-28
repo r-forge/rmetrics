7d0b47f4f983ce30285c48e3b9d7f8ff451d9876
luda (David Lüthi) <luethid@gmail.com>
2009-06-28 19:46:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4216 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/FKF/R/fkf.r
+++ pkg/FKF/R/fkf.r
@@ -7,7 +7,6 @@ fkf <- function(a0, P0, dt, ct, Tt, Zt, HHt, GGt, yt, check.input = TRUE)
            "'HHt', 'GGt', and 'yt' must be misssing.")
     }
 
-
     ## 'check.input' should always be 'TRUE' unless the performance
     ## becomes crucial and correctness of the arguments concerning
     ## dimensions, class, storage.mode and absence of 'NA's is ensured.
@@ -30,7 +29,6 @@ fkf <- function(a0, P0, dt, ct, Tt, Zt, HHt, GGt, yt, check.input = TRUE)
                  paste(names(stor.mode)[stor.mode != "double"],
                        collapse = "', '"),
                  "' is not 'double'!\n", sep = "")
-
         }
 
         ## Check classes of arguments
@@ -139,5 +137,4 @@ fkf <- function(a0, P0, dt, ct, Tt, Zt, HHt, GGt, yt, check.input = TRUE)
 
     return(ans)
 }
-
 
