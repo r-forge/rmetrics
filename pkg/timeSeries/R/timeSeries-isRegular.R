fe0b7add95732b8bc440365cee91800015f386af
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2009-08-30 09:24:28 +0000

    isRegular methods are now proper S4 methods.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4347 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/timeSeries/R/timeSeries-isRegular.R
+++ pkg/timeSeries/R/timeSeries-isRegular.R
@@ -6,127 +6,33 @@
 #
 # This R package is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this R package; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this R package; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 
 ################################################################################
-# FUNCTION:                DESCRIPTION:
-#  isDaily                  Tests if a time series is a daily series
-#  isMonthly                Tests if a time series is a monthly series
-#  isQuarterly              Tests if a time series is a quarterly series
-#  isRegular                Tests if a time series is a regular series
-#  frequency                Returns the frequency of a regular time series  
+# FUNCTION:                      DESCRIPTION:
+#  isDaily,timeSeries-method      Tests if a time series is a daily series
+#  isMonthly,timeSeries-method    Tests if a time series is a monthly series
+#  isQuarterly,timeSeries-method  Tests if a time series is a quarterly series
+#  isRegular,timeSeries-method    Tests if a time series is a regular series
+#  frequency,timeSeries-method    Returns the frequency of a regular time series
 ################################################################################
 
+setMethod("isDaily", "timeSeries", function(x) callGeneric(time(x)))
 
-isDaily.timeSeries <- 
-function(x) 
-{
-    # A function implemented by Diethelm Wuertz
-    
-    # Description:
-    #   Tests if a time series is a daily series   
-    
-    # FUNCTION:
-    
-    # Check:
-    ans <- isDaily(time(x))
-    
-    # Return Value:
-    ans
-}
-    
+setMethod("isQuarterly", "timeSeries", function(x) callGeneric(time(x)))
 
-# ------------------------------------------------------------------------------
+setMethod("isMonthly", "timeSeries", function(x) callGeneric(time(x)))
 
+setMethod("isRegular", "timeSeries", function(x) callGeneric(time(x)))
 
-isMonthly.timeSeries <- 
-function(x) 
-{
-    # A function implemented by Diethelm Wuertz
-    
-    # Description:
-    #   Tests if a time series is a monthly series  
-    
-    # FUNCTION:
-    
-    # Check:
-    ans <- isMonthly(time(x))
-    
-    # Return Value:
-    ans
-}
-    
-
-# ------------------------------------------------------------------------------
-
-
-isQuarterly.timeSeries <- 
-function(x) 
-{
-    # A function implemented by Diethelm Wuertz
-    
-    # Description:
-    #   Tests if a time series is a quarterly series  
-    
-    # FUNCTION:
-    
-    # Check:
-    ans <- isQuarterly(time(x))
-    
-    # Return Value:
-    ans
-}
-    
-
-# ------------------------------------------------------------------------------
-
-
-isRegular.timeSeries <-
-function(x)
-{
-    # A function implemented by Diethelm Wuertz
-    
-    # Description:
-    #   Tests if a time series is a regular series  
-    
-    # FUNCTION:
-    
-    # Check:
-    ans <- isRegular(time(x))
-    
-    # Return Value:
-    ans
-}
-
-
-# ------------------------------------------------------------------------------
-
-
-frequency.timeSeries <-
-function(x, ...)
-{
-    # A function implemented by Diethelm Wuertz
-    
-    # Description:
-    #   Returns the frequency of a regular time series  
-    
-    # FUNCTION:
-    
-    # Check:
-    ans <- frequency(time(x))
-    
-    # Return Value:
-    ans
-}
-        
-
+setMethod("frequency", "timeSeries", function(x, ...) callGeneric(time(x), ...))
 
 ################################################################################
 
