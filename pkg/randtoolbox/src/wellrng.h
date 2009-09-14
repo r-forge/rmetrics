869e9671c4093fc39d150be009660456a6a07b3b
dutangc (Mr ) <dutangc@gmail.com>
2009-09-14 22:02:50 +0000

    declare well functions in a dedicated header.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4393 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/randtoolbox/src/wellrng.h
+++ pkg/randtoolbox/src/wellrng.h
@@ -56,58 +56,12 @@
 #include <Rmath.h>
 #include <R_ext/Error.h>
 
-#include "config.h"
-#include "locale.h"
-
-//congruRand
-#include "congruRand.h"
-
-//SFMT
-#include "SFMT.h"
-
-//Knuth TAOCP
-#include "knuthTAOCP2002.h"
-
-//WELL RNGs
-#include "wellrng.h"
 
 
-//time header files
-#if HAVE_TIME_H
-# include <time.h>
-#endif
+//well RNG function coming from rngWELL package
+void (*WELLrng)(double *u, int nb, int dim, int order, int temper, int version);
 
-#if HAVE_SYS_TIME_H
-# include <sys/time.h>
-#endif
+//well RNG function coming from rngWELL package
+void (*setSeed4WELL)(double *u, int nb, int dim, int order, int temper, int version);
 
-#if HAVE_WINDOWS_H
-# include <windows.h>
-#endif
-
-#if defined(HAVE_SSE2)
-# include <emmintrin.h>
-#endif
-
-
-/* Functions accessed from .Call() */
-SEXP doTorus(SEXP n, SEXP d, SEXP p, SEXP offset, SEXP ismixed, SEXP timedseed);
-SEXP doSetSeed(SEXP s);
-SEXP doCongruRand(SEXP n, SEXP d, SEXP modulus, SEXP multiplier, SEXP increment, SEXP echo);
-SEXP doSFMersenneTwister(SEXP n, SEXP d, SEXP mersexpo, SEXP paramset);
-SEXP doWELL(SEXP n, SEXP d, SEXP order, SEXP tempering, SEXP version);
-SEXP doKnuthTAOCP(SEXP n, SEXP d);
-
-/* utility functions */
-void torus(double *u, int nb, int dim, int *prime, int offset, int ismixed, int usetime);
-void congruRand(double *u, int nb, int dim, unsigned long long mod, unsigned long long mult, unsigned long long incr, int show);
-void SFmersennetwister(double *u, int nb, int dim, int mexp, int usepset);
-void knuthTAOCP(double *u, int nb, int dim);
-
-void setSeed(long s);
-void randSeedByArray(int length);
-void randSeed();
-
-void reconstruct_primes();
-void get_primes(int *n, int *pri);
 
