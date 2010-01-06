280784855eee231c928f725439998fa75d38e996
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2010-01-06 15:39:59 +0000

    missing file name when saving data set to file. Thanks to Nicolas Chapados !
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4688 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fImport/R/fredImport.R
+++ pkg/fImport/R/fredImport.R
@@ -6,117 +6,117 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - 2008, Diethelm Wuertz, Rmetrics Foundation, GPL
 #   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
 #   www.rmetrics.org
 
 
 ################################################################################
-# FUNCTION:             DESCRIPTION:   
+# FUNCTION:             DESCRIPTION:
 #  fredImport            Downloads market data from research.stlouisfed.org
 #  fredSeries            Easy to use download from research.stlouisfed.org
 ################################################################################
 
 
-fredImport <-  
+fredImport <-
     function(query, file = "tempfile", source = NULL,
-    frequency = "daily", 
+    frequency = "daily",
     from = NULL, to = Sys.timeDate(), nDaysBack = NULL,
-    save = FALSE, sep = ";", try = TRUE) 
-{   
+    save = FALSE, sep = ";", try = TRUE)
+{
     # A function implemented by Diethelm Wuertz
 
     # Description:
-    #   Downloads Monthly Market Data, Indices and Benchmarks from 
+    #   Downloads Monthly Market Data, Indices and Benchmarks from
     #   St. Louis FED, "research.stlouisfed.org".
-    
+
     # Value:
     #   An One Column data frame with row names denoting the dates
     #   given in the POSIX format "%Y%m%d". The column lists the
     #   downloaded data records.
-        
+
     # Examples:
     #   fredImport("DPRIME")
-    
+
     # Notes:
     #   This function is written for one-column daily data sets.
-    #   Some example data sets include:  
-    #     DEXUSEU   U.S. / Euro Foreign Exchange Rate 
-    #     DEXSZUS   Switzerland / U.S. Foreign Exchange Rate 
-    #     DGS1      1-Year Treasury Constant Maturity Rate 
-    #     DPRIME    Bank Prime Loan Rate     
-  
+    #   Some example data sets include:
+    #     DEXUSEU   U.S. / Euro Foreign Exchange Rate
+    #     DEXSZUS   Switzerland / U.S. Foreign Exchange Rate
+    #     DGS1      1-Year Treasury Constant Maturity Rate
+    #     DPRIME    Bank Prime Loan Rate
+
     # FUNCTION:
-    
+
     # Settings:
     stopifnot(length(query) == 1)
-    
+
     # Source"
     if (is.null(source))
         source = "http://research.stlouisfed.org/fred2/series/"
-    
+
     # Check:
     if (frequency != "daily")
         stop("Only daily data records are supported!")
-    
+
     # Download:
     if (try) {
         # Try for Internet Connection:
-        z = try(fredImport(query, file, source, frequency, from, to, 
+        z = try(fredImport(query, file, source, frequency, from, to,
             nDaysBack, save, sep, try = FALSE))
         if (inherits(z, "try-error") || inherits(z, "Error")) {
-            return("No Internet Access") 
+            return("No Internet Access")
         } else {
-            return(z) 
-        } 
-    } else { 
+            return(z)
+        }
+    } else {
         # Download File:
         queryFile = paste(query, "/downloaddata/", query, ".txt", sep = "")
         url = paste(source, queryFile, sep = "")
         tmp = tempfile()
-        download.file(url = url, destfile = tmp) 
-        
+        download.file(url = url, destfile = tmp)
+
         # Scan the file:
         x1 = scan(tmp, what = "", sep = "\n")
-        
+
         # Extract dates ^19XX and ^20XX:
         x2 = x1[regexpr("^[12][90]", x1) > 0]
         x1 = x2[regexpr(" .$", x2) < 0]
-        
+
         # Compose Time Series:
         data = matrix(
             as.numeric(substring(x1, 11, 999)), byrow = TRUE, ncol = 1)
         charvec = substring(x1, 1, 10)
         X = timeSeries(data, charvec, units = query)
     }
-    
+
     # Save to file:
     if (save) {
-        write.table(as.data.frame(X)) 
+        write.table(as.data.frame(X), file = file, sep = sep)
     } else {
-        unlink(file) 
+        unlink(file)
     }
-    
+
     # Result:
-    ans = new("fWEBDATA",     
+    ans = new("fWEBDATA",
         call = match.call(),
         param = c(
-            "Instrument" = query, 
+            "Instrument" = query,
             "Frequency " = frequency),
-        data = X, 
-        title = "Data Import from research.stlouisfed.org", 
+        data = X,
+        title = "Data Import from research.stlouisfed.org",
         description = description() )
-    
+
     # Return Value:
     ans
 }
@@ -125,44 +125,44 @@ fredImport <-
 # ------------------------------------------------------------------------------
 
 
-fredSeries <-  
+fredSeries <-
     function(symbols, from = NULL, to = Sys.timeDate(), nDaysBack = 366, ...)
-{   
+{
     # A function implemented by Diethelm Wuertz
 
     # Description:
     #   Downloads easily time series data from St. Louis FRED
-    
+
     # Arguments:
     #   symbols - a character vector of symbol names
     #   from - from date
     #   to - to date
     #   nDaysBack - number of n-days back
     #   ... - arguments passed to the *Import()
-    
+
     # Examples:
     #   fredSeries("DPRIME")[1:10, ]
-    
+
     # FUNCTION:
-    
+
     # Download:
     X = fredImport(query = symbols[1], ...)@data
     N = length(symbols)
     if (N > 1) {
         for (i in 2:N) {
-            X = merge(X, fredImport(query = symbols[i], ...)@data) 
+            X = merge(X, fredImport(query = symbols[i], ...)@data)
         }
-    }    
-    
+    }
+
     # Time Window:
     if (is.null(from)) from = to - nDaysBack*24*3600
     X = window(X, from, to)
-     
+
     # Return Value:
-    X  
+    X
 }
-   
+
 
 ################################################################################
 
-     
+
--- pkg/fImport/R/oandaImport.R
+++ pkg/fImport/R/oandaImport.R
@@ -6,70 +6,70 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received a copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received a copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 # Copyrights (C)
-# for this R-port: 
+# for this R-port:
 #   1999 - 2008, Diethelm Wuertz, Rmetrics Foundation, GPL
 #   Diethelm Wuertz <wuertz@itp.phys.ethz.ch>
 #   www.rmetrics.org
 
 
 ################################################################################
-# FUNCTION:             DESCRIPTION:  
+# FUNCTION:             DESCRIPTION:
 #  oandaImport           Downloads market data from www.oanda.com
 #  oandaSeries           Easy to use download from www.oanda.com
 ################################################################################
 
 
-oandaImport <-  
+oandaImport <-
     function(query, file = "tempfile", source = NULL,
-    frequency = "daily", 
-    from = NULL, to = Sys.timeDate(), nDaysBack = 366, 
-    save = FALSE, sep = ";", try = TRUE) 
-{   
+    frequency = "daily",
+    from = NULL, to = Sys.timeDate(), nDaysBack = 366,
+    save = FALSE, sep = ";", try = TRUE)
+{
     # A function implemented by Diethelm Wuertz
 
     # Description:
     #   Downloads market data from www.oanda.com
-    
+
     # Example:
-    #   oandaImport("USD/EUR") 
-    
+    #   oandaImport("USD/EUR")
+
     # Value:
     #   An One Column data frame with row names denoting the dates
     #   given in the POSIX format "%Y%m%d". The column lists the
-    #   downloaded data records.      
-  
+    #   downloaded data records.
+
     # FUNCTION:
-    
+
     # Settings:
     stopifnot(length(query) == 1)
-    
+
     # Check:
     stopifnot (frequency == "daily")
-    
+
     # Source:
     if (is.null(source))
         source = "http://www.oanda.com/convert/fxhistory?lang=en&"
-    
+
     # Download:
     if (try) {
         # Try for Internet Connection:
-        z = try(oandaImport(query, file, source, frequency, from, to, 
+        z = try(oandaImport(query, file, source, frequency, from, to,
             nDaysBack, save, sep, try = FALSE))
         if (inherits(z, "try-error") || inherits(z, "Error")) {
-            return("No Internet Access") 
+            return("No Internet Access")
         } else {
-            return(z) 
-        } 
-    } else { 
+            return(z)
+        }
+    } else {
         # Download File:
         to = trunc(to, "days")
         from = to - nDaysBack*24*3600
@@ -80,15 +80,15 @@ oandaImport <-
         ccy.pair <- strsplit(toupper(query),"/")[[1]]
         tmp <- tempfile()
         download.file(
-            paste(source, from.date, to.date, "exch=", ccy.pair[1], 
+            paste(source, from.date, to.date, "exch=", ccy.pair[1],
                 "&expr2=", ccy.pair[2], "&margin_fixed=0&SUBMIT=Get+Table&",
-                "format=CSV&redirected=1", sep = ""), 
+                "format=CSV&redirected=1", sep = ""),
             destfile = tmp)
-            
+
         # Compose Time Series:
-        fx <- readLines(tmp)       
+        fx <- readLines(tmp)
         fx <- unlist(strsplit(
-            gsub("<PRE>|</PRE>", "", 
+            gsub("<PRE>|</PRE>", "",
             fx[(grep("PRE", fx)[1]):(grep("PRE",fx)[2])]), ","))
         fx = matrix(fx, byrow = TRUE, ncol = 2)
         data = as.numeric(fx[, 2])
@@ -97,24 +97,24 @@ oandaImport <-
             substr(time, 4, 5), sep = "-")
         X = timeSeries(data, charvec, units = query)
     }
-    
+
     # Save to file:
     if (save) {
-        write.table(as.data.frame(X)) 
+        write.table(as.data.frame(X), file = file, sep = sep)
     } else {
-        unlink(file) 
+        unlink(file)
     }
-    
+
     # Result:
-    ans = new("fWEBDATA",     
+    ans = new("fWEBDATA",
         call = match.call(),
         param = c(
-            "Instrument" = query, 
+            "Instrument" = query,
             "Frequency " = frequency),
-        data = X, 
-        title = "Data Import from www.oanda.com", 
+        data = X,
+        title = "Data Import from www.oanda.com",
         description = description() )
-        
+
     # Return Value:
     ans
 }
@@ -124,43 +124,43 @@ oandaImport <-
 
 
 oandaSeries <-
-    function(symbols, from = NULL, to = Sys.timeDate(), nDaysBack = 366,  ...) 
-{ 
+    function(symbols, from = NULL, to = Sys.timeDate(), nDaysBack = 366,  ...)
+{
     # A function implemented by Diethelm Wuertz
-    
+
     # Description:
     #   Easy to use download from www.oanda.com
-    
+
     # Arguments:
     #   symbols - a character vector of symbol names
     #   from - from date
     #   to - to date
     #   nDaysBack - number of n-days back
     #   ... - arguments passed to the *Import()
-    
+
     # Example:
-    #   oandaSeries("USD/EUR") 
-    #   oandaSeries(c("USD/EUR", "USD/JPY"), nDaysBack = 10) 
-    
+    #   oandaSeries("USD/EUR")
+    #   oandaSeries(c("USD/EUR", "USD/JPY"), nDaysBack = 10)
+
     # FUNCTION:
-        
+
     # Download:
     X = oandaImport(query = symbols[1], ...)@data
     N = length(symbols)
     if (N > 1) {
         for (i in 2:N) {
-            X = merge(X, oandaImport(query = symbols[i], ...)@data)  
+            X = merge(X, oandaImport(query = symbols[i], ...)@data)
         }
-    }    
-    
+    }
+
     # Time Window:
     if (is.null(from)) from = to - nDaysBack*24*3600
     X = window(X, from, to)
-    
+
     # Return Value:
-    X  
+    X
 }
 
-         
+
 ################################################################################
 
