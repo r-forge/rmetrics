cfc449f4cbc2c3f407eb12172c050a602784c354
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2010-01-22 20:40:14 +0000

    deprecated functions moved to aaa-deprecated.R, started to clean up ....
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4697 5d4a7380-5c0c-0410-b446-d4a5018c0119

---
+++ pkg/timeSeries/R/aaa-Deprecated.R
@@ -0,0 +1,59 @@
+
+
+# deprecated functions moved to here ...
+
+
+# durationSeries
+# midquoteSeries
+# spreadSeries
+
+
+################################################################################
+
+
+durationSeries <- 
+function(...) 
+{
+    # Deprecated:
+    .Deprecated("returns", "timeSeries")
+    
+    # Durations
+    durations(...)
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+midquoteSeries =
+function(...)
+{
+    # FUNCTION:
+    
+    # Deprecated:
+    .Deprecated("midquoteSeries")
+    
+    # Return Value:
+    midquotes(...)
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+spreadSeries =
+function(...)
+{
+    # FUNCTION:
+    
+    # Deprecated:
+    .Deprecated("spreadSeries")
+    
+    # Return Value:
+    spreads(...)
+}
+
+
+################################################################################
+
+    
