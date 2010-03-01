08b1860e0223806da5b20d91ec6b03f646a56b5f
dscott (Dr ) <d.scott@auckland.ac.nz>
2010-03-01 04:18:51 +0000

    Cleaned up code and documentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4777 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/NormalLaplace/R/millsRatio.R
+++ pkg/NormalLaplace/R/millsRatio.R
@@ -1,11 +1,10 @@
-millsR <- function (y, log = FALSE) {
- if (log) {
-   millsR <- pnorm(y, lower.tail = FALSE, log.p = TRUE) -
-             dnorm(y, log = TRUE)
- } else {
-
-   millsR <- (1 - pnorm(y))/dnorm(y) 
-
- }
- return(millsR)
+millsR <- function(y, log = FALSE)
+{
+    if (log) {
+        millsR <- pnorm(y, lower.tail = FALSE, log.p = TRUE) -
+                  dnorm(y, log = TRUE)
+    } else {
+        millsR <- (1 - pnorm(y))/dnorm(y)
+    }
+    return(millsR)
 }
