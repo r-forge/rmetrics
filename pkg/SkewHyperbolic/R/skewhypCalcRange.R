8e84b8648fae2e691884c84bfa6cf03a27eeed38
dscott (Dr ) <d.scott@auckland.ac.nz>
2010-03-17 10:17:33 +0000

    Big changes to skewhypCalcRange.R: step size calculations
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4794 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/SkewHyperbolic/R/skewhypCalcRange.R
+++ pkg/SkewHyperbolic/R/skewhypCalcRange.R
@@ -2,7 +2,7 @@
 skewhypCalcRange <- function(mu = 0, delta = 1, beta = 1, nu = 1,
                              param = c(mu,delta,beta,nu), tol= 10^(-5), ...){
 
-    #check parameters
+    ## check parameters
     parResult <- skewhypCheckPars(param)
     case <- parResult$case
     errMessage <- parResult$errMessage
@@ -12,27 +12,60 @@ skewhypCalcRange <- function(mu = 0, delta = 1, beta = 1, nu = 1,
     beta <- param[3]
     nu <- param[4]
 
-    #find xHigh, xLow and Mode
+    upperProb <- function(x){
+        px <- integrate(dskewhyp, x, Inf, param = param)$value
+        return(px)
+    }
+
+    ## find xHigh, xLow and Mode
     modeDist <- skewhypMode(param = param)
-    xHigh <- modeDist + 0.1
-    while (dskewhyp(x=xHigh, param=param, log=FALSE) > tol){
-        xHigh <- xHigh + 0.1
-    }
-    xLow <- modeDist - 0.1
-    while (dskewhyp(x=xLow, param=param, log=FALSE) > tol){
-        xLow <- xLow - 0.1
+    xHigh <- modeDist + stepSize(delta, nu, beta, "right")
+    while (upperProb(xHigh) > tol){
+        xHigh <- xHigh + stepSize(xHigh - modeDist, nu, beta, "right")
     }
 
-    #find xLower and xUpper
-    zeroFun <- function(x) dskewhyp(x=x, param=param, log=FALSE) - tol
+    lowerProb <- function(x){
+        px <- integrate(dskewhyp, -Inf, x, param = param)$value
+        return(px)
+    }
 
+    xLow <- modeDist  - stepSize(delta, nu, beta, "left")
+    while (lowerProb(xLow) > tol){
+        xLow <- xLow - stepSize(modeDist - xLow, nu, beta, "left")
+    }
+
+    ##find xLower and xUpper
+    zeroFun <- function(x) upperProb(x) - tol
     xUpper <- uniroot(zeroFun, c(modeDist,xHigh),...)$root
+    zeroFun <- function(x) lowerProb(x) - tol
     xLower <- uniroot(zeroFun, c(xLow, modeDist),...)$root
 
-    #put it all together
+    ##put it all together
     range <- c(xLower, xUpper)
 
     return(range)
 
 }
+
+
+stepSize <- function(dist, nu, beta, side = c("right","left"))
+{
+    ## Purpose: determine the step size for a skewhyperbolic
+    ## ----------------------------------------------------------------------
+    ## Arguments:
+    ## ----------------------------------------------------------------------
+    ## Author: David Scott, Date: 17 Mar 2010, 21:50
+    side <- match.arg(side)
+    if (beta > 0){
+        step <- ifelse(side == "right", exp(dist/nu), dist)
+    }
+    if (beta < 0){
+        step <- ifelse(side == "left", exp(dist/nu), dist)
+    }
+    if (isTRUE(all.equal(beta, 0))){
+        step <- exp(dist/nu)
+    }
+    return(step)
+}
+
 
