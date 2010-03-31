ea30da9349bf2d5cfd34197265bcdbe535a6c526
dscott (Dr ) <d.scott@auckland.ac.nz>
2010-03-31 11:11:38 +0000

    Added skewhypTailPlotLine.R with documentation and tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4811 5d4a7380-5c0c-0410-b446-d4a5018c0119

---
+++ pkg/SkewHyperbolic/tests/testskewhypTailPlotLine.R
@@ -0,0 +1,24 @@
+require(DistributionUtils)
+require(SkewHyperbolic)
+source("../R/skewhypTailPlotLine.R")
+### Draw tail plot of some data
+param <- c(0,1,2,10)
+x <- rskewhyp(200, param = param)
+tailPlot(x)
+### Add skew hyperbolic t-distribution line
+skewhypTailPlotLine(x, param = param)
+### Parameters from fit may look better
+paramFit <- skewhypFit(x, plots = FALSE)$param
+tailPlot(x)
+skewhypTailPlotLine(x, param = param)
+skewhypTailPlotLine(x, param = paramFit, col = "steelblue")
+
+### Left tail example
+tailPlot(x, side = "l")
+### Add skew hyperbolic t-distribution line
+skewhypTailPlotLine(x, param = paramFit, side = "l")
+### Log scale on both axes
+tailPlot(x, side = "r", log = "xy")
+### Add skew hyperbolic t-distribution line
+skewhypTailPlotLine(x, param = paramFit, side = "r")
+
