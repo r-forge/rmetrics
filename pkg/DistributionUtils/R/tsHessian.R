38d29e5af9849b9ef128aa9b473c83af8e426d1e
dscott (Dr ) <d.scott@auckland.ac.nz>
2010-05-11 11:53:58 +0000

    Added tsHessian.Rd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4856 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/DistributionUtils/R/tsHessian.R
+++ pkg/DistributionUtils/R/tsHessian.R
@@ -2,26 +2,31 @@ tsHessian <- function(param, fun, ...)
 {
   ## Description:
   ##   Computes two sided (TS) approximated Hessian
-  
+
   ## Source:
   ## This code was borrowed from the fBasics
   ## package, utils-hessian.R with slight modification
-  
-  
+  ## A function borrowed from Kevin Sheppard's Matlab garch toolbox
+  ## as implemented by Alexios Ghalanos in his rgarch package
+
+  ## Notes:
+  ##   optionally requires package Matrix (added as suggestion)
+
+
   ## FUNCTION:
-  
+
   ## Settings:
   n <- length(param)
   fx <- fun(param, ...)
   eps <- .Machine$double.eps
-  
+
   ## Compute the stepsize:
-  h <- eps^(1/3) * 
+  h <- eps^(1/3) *
     apply( as.data.frame(param), 1, FUN = function(z) max(abs(z), 1.0e-2))
   paramh <- param + h
   h <- paramh - param
   ee <- diag(h) # Matrix(diag(h), sparse = TRUE)
-  
+
   ## Compute forward and backward steps:
   gp <- vector(mode = "numeric", length = n)
   for(i in 1:n) gp[i] <- fun(param + ee[, i])
@@ -30,12 +35,12 @@ tsHessian <- function(param, fun, ...)
   H <- h %*% t(h)
   Hm <- H
   Hp <- H
-  
+
   ## Compute double forward and backward steps:
   for(i in 1:n){
     for(j in  i:n){
       Hp[i, j] <- fun(param + ee[, i] + ee[, j])
-      Hp[j, i] <- Hp[i, j]                
+      Hp[j, i] <- Hp[i, j]
       Hm[i, j] <- fun(param - ee[, i] - ee[, j])
       Hm[j, i] <- Hm[i, j]
     }
@@ -49,7 +54,7 @@ tsHessian <- function(param, fun, ...)
       H[j, i] <- H[i, j]
     }
   }
-  
+
   ## Return Value:
   return(H)
 }
