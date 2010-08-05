b7261b356612ec0434f2ce1862290df41c577f59
dscott (Dr ) <d.scott@auckland.ac.nz>
2010-08-05 13:16:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@4939 5d4a7380-5c0c-0410-b446-d4a5018c0119

---
+++ pkg/DistributionUtils/tests/testincompleteBesselK.R
@@ -0,0 +1,46 @@
+require(GeneralizedHyperbolic)
+##source("../R/incompleteBesselK.R")
+
+options(digits = 15)
+for (i in 0:9){
+  ibf <- incompleteBesselK(0.01, 4, i, nmax = 100)
+  print(ibf)
+}
+
+options(digits = 15)
+numIBF <- numeric(10)
+for (i in 0:9){
+  ibf <- incompleteBesselK(4, 0.01, i, nmax = 100)
+  numIBF[i + 1] <- ibf
+  print(ibf)
+}
+
+besselFn <- function(t, x, y, nu) {
+  (t^(-nu - 1))*exp(-x*t - y/t)
+}
+
+intIBF <- numeric(10)
+for (i in 0:9){
+  ibf <- integrate(besselFn, 1, Inf, x = 4, y = 0.01, nu = i)
+  intIBF[i + 1] <- ibf$value
+  print(ibf)
+}
+
+numIBF - intIBF
+system.time({
+            numIBF <- numeric(10)
+            for (i in 0:9){
+              ibf <- incompleteBesselK(4, 0.01, i, tol = 10^(-7), nmax = 100)
+              numIBF[i + 1] <- ibf
+              print(ibf)
+            }
+})
+
+system.time({
+            numIBF <- numeric(10)
+            for (i in 0:9){
+              ibf <- incompleteBesselK(4, 0.01, i, nmax = 100)
+              numIBF[i + 1] <- ibf
+              print(ibf)
+            }
+})
