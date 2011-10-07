bfc83a27eea99dccbaa8674616127ca256530692
savicky (Petr Savicky) <savicky@cs.cas.cz>
2011-10-07 19:03:17 +0000

    combined version names for WELL generators
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@5137 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/randtoolbox/tests/testWELL.R
+++ pkg/randtoolbox/tests/testWELL.R
@@ -1,28 +1,26 @@
 library(randtoolbox)
 
-param <- rbind(
-	c(512, "a"),
-	c(521, "a"),
-	c(521, "b"),
-	c(607, "a"),
-	c(607, "b"),
-	c(800, "a"),
-	c(800, "b"),
-	c(1024, "a"),
-	c(1024, "b"),
-	c(19937, "a"),
-	c(19937, "b"),
-	c(19937, "c"),
-	c(21701, "a"),
-	c(23209, "a"),
-	c(23209, "b"),
-	c(44497, "a"),
-	c(44497, "b"))
+params <- c(
+	"512a",
+	"521a",
+	"521b",
+	"607a",
+	"607b",
+	"800a",
+	"800b",
+	"1024a",
+	"1024b",
+	"19937a",
+	"19937b",
+	"19937c",
+	"21701a",
+	"23209a",
+	"23209b",
+	"44497a",
+	"44497b")
 
-dimnames(param) <- list(NULL, c("order", "version"))
-
-result1 <- rep(NA, times=nrow(param))
-result2 <- rep(NA, times=nrow(param))
+result1 <- rep(NA, times=length(params))
+result2 <- rep(NA, times=length(params))
 
 seed <- floor(2^31*runif(1))
 cat("using seed", seed, "for test of the output of WELL RNG\n")
@@ -30,23 +28,20 @@ cat("using seed", seed, "for test of the output of WELL RNG\n")
 m <- 100
 cat("generating sequences of the length", m, "from each generator\n")
 
-for (i in 1:nrow(param))
+for (i in 1:length(params))
 {
 	cat(i, "")
-	order <- param[i, 1]
-	version <- param[i, 2]
-	generator <- paste(order, version, sep="")
-	set.generator("WELL", order=order, version=version, seed=seed)
+	set.generator("WELL", version=params[i], seed=seed)
 	s0 <- getWELLState()
 	x <- runif(m)
-	out <- rngWELLScriptR(m, s0, generator, includeState=TRUE)
+	out <- rngWELLScriptR(m, s0, params[i], includeState=TRUE)
 	s1 <- getWELLState()
 	result1[i] <- all(x == out$x)
 	result2[i] <- all(s1 == out$state)
 }
 
 cat("\n\n")
-print(cbind(data.frame(param), result1=result1, result2=result2))
+print(data.frame(params, result1, result2))
 
 stopifnot(all(result1, result2))
 
