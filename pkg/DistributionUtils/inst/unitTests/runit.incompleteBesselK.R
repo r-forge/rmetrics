389b4c9b6f1496697fa84481ff626f5c16db2b4d
dscott (Dr ) <d.scott@auckland.ac.nz>
2012-01-05 16:57:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@5179 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/DistributionUtils/inst/unitTests/runit.incompleteBesselK.R
+++ pkg/DistributionUtils/inst/unitTests/runit.incompleteBesselK.R
@@ -13,6 +13,55 @@ test.incompleteBesselK <- function()
     ## ----------------------------------------------------------------------
     ## Author: David Scott, Date: 5 Jan 2012
 
+    ## Original version of the test file which was misplaced
+
+    ## Data from Harris (2008)
+    HarrisCase1 <- c(2.225310761266469,
+                     0.213894166822940,
+                     0.054503469799701,
+                     0.023253121507708,
+                     0.013042750996080,
+                     0.008567534990649,
+                     0.006208676806601,
+                     0.004801085238177,
+                     0.003884072049627,
+                     0.003246798003149)
+
+    HarrisCase2 <- 0.000012249987981
+
+    HarrisCase3 <- 0.00000041500106421228
+
+    HarrisCase4 <- 0.000528504325244
+
+    ## Calculations for Harris Case 1
+    numIBF <- numeric(10)
+    for (i in 0:9){
+        ibf <- incompleteBesselK(0.01, 4, i, nmax = 100)
+        numIBF[i + 1] <- ibf
+    }
+    maxDiff <- max(abs(numIBF - HarrisCase1))
+    checkTrue(maxDiff <= 10^(-13),
+              msg = paste("Harris Case 1: maxDiff =", maxDiff))
+
+    ## Calculations for Harris Case 2
+    ibf <- incompleteBesselK(4.95, 5, 2)
+    maxDiff <- max(abs(ibf - HarrisCase2))
+    checkTrue(maxDiff <= 10^(-13), msg = "Harris Case 2")
+
+    ## Calculations for Harris Case 3
+    ibf <- incompleteBesselK(10, 2, 6)
+    maxDiff <- max(abs(ibf - HarrisCase3))
+    checkTrue(maxDiff <= 10^(-11), msg = "Harris Case 3")
+
+    ## Calculations for Harris Case 4
+    ibf <- incompleteBesselK(3.1, 2.6, 5)
+    maxDiff <- max(abs(ibf - HarrisCase4))
+    checkTrue(maxDiff <= 10^(-13),
+              msg = paste("Harris Case 4: maxDiff =", maxDiff))
+
+    ## Newer version of the test file: not necessarily better
+    ## But includes testing of the pure R version
+
     ## Values given by Harris (2008)
     checkEquals(incompleteBesselK(0.01, 4, 0), 2.225310761266469)
     checkEquals(incompleteBesselKR(0.01, 4, 0), 2.225310761266469)
