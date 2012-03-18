998b543def30eb0aa129e407448e91258ccb4f31
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2012-03-18 22:56:24 +0000

    isDaily, isMonthly, isQuarterly, isRegular merged to one script file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@5237 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/timeDate/R/calendar-isDaily.R
+++ pkg/timeDate/R/calendar-isDaily.R
@@ -16,40 +16,147 @@
 
 
 ################################################################################
-# FUNCTION:                DESCRIPTION:
-#  isDaily,timeDate-method  Tests if a timeDate object has daily time stamps
+# FUNCTION:                      DESCRIPTION:
+#  isDaily,timeDate-method        Tests 'timeDate' has daily time stamps
+#  isMonthly,timeDate-method      Tests 'timeDate' has monthly time stamps
+#  isQuarterly,timeDate-method    Tests 'timeDate' has quarterly time stamps
+#  isRegular,timeDate-method      Tests 'timeDate' has regular time stamps
 ################################################################################
-
 
 
 setMethod("isDaily", "timeDate", function(x)
-     {
-         # A function implemented by Diethelm Wuertz
+{
+    # A function implemented by Diethelm Wuertz
+    
+    # Descriptions:
+    #   Test if a timeDate object has daily time stamps
+    
+    # Example:
+    #   isDaily(timeSequence(by = "day", length.out = 20))
+    #   isDaily(timeCalendar())
+    #   isDaily(timeSequence(by = "hour", length.out = 100))
+    
+    # Details:
+    #   Definition: A timeDate Object is a Daily timeDate object
+    #   if we have not more than one date/time stamp per day.
+    
+    # Arguments:
+    #   x - an object of class timeDate
+    
+    # FUNCTION:
+    
+    # Daily ?
+    num <- as.numeric(as.POSIXct(x))
+    daily <- seq(from = num[1], by = 60*60*24, length.out=length(num))
+    
+    # Return
+    (identical(daily, num))
+})
 
-         # Descriptions:
-         #   Test if a timeDate object has daily time stamps
-
-         # Example:
-         #   isDaily(timeSequence(by = "day", length.out = 20))
-         #   isDaily(timeCalendar())
-         #   isDaily(timeSequence(by = "hour", length.out = 100))
-
-         # Details:
-         #   Definition: A timeDate Object is a Daily timeDate object
-         #   if we have not more than one date/time stamp per day.
-
-         # Arguments:
-         #   x - an object of class timeDate
-
-         # FUNCTION:
-
-         # Daily ?
-         num <- as.numeric(as.POSIXct(x))
-         daily <- seq(from = num[1], by = 60*60*24, length.out=length(num))
-
-         # Return
-         (identical(daily, num))
-     })
 
 ################################################################################
 
+
+setMethod("isMonthly", "timeDate", function(x)
+{
+    # A function implemented by Diethelm Wuertz
+    
+    # Descriptions:
+    #   Tests if a timeDate object has monthly time stamps
+    
+    # Arguments:
+    #   x - an object of class timeDate
+    
+    # Details:
+    #   Definition: A timeDate Object is a Monthly timeDate object
+    #   if we have not more than one date/time stamp per month.
+    #   Note a monthly series is also a daily series.
+    
+    # Example:
+    #   isMonthly(timeSequence(by = "day", length.out = 20))
+    #   isMonthly(timeCalendar())
+    #   isDaily(timeCalendar())
+    #   isMonthly(timeSequence(by = "hour", length.out = 100))
+    
+    # FUNCTION:
+    
+    # Monthly ?
+    m <- c(timeDate::months(x)) #-> c() to remove attributes
+    # (m[1] -1) -> shift vector to match first entry in m
+    monthly <- seq(from = m[1]-1, length.out=length(m)) %% 12 + 1
+    
+    # Return
+    (identical(monthly, m))
+})
+
+
+################################################################################
+
+
+setMethod("isQuarterly", "timeDate", function(x)
+{
+    # A function implemented by Diethelm Wuertz
+    
+    # Descriptions:
+    #   Tests if a timeDate object has quarterly time stamps
+    
+    # Arguments:
+    #   x - an object of class timeDate
+    
+    # Details:
+    #   Definition: A timeDate Object is a Quarterly timeDate object
+    #   if we have not more than one date/time stamp per quarter
+    #   Note a quarterly series is also a daily and a monthly series.
+    
+    # Example:
+    #   isQuarterly(timeSequence(by = "day", length.out = 20))
+    #   isQuarterly(timeCalendar())
+    #   isQuarterly(timeSequence(by = "hour", length.out = 100))
+    #   isQuarterly(timeCalendar()[(1:4)*3])
+    #   isMonthly(timeCalendar()[(1:4)*3])
+    #   isDaily(timeCalendar()[(1:4)*3])
+    
+    # FUNCTION:
+    
+    # Quartertly ?
+    m <- c(timeDate::months(x)) #-> c() to remove attributes
+    # (m[1] -1) -> shift vector to match first entry in m
+    quarterly <- seq(from = m[1]-1, by = 3, length=length(m)) %% 12 + 1
+    
+    # Return
+    (identical(quarterly, m))
+})
+
+
+################################################################################
+
+
+
+setMethod("isRegular", "timeDate",  function(x)
+{
+    # A function implemented by Diethelm Wuertz
+    
+    # Descriptions:
+    #   Tests if a timeDate object has regular time stamps
+    
+    # Example:
+    #   isRegular(timeSequence(by = "day", length.out = 20))
+    #   isRegular(timeCalendar())
+    #   isRegular(timeSequence(by = "hour", length.out = 100))
+    
+    # Details:
+    #   Definition: A timeDate Object is a Regular timeDate object
+    #   if the timeDate object is either monthly or quarterly,
+    #   otherwise not.
+    
+    # Arguments:
+    #   x - an object of class timeDate
+    
+    # FUNCTION:
+    
+    # Regular ?
+    (isMonthly(x) | isQuarterly(x))
+    })
+
+
+################################################################################
