271a4949f584d33aee9180bde65b1b38d3ff970d
chalabi (Mr. ) <chalabi@phys.ethz.ch>
2012-03-19 13:29:59 +0000

    read.lines() is now a wrapper for readLines().
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@5250 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fImport/R/read-lines.R
+++ pkg/fImport/R/read-lines.R
@@ -6,12 +6,12 @@
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Library General Public License for more details.
 #
-# You should have received A copy of the GNU Library General 
-# Public License along with this library; if not, write to the 
-# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# You should have received A copy of the GNU Library General
+# Public License along with this library; if not, write to the
+# Free Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 
 # Copyrights (C) for this R-port:
@@ -24,38 +24,12 @@
 #  read.lines              A synonyme function call to readLines
 ################################################################################
 
+# wrapper function of read.lines to have consistent function names as
+# described in ebook.
 
-read.lines <-
-function(con=stdin(), n=-1L, ok=TRUE, warn=FALSE, encoding="unknown") 
-{
-    # Description:
-    #   A synonyme function call to readLines
-    
-    # Arguments:
-    #   con - a connection object or a character string
-    #   n - an integer, the (maximal) number of lines to read. 
-    #       Negative values indicate that one should read up 
-    #       to the end of input on the connection.
-    #   ok - a logical, is it OK to reach the end of the connection 
-    #       before n > 0 lines are read? If not, an error will be 
-    #       generated.
-    #   warn - a logical, warn if a text file is missing a final EOL.
-    #       The default is FALSE, note different from function
-    #       \code{readLines}.
-    #   encoding - encoding to be assumed for input strings. 
-    
-    # FUNCTION
-    
-    # Check connection
-    if (is.character(con)) {
-        con <- file(con, "r")
-        on.exit(close(con))
-    }
-    
-    # Return Value:
-    .Internal(readLines(con, n, ok, warn, encoding))
-}
-
+read.lines <- function(con = stdin(), n = -1L, ok = TRUE, warn = FALSE,
+                       encoding = "unknown")
+    readLines(con = con, n = n, ok = ok, warn = warn, encoding = encoding)
 
 ################################################################################
 
