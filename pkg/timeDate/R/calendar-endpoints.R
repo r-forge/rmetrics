01d2147c720390e2dc721e02f8472e07bee6bffc
wuertz (Diethelm Wuertz) <wuertz@phys.ethz.ch>
2012-03-20 06:29:35 +0000

    .endpoints renamed yo endpoints and man page added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@5259 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/timeDate/R/calendar-endpoints.R
+++ pkg/timeDate/R/calendar-endpoints.R
@@ -21,46 +21,53 @@
 ################################################################################
 
 
-.endpoints <- 
-function (x, on = c("months", "years", "qaurters", "weeks", "days", 
+endpoints <- 
+function (x, on = c("months", "years", "quarters", "weeks", "days", 
     "hours", "minutes", "seconds"), k = 1) 
 {
     # Description:
-    #   Returns endpoint indexes from a 'timeDate' object
+    #   Returns endpoint indexes from a 'timeDate' object.
     
     # Arguments:
-    #   x - an object of class(timeDate)
+    #   x - a 'timeDate' object  
+    #   on - the periods endpoints to find as a character string
+    #   k - along every k-th element
     
     # Note:
-    #   Behaves like function entpoints() from R package xts
-    #   The Index rules are borrowed from Jeff Ryans endpoints() function
-    
+    #   Behaves like function entpoints() from R package xts, which
+    #     extracts index values of a given "xts" object corresponding 
+    #     to the last observations given a period specified by "on",
+    #   with a zero added to the beginning of the vector, and the 
+    #     index of the last observation in x at the end.
+    #   The Index rules are borrowed from Jeff Ryans endpoints() 
+    #     function.
+       
     # FUNCTION:
     
     # Convert to POSIX:
-    on = match.arg(on)
-    posix = as.POSIXct(x)
-    .posix = unclass(posix)
+    on <- match.arg(on)
+    posix <- as.POSIXct(x)
+    .posix <- unclass(posix)
     
     # Apply index extraction rules:
     if (on == "years") {
-        ans = as.integer(which(diff(as.POSIXlt(posix)$year %/% k + 1) != 0))
+        ans <- as.integer(which(diff(as.POSIXlt(posix)$year %/% k + 1) != 0))
     } else if (on == "quarters") {
-        ans = as.integer(which(diff((as.POSIXlt(posix)$mon %/% 3) + 1) != 0))
+        ans <- as.integer(which(diff((as.POSIXlt(posix)$mon %/% 3) + 1) != 0))
     } else if (on == "months") {
-        ans = as.integer(which(diff(as.POSIXlt(posix)$mon %/% k + 1) != 0))
+        ans <- as.integer(which(diff(as.POSIXlt(posix)$mon %/% k + 1) != 0))
     } else if (on == "weeks") {
-        as.integer(which(diff((.posix + (3L * 86400L))%/%604800L %/% k + 1) != 0))
+        ans <- as.integer(which(diff((.posix + (3L * 86400L))%/%604800L %/% k + 1) != 0))
     } else if (on == "days") {
-        ans = as.integer(which(diff(.posix %/% 86400L %/% k + 1) != 0))
+        ans <- as.integer(which(diff(.posix %/% 86400L %/% k + 1) != 0))
     } else if (on == "hours") {
-        ans = as.integer(which(diff(.posix %/% 3600L %/% k + 1) != 0))
+        ans <- as.integer(which(diff(.posix %/% 3600L %/% k + 1) != 0))
     } else if (on == "minutes" || on == "mins") {
-        ans = as.integer(which(diff(.posix %/% 60L %/% k + 1) != 0))
+        ans <- as.integer(which(diff(.posix %/% 60L %/% k + 1) != 0))
     } else if (on == "seconds" || on == "secs") {
-        ans = as.integer(which(diff(.posix %/% k + 1) != 0))
+        ans <- as.integer(which(diff(.posix %/% k + 1) != 0))
     } 
-    ans = c(0, ans, NROW(x))
+    ans <- c(0, ans, NROW(x))
     
     # Return Value:
     ans
