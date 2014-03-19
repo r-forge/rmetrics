737a8e3895feab2cf21f788af11628de21546a81
tsetz (Tobias Setz) <tobias.setz@live.com>
2014-03-19 13:41:10 +0000

    Updated DESCRIPTION and NAMESPACE files and added a custom startup message into zzz.R...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@5626 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fMultivar/R/zzz.R
+++ pkg/fMultivar/R/zzz.R
@@ -27,7 +27,7 @@
 
   packageStartupMessage( "\n" )
   packageStartupMessage( "Rmetrics Package fMultivar" ) 
-  packageStartupMessage( "Analysing and Modeling Multivariate Financial Return Distributions" )-
+  packageStartupMessage( "Analysing and Modeling Multivariate Financial Return Distributions" )
   packageStartupMessage( "Copyright (C) 2005-2014 Rmetrics Association Zurich" )  
   packageStartupMessage( "Educational Software for Financial Engineering and Computational Science" ) 
   packageStartupMessage( "Rmetrics is free software and comes with ABSOLUTELY NO WARRANTY." ) 
