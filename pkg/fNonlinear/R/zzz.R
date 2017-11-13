4a3ac49f92daf45880909910acd25458478c1111
tsetz (Tobias Setz) <tobias.setz@live.com>
2017-11-13 13:55:56 +0000

    Checks and adaptions for R 3.4.2. Startup Message removed.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@6063 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fNonlinear/R/zzz.R
+++ pkg/fNonlinear/R/zzz.R
@@ -24,13 +24,13 @@
   # some people use it to bombard users with 
   # messages using 
   
-  packageStartupMessage( "\n" )
-  packageStartupMessage( "Rmetrics Package fNonlinear" ) 
-  packageStartupMessage( "Nonlinear and Chaotic Time Series Modelling" )
-  packageStartupMessage( "Copyright (C) 2005-2014 Rmetrics Association Zurich" )  
-  packageStartupMessage( "Educational Software for Financial Engineering and Computational Science" ) 
-  packageStartupMessage( "Rmetrics is free software and comes with ABSOLUTELY NO WARRANTY." ) 
-  packageStartupMessage( "https://www.rmetrics.org --- Mail to: info@rmetrics.org" ) 
+  # packageStartupMessage( "\n" )
+  # packageStartupMessage( "Rmetrics Package fNonlinear" ) 
+  # packageStartupMessage( "Nonlinear and Chaotic Time Series Modelling" )
+  # packageStartupMessage( "Copyright (C) 2005-2014 Rmetrics Association Zurich" )  
+  # packageStartupMessage( "Educational Software for Financial Engineering and Computational Science" ) 
+  # packageStartupMessage( "Rmetrics is free software and comes with ABSOLUTELY NO WARRANTY." ) 
+  # packageStartupMessage( "https://www.rmetrics.org --- Mail to: info@rmetrics.org" )
 }
 
 
