4bd3d8e64c2ac36343b1f4256b5fdb58285c296d
dscott (Dr ) <d.scott@auckland.ac.nz>
2018-11-26 06:49:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@6125 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/SkewHyperbolic/R/skewhypFit.R
+++ pkg/SkewHyperbolic/R/skewhypFit.R
@@ -5,53 +5,59 @@ skewhypFit <- function (x, freq = NULL, breaks = NULL, startValues = "LA",
                         controlNM = list(maxit = 1000),
                         maxitNLM = 1500, ...){
 
-    xName <- paste(deparse(substitute(x), 500), collapse = "\n")
+  xName <- paste(deparse(substitute(x), 500), collapse = "\n")
 
-    #allow frequency data
-    if (!is.null(freq)) {
-        if (length(freq) != length(x)) {
-            stop("vectors x and freq are not of the same length")}
-        x <- rep(x, freq)
-    }
+  ## allow frequency data
+  if (!is.null(freq)) {
+    if (length(freq) != length(x)) {
+      stop("vectors x and freq are not of the same length")}
+    x <- rep(x, freq)
+  }
 
-    #get the starting value information
-    if (startValues == "US") {
-        startInfo <- skewhypFitStart(x, breaks = breaks,
-                     startValues = startValues, paramStart = paramStart)
-    }
-    if(startValues == "LA"){
-        startInfo <- skewhypFitStart(x, breaks = breaks,
-                     startValues = startValues)
-    }
-    paramStart <- startInfo$paramStart
-    svName <- startInfo$svName
-    breaks <- startInfo$breaks
-    empDens <- startInfo$empDens
-    midpoints <- startInfo$midpoints
+  ## get the starting value information
+  if (startValues == "US") {
+    startInfo <- skewhypFitStart(x, breaks = breaks,
+                                 startValues = startValues,
+                                 paramStart = paramStart)
+  }
+  if(startValues == "LA"){
+    startInfo <- skewhypFitStart(x, breaks = breaks,
+                                 startValues = startValues)
+  }
+  paramStart <- startInfo$paramStart
+  svName <- startInfo$svName
+  breaks <- startInfo$breaks
+  empDens <- startInfo$empDens
+  midpoints <- startInfo$midpoints
 
-    #maximise likelihood
-    llfunc <- function(param){
-        param[2] <- exp(param[2])
-        param[4] <- exp(param[4])
-        -sum(dskewhyp(x, param = param, log = TRUE),
-             na.rm = TRUE)}
+  ## Ensure parameters are positive
+  logParamStart <- c(paramStart[1], log(paramStart[2]),
+                     paramStart[3], log(paramStart[4]))
 
-    ind <- 1:4
-    if (method == "BFGS") {
-        opOut <- optim(paramStart, llfunc, NULL, method = "BFGS",
-            hessian = hessian, control = controlBFGS, ...)
-    }
-    if (method == "Nelder-Mead") {
-        opOut <- optim(paramStart, llfunc, NULL, method = "Nelder-Mead",
-            hessian = hessian, control = controlNM, ...)
-    }
-    if (method == "nlm") {
-        ind <- c(2, 1, 5, 4)
-        opOut <- nlm(llfunc, paramStart, hessian = hessian, iterlim = maxitNLM,
-            ...)
-    }
+  ## maximise likelihood
+  llfunc <- function(param){
+    param[2] <- exp(param[2])
+    param[4] <- exp(param[4])
+    -sum(dskewhyp(x, param = param, log = TRUE),
+         na.rm = TRUE)}
 
-    #results
+  ind <- 1:4
+  if (method == "BFGS") {
+    opOut <- optim(logParamStart, llfunc, NULL, method = "BFGS",
+                   hessian = hessian, control = controlBFGS, ...)
+  }
+  if (method == "Nelder-Mead") {
+    opOut <- optim(logParamStart, llfunc, NULL, method = "Nelder-Mead",
+                   hessian = hessian, control = controlNM, ...)
+  }
+  if (method == "nlm") {
+    ind <- c(2, 1, 5, 4)
+    opOut <- nlm(llfunc, logParamStart, hessian = hessian,
+                 iterlim = maxitNLM, ...)
+  }
+
+  ## results
+  if (method %in% c("BFGS","Nelder-Mead","nlm")){
     param <- as.numeric(opOut[[ind[1]]])[1:4]
     param[2] <- exp(param[2])
     param[4] <- exp(param[4])
@@ -59,20 +65,62 @@ skewhypFit <- function (x, freq = NULL, breaks = NULL, startValues = "LA",
     maxLik <- -(as.numeric(opOut[[ind[2]]]))
     conv <- as.numeric(opOut[[ind[4]]])
     iter <- as.numeric(opOut[[ind[3]]])[1]
-    paramStart <- c(paramStart[1], exp(paramStart[2]), paramStart[3],
-                    exp(paramStart[4]))#unlog the logged parameters
-    fitResults <- list(param=param , maxLik = maxLik,
-                       hessian = if (hessian) opOut$hessian else NULL,
-                       method = method, conv = conv, iter = iter, x=x, xName = xName,
-                       paramStart = paramStart, svName = svName, startValues = startValues,
-                       breaks = breaks, midpoints = midpoints, empDens = empDens)
-    class(fitResults) <- "skewhypFit"
-    if (printOut)
-        print.skewhypFit(fitResults, ...)
-    if (plots)
-        plot.skewhypFit(fitResults, ...)
+  }
 
-    return(fitResults)
+  fitResults <- list(param = param , maxLik = maxLik,
+                     hessian = if (hessian) opOut$hessian else NULL,
+                     method = method, conv = conv, iter = iter, x = x,
+                     xName = xName,
+                     paramStart = paramStart, svName = svName,
+                     startValues = startValues,
+                     breaks = breaks, midpoints = midpoints,
+                     empDens = empDens)
+  class(fitResults) <- "skewhypFit"
+  if (printOut)
+    print.skewhypFit(fitResults, ...)
+  if (plots)
+    plot.skewhypFit(fitResults, ...)
+
+  return(fitResults)
 }
 
+skewhypFitEM <- function(x, freq = NULL, breaks = NULL, startValues = "LA",
+                         paramStart = NULL,  method = "Nelder-Mead",
+                         hessian = TRUE, plots = FALSE, printOut = TRUE,
+                         controlBFGS = list(maxit = 200),
+                         controlNM = list(maxit = 1000),
+                         maxitNLM = 1500, ...){
+  xName <- paste(deparse(substitute(x), 500), collapse = "\n")
 
+  ## allow frequency data
+  if (!is.null(freq)) {
+    if (length(freq) != length(x)) {
+      stop("vectors x and freq are not of the same length")}
+    x <- rep(x, freq)
+  }
+
+  ## get the starting value information
+  if (startValues == "US") {
+    startInfo <- skewhypFitStart(x, breaks = breaks,
+                                 startValues = startValues,
+                                 paramStart = paramStart)
+  }
+  if(startValues == "LA"){
+    startInfo <- skewhypFitStart(x, breaks = breaks,
+                                 startValues = startValues)
+  }
+  if(startValues == "MM"){
+    startInfo <- skewhypFitStart(x, breaks = breaks,
+                                 startValues = startValues, ...)
+  }
+  paramStart <- startInfo$paramStart
+  svName <- startInfo$svName
+  breaks <- startInfo$breaks
+  empDens <- startInfo$empDens
+  midpoints <- startInfo$midpoints
+
+
+
+}
+
+skewhypFitEStep <- function(
--- pkg/SkewHyperbolic/R/skewhypFitStart.R
+++ pkg/SkewHyperbolic/R/skewhypFitStart.R
@@ -1,66 +1,81 @@
 ###### start values function ##############################################
 skewhypFitStart <- function (x, breaks = NULL, startValues = "LA",
-                             paramStart = NULL)
+                             paramStart = NULL, ...)
 {
-    if (startValues == "US") {
-        ## user supplied starting values
-        x <- as.numeric(na.omit(x))
-        svName <- "User Specified"
-        if (is.null(paramStart))  stop("paramStart must be specified")
-        if (!is.null(paramStart)) {
-            #check parameters
-            parResult <- skewhypCheckPars(paramStart)
-            case <- parResult$case
-            errMessage <- parResult$errMessage
-            if(case == "error") stop(errMessage)
-            paramStart <- c(beta = paramStart[1], delta = log(paramStart[2]),
-                            mu = paramStart[3], nu = log(paramStart[4]))
-        }
-        names(paramStart) <- c("mu", "log(delta)","beta" , "log(nu)")
-        histData <- hist(x, plot = FALSE, right = FALSE)
-        if (is.null(breaks)) {
-            breaks <- histData$breaks
-        }
-        midpoints <- histData$mids
-        empDens <- ifelse(!is.finite(log(histData$density)), NA,
-                          histData$density)
-        ## maxIndex <- order(empDens, na.last = FALSE)[length(empDens)]
-        list(paramStart = paramStart, breaks = breaks,
-             midpoints = midpoints, empDens = empDens, svName = svName)
+  if (startValues == "US") {
+    ## user supplied starting values
+    x <- as.numeric(na.omit(x))
+    svName <- "User Specified"
+    if (is.null(paramStart))  stop("paramStart must be specified")
+    if (!is.null(paramStart)) {
+      ## check parameters
+      parResult <- skewhypCheckPars(paramStart)
+      case <- parResult$case
+      errMessage <- parResult$errMessage
+      if(case == "error") stop(errMessage)
+      paramStart <- c(beta = paramStart[1], delta = paramStart[2],
+                      mu = paramStart[3], nu = paramStart[4])
     }
-    else if(startValues == "LA") {
-        ## combination of moments and linear approx to log density
+    names(paramStart) <- c("mu", "delta", "beta", "nu")
+    histData <- hist(x, plot = FALSE, right = FALSE)
+    if (is.null(breaks)) {
+      breaks <- histData$breaks
+    }
+    midpoints <- histData$mids
+    empDens <- ifelse(!is.finite(log(histData$density)), NA,
+                      histData$density)
+    ## maxIndex <- order(empDens, na.last = FALSE)[length(empDens)]
+    list(paramStart = paramStart, breaks = breaks,
+         midpoints = midpoints, empDens = empDens, svName = svName)
+  } else {
+    if(startValues == "LA") {
+      ## combination of moments and linear approx to log density
+      x <- as.numeric(na.omit(x))
+      svName <- "Linear Approximation"
+      start <- skewhypFitStartLA(x, breaks = breaks)
+      par <- as.vector(start$param)
+      list(paramStart = c(mu  = par[1], "delta" = par[2],
+                          beta= par[3], "nu"    = par[4]),
+           breaks = start$breaks,
+           midpoints = start$mids,
+           empDens = start$dens,
+           svName = svName)
+    } else {
+      if (startValues == "MM"){
         x <- as.numeric(na.omit(x))
-        svName <- "Linear Approximation"
-        start <- skewhypFitStartLA(x, breaks = breaks)
+        svName <- "Method of Moments"
+        start <- skewhypFitStartMM(x, nuTol = 1e-6, ...)
         par <- as.vector(start$param)
-        list(paramStart = c(mu  = par[1], "log(delta)" = log(par[2]),
-                            beta= par[3], "log(nu)"    = log(par[4])),
+        list(paramStart = c(mu  = par[1], "delta" = par[2],
+                            beta= par[3], "nu"    = par[4]),
              breaks = start$breaks,
              midpoints = start$mids,
              empDens = start$dens,
              svName = svName)
-    } else
+      } else {
         stop("invalid 'startValues' string: ", startValues)
+      }
+    }
+  }
 }
 
 ###### Linear Approximation function #######################################
 skewhypFitStartLA <- function(x, breaks = NULL){
 
-    #fit using combination of method of moments and a linear approxination
-    #to the log density in the tails
+    ## fit using combination of method of moments and a linear approximation
+    ## to the log density in the tails
 
-    #histogram
+    ## histogram
     if(is.null(breaks)) breaks <- 30
     histInfo <- hist(x, plot = FALSE, breaks = breaks)
     mids <- as.matrix(histInfo$mids)
-    #density
+    ## density
     density <- density(x)
-    #find the closest density estimate to the midpoint
+    ## find the closest density estimate to the midpoint
     func <- function(x) which.min(abs(density$x - x))
     estx <- apply(mids, 1, func)
     esty <- density$y[estx]
-    #lower and upper tail
+    ## lower and upper tail
     quantiles <- quantile(x, probs = c(0.1,0.9))
     lower <-  which(mids <= quantiles[1])
     upper <-  which(mids >= quantiles[2])
@@ -68,7 +83,7 @@ skewhypFitStartLA <- function(x, breaks = NULL){
     xUpper <- mids[upper]
     yLower <- esty[lower]
     yUpper <- esty[upper]
-    #stop if either tail has less than two points
+    ## stop if either tail has less than two points
     if( length(xLower) < 4 | length(xUpper) < 4 )
         stop("not enough points to model tail behaviour")
 
@@ -78,25 +93,25 @@ skewhypFitStartLA <- function(x, breaks = NULL){
     if( abs(skew) < 0.1 ) {     ## SYMMETRY ##
         beta <- 0
         mu <-  mean(x)
-        #fit models
+        ## fit models
         upperModel <- lm( log(yUpper) ~ log(abs(xUpper)) )
         lowerModel <- lm( log(yLower) ~ log(abs(xLower)) )
-        #estimates
+        ## estimates
         nuLow <- -2*(upperModel$coeff[2] + 1)
         nuUpp <- -2*(lowerModel$coeff[2] + 1)
-        #average to get nu
+        ## average to get nu
         nu <- mean(c(nuLow,nuUpp))
-        #nu must be >4
+        ## nu must be >4
         if(nu <= 4) nu <- 4.1
-        #find delta
+        ##find delta
         delta <- sqrt( var(x)*(nu - 2) )
     }
     else if( skew >= 0.1 ) {    ## POSITIVE SKEW ##
         ## heavy tail is upper tail
-                                        #fit models
+                                        # fit models
         lowerModel <- lm( log(yLower) ~ log(abs(xLower)) + abs(xLower) )
         upperModel <- lm( log(yUpper) ~ log(abs(xUpper)) )
-                                        #estimates
+                                        # estimates
         estLow <- lowerModel$coeff[c(2,3)]
         nuLow <- -2*(estLow[1] + 1)
         betaLow <- (estLow[2]/ - 2)
@@ -113,7 +128,7 @@ skewhypFitStartLA <- function(x, breaks = NULL){
                                  32*beta^2*var)^(1/2))/beta^2*(nu - 2)
         sol2 <- -1/4*(nu - 4 + (nu^2 - 8*nu + 16 + 8*beta^2*var*nu -
                                 32*beta^2*var)^(1/2))/beta^2*(nu - 2)
-                                        #delta^2 must be positive
+                                        # delta^2 must be positive
         delta2 <- if(sol1 > 0) sol1 else sol2
         delta <- sqrt(delta2)
                                         #solve for mu
@@ -122,10 +137,10 @@ skewhypFitStartLA <- function(x, breaks = NULL){
 
     ## NEGATIVE SKEW ##
     if(skew <= -0.1) { # heavy tail is lower tail
-        #fit models
+        ## fit models
         lowerModel <- lm( log(yLower) ~ log(abs(xLower)) )
         upperModel <- lm( log(yUpper) ~ log(abs(xUpper)) + abs(xUpper) )
-        #estimates
+        ## estimates
         estLow <- lowerModel$coeff[2]
         nuLow <- -2*(estLow + 1)
         estUpp <- upperModel$coeff[c(2,3)]
@@ -145,14 +160,66 @@ skewhypFitStartLA <- function(x, breaks = NULL){
         ## delta^2 must be positive
         delta2 <- if(sol1 > 0) sol1 else sol2
         delta <- sqrt(delta2)
-        #solve for mu
+        ## solve for mu
         mu <- mean(x) - beta*delta2/(nu-2)
     }
 
-    #return results
+    ## return results
     param <- c(mu,delta,beta,nu)
     names(param) <- c("mu","delta","beta","nu")
     out <- list(param = param, breaks = histInfo$breaks,
                 mids = as.vector(mids), dens = esty)
     return(out)
 }
+
+###### Method of Moments function #######################################
+skewhypFitStartMM <- function(x, nuTol = 1e-6, nuStart = 5, ...){
+
+  ## from Aas& Haff (2004)
+  require(DistributionUtils)
+  require(numDeriv)
+  ## Calculate moments
+  m1 <- mean(x, na.rm = TRUE)
+  m2 <- var(x, na.rm = TRUE)
+  m3 <- skewness(x, na.rm = TRUE)
+  m4 <- kurtosis(x, na.rm = TRUE)
+
+  ## Estimate nu
+  nuFn <- function(nu, m3, m4){
+    kp <- (1/(3*nu^2 - 2*nu - 32))*
+          (1 - sqrt(1 - (3*nu^2 - 2*nu - 32)*
+          (12*(5*nu - 22) - (nu - 6)*(nu - 8)*m4)/
+          (216*(nu -2)^2*(nu - 4))))
+    fn <- (4 - 6*(nu + 2)*(nu - 2)*kp)*sqrt(2)*sqrt(nu - 4)*
+          sqrt(1 - 6*(nu - 2)*(nu - 4)*kp) - m3*(nu - 6)
+    fn^2
+  }
+
+  nuSoln <- nlm(nuFn, p = nuStart, m3 = m3, m4 = m4, fscale = 0, ...)
+  solnMin <- nuSoln$minimum
+  if (abs(solnMin) > nuTol){
+    stop("Minimum of function to find nu is greater than nuTol")
+  } else {
+    nu <- nuSoln$estimate
+    kp <- (1/(3*nu^2 - 2*nu - 32))*
+          (1 - sqrt(1 - (3*nu^2 - 2*nu - 32)*
+          (12*(5*nu - 22) - (nu - 6)*(nu - 8)*m4)/
+          (216*(nu - 2)^2*(nu - 4))))
+    delta2 <- (6*(nu - 2)^2*(nu - 4)*m2)*kp
+    if (delta2 < 0){
+      stop("delta^2 is negative in method of moments estimates")
+    } else {
+      delta <- sqrt(delta2)
+    }
+    beta <- sign(m3)*sqrt((nu - 2)*(nu - 4)*(m2*(nu - 2) - delta2)/(2*delta2^2))
+    mu <- m1 - beta*delta2/(nu - 2)
+    ## return results
+    param <- c(mu,delta,beta,nu)
+    names(param) <- c("mu","delta","beta","nu")
+    out <- list(param = param)
+    return(out)
+  }
+
+}
+
+
