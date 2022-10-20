69ce8e59adfdccf5486f50e2c89d612c05f1f5a0
geobosh (Dr ) <georgi.boshnakov@manchester.ac.uk>
2022-10-20 16:08:37 +0000

    add msft.dat.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@6310 5d4a7380-5c0c-0410-b446-d4a5018c0119

---
+++ pkg/fBasics/data/msft.dat.R
@@ -0,0 +1,18 @@
+## take "extdata/msft.csv" from package timeSeries which is identical to the file
+## "msft.dat.csv" "msft.dat.csv" which was formerly here in data/
+fn <- system.file("extdata", "msft.csv", package = "timeSeries")
+
+## We could read the first column as Date but do it as 'factor' for backward
+## compatibility, since that was its class in object 'msft.dat'.
+##
+## but we do avoid mangling the name of the first column.
+msft.dat <- utils::read.csv2(fn, colClasses = c("factor", rep("numeric", 5)),
+                             dec = ".", check.names = FALSE)
+
+## a comparison of the old 'msft.dat' and the new one gave:
+##
+##     > all.equal(msft.dat, new_msft_dat)
+##     [1] "Names: 1 string mismatch"
+##
+## as expected. The difference is in the name of the first column which was "X.Y..m..d"
+## and now is "%Y-%m-%d".
