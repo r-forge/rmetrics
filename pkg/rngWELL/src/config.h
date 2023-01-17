e50dc54372965a12017921547051d8eee62a2a45
dutangc (Mr ) <dutangc@gmail.com>
2023-01-17 08:37:15 +0000

    start devel version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@6371 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/rngWELL/src/config.h
+++ pkg/rngWELL/src/config.h
@@ -44,7 +44,7 @@
 #define PACKAGE_NAME "rngWELL"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "rngWELL 0.10-8"
+#define PACKAGE_STRING "rngWELL 0.10-10"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "rngwell"
@@ -53,7 +53,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10-8"
+#define PACKAGE_VERSION "0.10-10"
 
 /* Define to 1 if all of the C90 standard headers exist (not just the ones
    required in a freestanding environment). This macro is provided for
