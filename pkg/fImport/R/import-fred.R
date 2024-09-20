b726cea20f419b6c3292545e0d474d23a32f07e7
geobosh (Dr ) <georgi.boshnakov@manchester.ac.uk>
2024-09-20 08:08:47 +0000

    submit v4041.88
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rmetrics@6500 5d4a7380-5c0c-0410-b446-d4a5018c0119

--- pkg/fImport/R/import-fred.R
+++ pkg/fImport/R/import-fred.R
@@ -56,58 +56,57 @@ fredImport <- function(query, file = "tempfile", source = NULL,
 
     # Source"
     if (is.null(source))
-      ## 2024-09-19 was:  source = "http://research.stlouisfed.org/fred2/series/"
-      source <- "https://fred.stlouisfed.org"
+        ## 2024-09-19 was:  source = "http://research.stlouisfed.org/fred2/series/"
+        source <- "https://fred.stlouisfed.org"
 
     # Check:
     if (frequency != "daily")
-      stop("Only daily data records are supported!")
+        stop("Only daily data records are supported!")
 
     # Download:
     if (try) {
-      # Try for Internet Connection:
-      z = try(fredImport(query, file, source, frequency, from, to,
-                         nDaysBack, save, sep, try = FALSE))
-      if (inherits(z, "try-error") || inherits(z, "Error")) {
-        return("No Internet Access")
-      } else {
-        return(z)
-      }
+        ## Try for Internet Connection:
+        z = try(fredImport(query, file, source, frequency, from, to,
+                           nDaysBack, save, sep, try = FALSE))
+        if (inherits(z, "try-error") || inherits(z, "Error")) {
+            return("No Internet Access or another error")
+        } else {
+            return(z)
+        }
     } else {
-      # Download File:
-      ## queryFile = paste(query, "/downloaddata/", query, ".txt", sep = "")
-      ## url = paste(source, queryFile, sep = "")
-      queryFile = paste0("/data/", query)
-      url = paste0(source, queryFile)
+        ## Download File:
+        ## was:
+        ##     queryFile = paste(query, "/downloaddata/", query, ".txt", sep = "")
+        ##     url = paste(source, queryFile, sep = "")
+        queryFile <- paste0("/data/", query)
+        url <- paste0(source, queryFile)
 
-      tmp = tempfile()
-      download.file(url = url, destfile = tmp)
-#browser()
+        tmp <- tempfile()
+        download.file(url = url, destfile = tmp)
+
         ## # Scan the file:
-        ## x1 = scan(tmp, what = "", sep = "\n")
+        ## was:
+        ##     x1 = scan(tmp, what = "", sep = "\n")
         ##
-        ## # Extract dates ^19XX and ^20XX:
-        ## x2 = x1[regexpr(pattern="^[12][90]", x1, perl=TRUE) > 0]
-        ## x1 = x2[regexpr(pattern=" .$", x2, perl=TRUE) < 0]
+        ##     # Extract dates ^19XX and ^20XX:
+        ##     x2 = x1[regexpr(pattern="^[12][90]", x1, perl=TRUE) > 0]
+        ##     x1 = x2[regexpr(pattern=" .$", x2, perl=TRUE) < 0]
         ##
-        ## # Compose Time Series:
-        ## data <- matrix(
-        ##   as.numeric(substring(x1, 11, 999)), byrow = TRUE, ncol = 1)
-        ## charvec <- substring(x1, 1, 10)
+        ##     # Compose Time Series:
+        ##     data <- matrix(
+        ##       as.numeric(substring(x1, 11, 999)), byrow = TRUE, ncol = 1)
+        ##     charvec <- substring(x1, 1, 10)
+        if(!requireNamespace("xml2") || !requireNamespace("rvest"))
+            stop("this function requires packages 'xml2' and 'rvest',please install them")
+        xml2 <- xml2::read_html(tmp, encoding = "UTF-8")
 
-        if(!requireNamespace("xml2") || requireNamespace("rvest"))
-
-            xml2 <- xml2::read_html(tmp, encoding = "UTF-8")
-            if(requireNamespace("rvest"))
-
-            tbls <- rvest::html_table(xml2)
-
-
+        tbls <- rvest::html_table(xml2)
         datatbl <- tbls[[2]]
 
-        ## VALUE is character since '.' stands for NA
-        ## maybe should be defensive here - what happens if there are no missing values?
-        ##    will VALUE be still character?
+        ## VALUE is character since '.' stands for NA;
+        ## TODO (GNB):
+        ##      maybe should be defensive here - what happens if there are no missing values?
+        ##      will VALUE be still character?
         datatbl$VALUE[datatbl$VALUE == "."] <- NA
         datatbl$VALUE <- as.numeric(datatbl$VALUE)
 
@@ -127,7 +126,7 @@ fredImport <- function(query, file = "tempfile", source = NULL,
           from <- to - nDaysBack*24*3600
         }
       }
-      from <- trunc(as.timeDate(from),"days")
+      from <- trunc(as.timeDate(from), "days")
 
       X <- window(X, from, to)
     }
@@ -140,6 +139,7 @@ fredImport <- function(query, file = "tempfile", source = NULL,
     }
 
     # Result:
+    ## TODO (GNB): could do better - tbls[[1]] contains header information
     ans <- new("fWEBDATA",
                call = match.call(),
                param = c(
@@ -151,7 +151,7 @@ fredImport <- function(query, file = "tempfile", source = NULL,
 
     # Return Value:
     ans
-  }
+}
 
 
 # ------------------------------------------------------------------------------
